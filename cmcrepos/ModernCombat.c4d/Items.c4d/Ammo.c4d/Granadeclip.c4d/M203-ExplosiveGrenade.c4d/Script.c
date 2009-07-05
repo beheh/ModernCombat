5bbecf3b5bd6d55071ca4991ed561228701bd1ad
Michael14 <michael@missile.beheh.de>
2009-07-05 17:27:47 +0000

    Minor Fix, verursacht durch SVN Übernahmefehler.

---
+++ cmcrepos/ModernCombat.c4d/Items.c4d/Ammo.c4d/Granadeclip.c4d/M203-ExplosiveGrenade.c4d/Script.c
@@ -0,0 +1,101 @@
+/*-- M203-Explosivgranate --*/
+
+#strict
+#include GREN
+#include STAP
+
+local active,sx,sy;
+
+public func IsAmmoPacket() { return(true); }
+public func AmmoID()       { return(GRAM); }
+public func AmmoCount()    { return(1); }
+public func IsRifleGrenade()    { return(true); }
+public func BlastRadius(){return(20);}
+
+
+func IsBulletTarget(id id)
+{
+  // Kann von anderen Geschossen getroffen werden
+  return(true);
+}
+
+func Launch(int xdir, int ydir, int iDmg,a,b,c)
+{
+  SetCategory(C4D_Vehicle(),this());
+  active = true;
+  sx = GetX();
+  sy = GetY();
+  inherited(xdir,ydir,iDmg,a,b,c);
+}
+
+protected func SecureDistance(){return(110);}
+
+protected func Secure()
+{
+  if(!active)
+    return(true);
+    
+  if(Distance(GetX(),GetY(),sx,sy) <= SecureDistance())
+    return(true);
+    
+  return(false);
+}
+
+func Hit()
+{
+  HitObject();
+}
+
+func ExplodeDelay() { return(35*3); }
+
+func FxGrenadeTimer(object target, int effect, int time)
+{
+  if(time > ExplodeDelay()) return(HitObject());
+  var vel=Abs(GetXDir())+Abs(GetYDir());
+  var alpha=Max(0,100-vel);
+  
+  CreateParticle("Smoke2", -GetXDir()/6, -GetYDir()/6, RandomX(-15, 15), -5,
+                       vel/6+RandomX(10, 30), RGBa(100,100,100,50+alpha));
+                       
+  SetR(Angle (0,0,GetXDir(),GetYDir()));
+}
+
+func HitObject(object pObj)
+{
+  if(Secure())
+  {
+    if(pObj)
+     {
+       DoDmg(Distance(GetXDir(),GetYDir())/5,DMG_Projectile,pObj); 
+       CastParticles("Smoke3",12,10,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+       if(GetOCF(pObj) & OCF_Living)
+         Sound("SharpnelImpact*.ogg");
+       else
+         Sound("BlockOff*.ogg");
+       RemoveObject();
+       return();
+     }
+     else
+     {
+       Sound("GrenadeHit*.ogg");
+       Sparks(30,RGB(255,128));
+       CastParticles("Smoke3",12,10,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+       RemoveObject();
+       return();
+     }
+     Sound("GrenadeHit*.ogg");
+     Sparks(30,RGB(255,128));
+     CastParticles("Smoke3",12,10,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+     RemoveObject();
+     return();
+  }
+  Trigger();
+}
+
+func Trigger(object pObj)
+{
+  Explode(BlastRadius()*2/3);
+  DamageObjects(BlastRadius()*3/2,BlastRadius()*2,this());
+  CreateParticle("Blast",0,0,0,0,10*BlastRadius(),RGB(255,255,128));
+  Sound("GrenadeExplosion*.ogg");
+}
