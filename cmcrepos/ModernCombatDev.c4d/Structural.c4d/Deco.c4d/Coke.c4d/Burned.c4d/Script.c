1228fb9d617df560261396a66381036caf2f99b0
Michael14 <michael@missile.beheh.de>
2009-10-27 15:21:01 +0000

    Veraltetes Structural.c4d und Vegetation.c4d entfernt.

--- cmcrepos/ModernCombatDev.c4d/Structural.c4d/Deco.c4d/Coke.c4d/Burned.c4d/Script.c
+++
@@ -1,8 +0,0 @@
-/*-- kaputter Automat --*/
-
-#strict
-
-protected func Incineration()
-{
-  Extinguish();
-}
--- cmcrepos/ModernCombatDev.c4d/Structural.c4d/Deco.c4d/Screens.c4d/Propaganda.c4d/Script.c
+++
@@ -1,8 +0,0 @@
-/*-- Propagandaschirm --*/
-
-#strict
-
-func Initialize() {
-  SetAction("Animate");
-  AddFrame();
-}
