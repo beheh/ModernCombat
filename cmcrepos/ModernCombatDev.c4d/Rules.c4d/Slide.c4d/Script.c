a4814cde337b6387b6879f5a2bbe5708e8e235e1
Michael14 <michael@missile.beheh.de>
2009-11-13 18:22:34 +0000

    Rutschen-Regel eingeführt: Bei hartem Bodenkontakt während man nach links oder rechts flog, rutscht man nun ein Stück in die entsprechende Richtung weiter. Objektescript geordnet, Grafik aktualisiert und Beschreibungen erstellt.

---
+++ cmcrepos/ModernCombatDev.c4d/Rules.c4d/Slide.c4d/Script.c
@@ -0,0 +1,44 @@
+/*-- Rutschen --*/
+
+#strict
+#appendto CLN2
+
+
+local speed;      //Geschwindigkeit vor dem Aufprall
+local speedfall;  //8tel der Geschwindigkeit beim Aufprall
+
+protected func Activate(iByPlayer)
+{
+  MessageWindow(GetDesc(), iByPlayer);
+  return(1);
+}
+
+/*-- Timer --*/
+
+public func UpdateCharge()
+{
+  //Nur, wenn Regel aktiviert
+  if(!FindObject(SLAP)) return 0;
+  if(GetAction() ne "FlatUp")
+    speed = GetXDir(this(),70);
+  if(GetAction() eq "FlatUp")
+    if(GetPhase(this())==1)
+      speedfall = speed / 7;
+  if(GetAction() eq "FlatUp")
+    Slipper();
+    
+  _inherited();
+}
+
+private func Slipper() 
+{
+  if(speed != 0)
+   {SetXDir(speed,this(),70);
+    speed -= speedfall;}
+  return(1);
+}
+
+public func IsChooseable()
+{
+  return(1);
+}
