dc8ab0a2833fca32f02c5175bd5bc46202720646
user821 <user821@missile.beheh.de>
2010-03-07 11:37:44 +0000

    *lösch*

--- cmcrepos/ModernCombat.c4f/Battlefield.c4f/BF_KubraDam_Unofficial_corrupted.c4s/Watersource.c4d/Script.c
+++
@@ -1,9 +0,0 @@
-/* Wasserquelle */
-
-#strict
-
-protected func CastWater()
-{
-  CastPXS("Water",1,5);
-  return(1);
-}
--- cmcrepos/ModernCombat.c4f/Battlefield.c4f/BF_LangleyFields.c4s/System.c4g/Splatter.c
+++
@@ -1,2 +0,0 @@
-#strict 2
-static const SplatterScale = 50;//Blut-Effekte um 50% kleiner.
