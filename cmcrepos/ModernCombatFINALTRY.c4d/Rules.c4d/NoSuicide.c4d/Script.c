8d6c5d5a04cc30afe27afdaf6cc205baebf82651
Michael14 <michael@missile.beheh.de>
2010-03-14 18:42:18 +0000

    Fake Death Icons hinzugefügt. Kein Selbstmord als Regel eingeführt.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/NoSuicide.c4d/Script.c
@@ -0,0 +1,11 @@
+/*-- Kein Selbstmord --*/
+
+#strict
+
+protected func Activate(iPlr)
+{
+  MessageWindow(GetDesc(),iPlr);
+}
+
+// Kann mittels des Spielzielauswählers ausgewählt werden
+public func IsChooseable() { return(1); }
