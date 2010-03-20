1ed240bdb421f6c95ab1fe9cd5f6ab1b5e47651d
Michael14 <michael@missile.beheh.de>
2010-03-20 23:07:48 +0000

    Klassen neu zusammengestellt und auf 5 beschränkt: Sturmeinheit, Sanitäter, Unterstützer, Antiskill sowie Artillerist. StringTbl verbessert und um englische Counterparts erweitert.

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Classes.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Classes.c
@@ -4,13 +4,14 @@
 #appendto MCSL
 
 
-//Rifleman
+/* Die Sturmeinheit */
+
 private func Class1Info(int iData)
 {
-  if(iData == CData_Name)  return("$rifleman$");
-  if(iData == CData_Desc)  return("$rifleman_desc$");
+  if(iData == CData_Name)  return("$assault$");
+  if(iData == CData_Desc)  return("$assault_desc$");
   if(iData == CData_Clonk) return(PCMK);
-  if(iData == CData_Items) return("1x {{ASTR}}|1x {{92FS}}|2x {{FGRN}}");
+  if(iData == CData_Items) return("1x {{ASTR}}|1x {{PSTL}}|2x {{FGRN}}");
   if(iData == CData_Facet) return(2);
   return(Default(iData));
 }
@@ -21,63 +22,71 @@ private func Class1Setup(int iPlr)
   DoAmmo(STAM,100,new);
   DoAmmo(GRAM,12,new);
   new->CreateContents(ASTR);
-  new->CreateContents(92FS);
+  new->CreateContents(PSTL);
   CreateObject(FGRN,0,0,iPlr)->Activate(new);
   new->CreateContents(FGRN);
   return(new);
 }
 
-//Assault
+/* Der Sanitäter */
+
 private func Class2Info(int iData)
 {
-  if(iData == CData_Name)  return("$assault$");
-  if(iData == CData_Desc)  return("$assault_desc$");
-  if(iData == CData_Clonk) return(PCMK);
-  if(iData == CData_Items) return("1x {{P29W}}|1x {{92FS}}|1x {{STUN}}");
-  if(iData == CData_Facet) return(2);
+
+  if(iData == CData_Name)  return("$medic$");
+  if(iData == CData_Desc)  return("$medic_desc$");
+  if(iData == CData_Clonk) return(MDIC);
+  if(iData == CData_Items) return("1x {{SMGN}}|1x {{PSTL}}|1x {{CDBT}}|1x {{FAPK}}|2x {{STUN}}");
+  if(iData == CData_Facet) return(1);
   return(Default(iData));
 }
 
 private func Class2Setup(int iPlr)
 {
-  var new = CreateObject(PCMK,0,0,iPlr);
-  DoAmmo(STAM,100,new);
-  new->CreateContents(P29W);
-  new->CreateContents(92FS);
+  var new = CreateObject(MDIC,0,0,iPlr);
+  DoAmmo(STAM,50,new);
+  new->CreateContents(SMGN);
+  new->CreateContents(PSTL);
+  new->CreateContents(CDBT);
+  new->CreateContents(FAPK);
+  CreateObject(STUN,0,0,iPlr)->Activate(new);
   new->CreateContents(STUN);
   return(new);
 }
 
-//Peacemaker
+/* Der Unterstützer */
+
 private func Class3Info(int iData)
 {
-
-  if(iData == CData_Name)  return("$peacemaker$");
-  if(iData == CData_Desc)  return("$peacemaker_desc$");
+  if(iData == CData_Name)  return("$support$");
+  if(iData == CData_Desc)  return("$support_desc$");
   if(iData == CData_Clonk) return(PCMK);
-  if(iData == CData_Items) return("1x {{PPGN}}|1x {{RSHL}}|2x {{FRAG}}");
-  if(iData == CData_Facet) return(10);
+  if(iData == CData_Items) return("1x {{MNGN}}|1x {{PSTL}}|1x {{BBTP}}|2x {{SGRN}}");
+  if(iData == CData_Facet) return(8);
   return(Default(iData));
 }
 
 private func Class3Setup(int iPlr)
 {
   var new = CreateObject(PCMK,0,0,iPlr);
-  DoAmmo(STAM,100,new);
-  new->CreateContents(PPGN);
-  new->CreateContents(RSHL);
-  CreateObject(FRAG,0,0,iPlr)->Activate(new);
-  new->CreateContents(FRAG);
+  DoAmmo(STAM,200,new);
+  new->CreateContents(MNGN);
+  new->CreateContents(PSTL);
+  new->CreateContents(BBTP);
+  CreateObject(SGRN,0,0,iPlr)->Activate(new);
+  new->CreateContents(SGRN);
   return(new);
 }
 
-//Riotcrusher
+/* Der Antiskill */
+
 private func Class4Info(int iData)
 {
-  if(iData == CData_Name)  return("$riotcrusher$");
-  if(iData == CData_Desc)  return("$riotcrusher_desc$");
+
+  if(iData == CData_Name)  return("$antiskill$");
+  if(iData == CData_Desc)  return("$antiskill_desc$");
   if(iData == CData_Clonk) return(PCMK);
-  if(iData == CData_Items) return("1x {{MP7R}}|1x {{RSHL}}|2x {{SGRN}}");
+  if(iData == CData_Items) return("1x {{PPGN}}|1x {{PSTL}}|1x {{RSHL}}|2x {{FRAG}}");
   if(iData == CData_Facet) return(5);
   return(Default(iData));
 }
@@ -86,101 +95,34 @@ private func Class4Setup(int iPlr)
 {
   var new = CreateObject(PCMK,0,0,iPlr);
   DoAmmo(STAM,100,new);
-  new->CreateContents(MP7R);
+  new->CreateContents(PPGN);
+  new->CreateContents(PSTL);
   new->CreateContents(RSHL);
-  CreateObject(SGRN,0,0,iPlr)->Activate(new);
-  new->CreateContents(SGRN);
+  CreateObject(FRAG,0,0,iPlr)->Activate(new);
+  new->CreateContents(FRAG);
   return(new);
 }
 
-//Support
+/* Der Artillerist */
+
 private func Class5Info(int iData)
 {
-
-  if(iData == CData_Name)  return("$support$");
-  if(iData == CData_Desc)  return("$support_desc$");
+  if(iData == CData_Name)  return("$artillerist$");
+  if(iData == CData_Desc)  return("$artillerist_desc$");
   if(iData == CData_Clonk) return(PCMK);
-  if(iData == CData_Items) return("1x {{M249}}|1x {{PDTW}}|2x {{SGRN}}");
-  if(iData == CData_Facet) return(8);
+  if(iData == CData_Items) return("1x {{SGST}}|1x {{SMGN}}|1x {{STUN}}|1x {{SGRN}}");
+  if(iData == CData_Facet) return(9);
   return(Default(iData));
 }
 
 private func Class5Setup(int iPlr)
 {
   var new = CreateObject(PCMK,0,0,iPlr);
-  DoAmmo(STAM,200,new);
-  new->CreateContents(M249);
-  new->CreateContents(PDTW);
-  CreateObject(SGRN,0,0,iPlr)->Activate(new);
-  new->CreateContents(SGRN);
-  return(new);
-}
-
-//Medic
-private func Class6Info(int iData)
-{
-
-  if(iData == CData_Name)  return("$medic$");
-  if(iData == CData_Desc)  return("$medic_desc$");
-  if(iData == CData_Clonk) return(MDIC);
-  if(iData == CData_Items) return("1x {{P29W}}|1x {{PDTW}}|1x {{FAPK}}|2x {{STUN}}");
-  if(iData == CData_Facet) return(1);
-  return(Default(iData));
-}
-
-private func Class6Setup(int iPlr)
-{
-  var new = CreateObject(MDIC,0,0,iPlr);
   DoAmmo(STAM,50,new);
-  new->CreateContents(P29W);
-  new->CreateContents(PDTW);
-  new->CreateContents(FAPK);
-  CreateObject(STUN,0,0,iPlr)->Activate(new);
-  new->CreateContents(STUN);
-  return(new);
-}
-
-//Infantryman
-private func Class7Info(int iData)
-{
-  if(iData == CData_Name)  return("$infantryman$");
-  if(iData == CData_Desc)  return("$infantryman_desc$");
-  if(iData == CData_Clonk) return(PCMK);
-  if(iData == CData_Items) return("1x {{DEGL}}|1x {{FAPK}}|4x {{FGRN}}");
-  if(iData == CData_Facet) return(3);
-  return(Default(iData));
-}
-
-private func Class7Setup(int iPlr)
-{
-  var new = CreateObject(PCMK,0,0,iPlr);
-  DoAmmo(STAM,100,new);
-  new->CreateContents(DEGL);
-  new->CreateContents(FAPK);
-  CreateObject(FGRN,0,0,iPlr)->Activate(new);
-  CreateObject(FGRN,0,0,iPlr)->Activate(new);
-  CreateObject(FGRN,0,0,iPlr)->Activate(new);
-  new->CreateContents(FGRN);
-  return(new);
-}
-
-//Artillerist
-private func Class8Info(int iData)
-{
-  if(iData == CData_Name)  return("$artillerist$");
-  if(iData == CData_Desc)  return("$artillerist_desc$");
-  if(iData == CData_Clonk) return(PCMK);
-  if(iData == CData_Items) return("1x {{SGST}}|1x {{92FS}}");
-  if(iData == CData_Facet) return(4);
-  return(Default(iData));
-}
-
-private func Class8Setup(int iPlr)
-{
-  var new = CreateObject(PCMK,0,0,iPlr);
-  DoAmmo(STAM,30,new);
   DoAmmo(GRAM,40,new);
   new->CreateContents(SGST);
-  new->CreateContents(92FS);
+  new->CreateContents(SMGN);
+  CreateObject(STUN,0,0,iPlr)->Activate(new);
+  new->CreateContents(SGRN);
   return(new);
 }
