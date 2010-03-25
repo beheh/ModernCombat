3a42bcebdd7b1ef8a4d70521001eb65c55c297d4
Michael14 <michael@missile.beheh.de>
2010-03-25 19:44:43 +0000

    Kein CMC-Munitions-HUD: Munitions-HUD von Hazard reaktiviert. Mehrere Minor Fixes.

--- cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/HazardClonkAp.c4d/Displays.c4d/HeadUpDisplay.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/HazardClonkAp.c4d/Displays.c4d/HeadUpDisplay.c4d/Script.c
@@ -1,48 +1,81 @@
-
 /*-- HUD --*/
 
 #strict
 
-local alpha,
+local ammoobjs,allAmmo,
+      alpha,
       charge, recharge, ftName,
       ammoID, ammoCount, ammoLoad, masterAmmoCount,
       info;
 
+public func GraphicsID(){return(1HUD);}
+public func ColorTrue(){return(RGB(255,255,0));}
+public func ColorFalse(){return(RGB(255,0,0));}
+public func ColorAmmoID(){return(RGB(255,255,255));}
+public func IdleAlpha(){return(150);}
+
+
 global func GetHUD(object obj, id theID)
 {
   if(!obj)
-    if(!(obj = this()))
-      return;
+   if(!(obj = this()))
+    return;
 
   if(!theID)
-    theID = 1HUD;
+   theID = 1HUD;
 
   var owner = GetOwner(obj);
   if(owner <= -1) return;
 
   var bar = FindObjectOwner(theID,owner);
   if(!bar)
-    bar = CreateObject(theID, 0,0, owner);
+   bar = CreateObject(theID, 0,0, owner);
 
   return bar;
 }
+
+/* Initalisierung */
       
 protected func Initialize()
 {
-  // Einstellungen
+  //Einstellungen
   SetPosition(85, -55);//75, -55
   SetGraphics(0, this(), GraphicsID());
   SetGraphics("Row", this(), GraphicsID(), 1, 1);
   SetVisibility(VIS_None);
-  // Infos
+
+  //Infos
   info = CreateObject(2HUD, 0,0, GetOwner());
   info->SetPosition(168, -23);//150, -55
   
   // Ammobag
-  //InitAmmoBagHUD();
+  InitAmmoBagHUD();
 }
 
-private func InitAmmoBagHUD(){}
+private func InitAmmoBagHUD()
+{
+  //Munitionsanzeige entfernen
+  var def = 0;
+  allAmmo = CreateArray();
+  var arrI = 0;
+  //Munition erfassen
+  for(var i=0; def = GetDefinition(i,C4D_StaticBack); ++i)
+  {
+   if(DefinitionCall(def,"IsAmmo"))
+    allAmmo[arrI++] = def;
+  }
+  ammoobjs = CreateArray();
+
+  //Munitionsobjekte plazieren
+  for(var i=0; i < GetLength(allAmmo); ++i)
+  {
+   ammoobjs[i] = CreateObject(2HUD,0,0,GetOwner());
+   ammoobjs[i]->SetPosition(70,-85-i*28);
+   ammoobjs[i]->SetGraphics(0, 0, allAmmo[i],1,4);
+   ammoobjs[i]->SetObjDrawTransform(750,0,-50000,0,750,-62000,0,1);
+   ammoobjs[i]->SetVisibility(VIS_None);
+  }
+}
 
 protected func Destruction()
 {
@@ -53,39 +86,33 @@ protected func Destruction()
 
 protected func Timer()
 {
-  // Spieler weg?
+  //Spieler weg?
   if(GetOwner() == NO_OWNER || !GetPlayerName(GetOwner())) return(RemoveObject());
-  // Clonk falsch?
+  //Clonk falsch?
   if(!GetCursor(GetOwner()))
   {
-    SetVisibility(VIS_None);
-    SetVisibility(VIS_None,info);
-    return();
+   SetVisibility(VIS_None);
+   SetVisibility(VIS_None,info);
+   return();
   }
   else
   {
-    SetVisibility(VIS_Owner);
-    SetVisibility(VIS_Owner,info);
+   SetVisibility(VIS_Owner);
+   SetVisibility(VIS_Owner,info);
   }
 
-  // Faden
-  /*if(alpha < IdleAlpha()) alpha++;
-  if(alpha > IdleAlpha() && charge != 0) alpha = IdleAlpha();
-  SetClrModulation(RGBa(255,255,255,alpha));
-  SetClrModulation(RGBa(255,255,255,alpha), 0, 1);*/
-
-  // Anzeige
+  //Anzeige
   SetObjDrawTransform(charge, 0, -1000 * (1000-charge)/20,0, 1000, 0, this(), 1);
   
-  // Recharge: roter Balken wandert von links nach rechts
+  //Recharge: Roter Balken wandert von links nach rechts
   if(recharge)
   {
-    SetGraphics("Red", this, GetID(), 2, 2, "Red");
-    SetObjDrawTransform(1000, 0, 50000 - 100 * (1000-recharge),0, 1000, 0, this(), 2);
+   SetGraphics("Red", this, GetID(), 2, 2, "Red");
+   SetObjDrawTransform(1000, 0, 50000 - 100 * (1000-recharge),0, 1000, 0, this(), 2);
   }
   else
   {
-    SetGraphics(0,0,0, 2);
+   SetGraphics(0,0,0, 2);
   }
 }
 
@@ -93,91 +120,132 @@ protected func Timer()
 
 public func Update(object weapon, object ammobag, object who)
 {
-	// Nur den Cursor updaten
-	if(who)
-		if(GetCursor(GetOwner(who)) == who)
-    {
-			UpdateWeapon(weapon);
-    }
+  //Nur den Cursor updaten
+  if(who)
+   if(GetCursor(GetOwner(who)) == who)
+   {
+    UpdateWeapon(weapon);
+   }
+}
+
+public func Update(object weapon, object ammobag, object who)
+{
+  //Nur den Cursor updaten
+  if(who)
+   if(GetCursor(GetOwner(who)) == who)
+   {
+    UpdateWeapon(weapon);
+    UpdateAmmoBag(ammobag, weapon);
+   }
 }
 
 private func UpdateWeapon(object weapon)
 {
-	// Munitionsstand anzeigen
-	if(weapon)
+  //Munitionsstand anzeigen
+  if(weapon)
   {
-    if(weapon->~CustomHUD())
-    {
-      SetVisibility(VIS_Owner);
-      Message("",info);
-      Message("",this);
-      weapon->UpdateHUD(this());
-      ShowSelectProcess(weapon);
-      return;
-    }
-    else if(weapon->~IsWeapon())
-    {
-			// Recharge updaten
-			if(weapon->IsRecharging())
-				recharge = weapon->GetRecharge()*10;
-			else
-				recharge = 0;
-				
-			// charge updaten
-			charge = weapon->GetCharge();
-		
-			// Nachricht ausgeben
-			SetVisibility(VIS_Owner);
-			
-			var ammoid = weapon->GetFMData(FM_AmmoID);
-			var ammoload = weapon->GetFMData(FM_AmmoLoad);
-			var modusname = weapon->GetFMData(FM_Name);
-			var ammocount = weapon->GetAmmo(ammoid);
-      
-      if(weapon->~IsWeapon2() && weapon->GetFMData(FT_Name))
-        Message("@%s|<c %x>%s</c>", this, modusname, RGB(128,128,128), weapon->GetFMData(FT_Name));
-      else
-        Message("@%s", this, modusname);
-		  
-			var color = ColorTrue();
-			if(!ammocount) color = ColorFalse();
-			Message("@<c %x>%d/%d</c>", info, color, ammocount, ammoload);
-      
-      ShowSelectProcess(weapon);
-			
-			return;
-		}
-    else if(weapon->~SelectionTime())
-    {
-      SetVisibility(VIS_Owner);
-      Message("",info);
-      Message("",this);
-      ShowSelectProcess(weapon);
-      return;
-    }
-	}
-	// keine Waffe: ausblenden
-	SetVisibility(VIS_None);
-	Message("",info);
-	Message("",this);
-	charge = 0;
-	recharge = 0;
+   if(weapon->~CustomHUD())
+   {
+    SetVisibility(VIS_Owner);
+    Message("",info);
+    Message("",this);
+    weapon->UpdateHUD(this());
+    ShowSelectProcess(weapon);
+    return;
+   }
+   else if(weapon->~IsWeapon())
+   {
+    //Recharge updaten
+    if(weapon->IsRecharging())
+     recharge = weapon->GetRecharge()*10;
+    else
+     recharge = 0;
+
+    //Charge updaten
+    charge = weapon->GetCharge();
+
+    //Nachricht ausgeben
+    SetVisibility(VIS_Owner);
+
+    var ammoid = weapon->GetFMData(FM_AmmoID);
+    var ammoload = weapon->GetFMData(FM_AmmoLoad);
+    var modusname = weapon->GetFMData(FM_Name);
+    var ammocount = weapon->GetAmmo(ammoid);
+
+    if(weapon->~IsWeapon2() && weapon->GetFMData(FT_Name))
+     Message("@%s|<c %x>%s</c>", this, modusname, RGB(128,128,128), weapon->GetFMData(FT_Name));
+    else
+     Message("@%s", this, modusname);
+
+    var color = ColorTrue();
+    if(!ammocount) color = ColorFalse();
+     Message("@<c %x>%d/%d</c>", info, color, ammocount, ammoload);
+
+    ShowSelectProcess(weapon);
+    return;
+   }
+   else
+   if(weapon->~SelectionTime())
+   {
+    SetVisibility(VIS_Owner);
+    Message("",info);
+    Message("",this);
+    ShowSelectProcess(weapon);
+    return;
+   }
+  }
+  //Keine Waffe: Ausblenden
+  SetVisibility(VIS_None);
+  Message("",info);
+  Message("",this);
+  charge = 0;
+  recharge = 0;
+}
+
+private func UpdateAmmoBag(object ammobag, object weapon)
+{
+  var ammoid = 0;
+  if(weapon)
+   if(weapon->~IsWeapon())
+    ammoid = weapon->GetFMData(FM_AmmoID);
+
+  for(var i=0; i < GetLength(ammoobjs); ++i)
+  {
+   var aobj = ammoobjs[i];
+
+   //Unsichtbar bei "Keine Munition"-Regel
+   if(NoAmmo() || !ammobag)
+   {
+    aobj->SetVisibility(VIS_None);
+    aobj->Message("",aobj);
+    continue;
+   }
+   //Ansonsten Munitionsstand anzeigen
+   aobj->SetVisibility(VIS_Owner);
+
+   var ammo = 0;
+   if(ammobag)
+    ammo = ammobag->GetAmmo(allAmmo[i]);
+
+   var color = "eeeeee";
+    if(!ammo) color = "777777";
+     if(allAmmo[i] == ammoid)
+     {
+      color = "ffff00";
+      if(!ammo) color = "ff0000";
+     }
+   aobj->Message("@<c %s>%d</c>", aobj, color, ammo);
+  }
 }
 
 private func ShowSelectProcess(object item)
 {
   if(GetEffect("SelectItem",item))
   {
-    Recharge(GetEffect("SelectItem",item,0,6),item->~SelectionTime());
+   Recharge(GetEffect("SelectItem",item,0,6),item->~SelectionTime());
   }
 }
 
-private func UpdateAmmoBag()
-{
-	//...
-}
-
-//Altes Zeugs...
 
 public func Charge(int part, int max)
 {
@@ -241,10 +309,3 @@ public func Clear()
   Ammo();
   MasterAmmo(0, -1);
 }
-
-//Modding!
-public func GraphicsID(){return(1HUD);}
-public func ColorTrue(){return(RGB(255,255,0));}
-public func ColorFalse(){return(RGB(255,0,0));}
-public func ColorAmmoID(){return(RGB(255,255,255));}
-public func IdleAlpha(){return(150);}
