3c4dfe2ac6c54f4a3ce3e06fed4233f9bff12cba
Ace <ace@missile.beheh.de>
2010-03-29 17:47:59 +0000

    ! Deathmatch fixed

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Deathmatch.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Deathmatch.c
@@ -5,16 +5,12 @@
 
 public func RelaunchPlayer(int iPlr, object pClonk, int iMurdererPlr)
 {
-  inherited(iPlr, pClonk, iMurdererPlr);
-  if(iPlr == -1 || !GetPlayerName(iPlr)) return;
-  if(iMurdererPlr == -1) //Selfkill? Tja, Pech gehabt.
-    return DoTeamScore(GetPlayerTeam(GetOwner(pClonk)),-1);
-  
-  // kein Teamkill?
-  if(GetPlayerTeam(iPlr) != GetPlayerTeam(iMurdererPlr))
+  if(iMurdererPlr == -1)
+    DoTeamScore(GetPlayerTeam(GetOwner(pClonk)),-1);  
+  //Teamkill?
+  if(GetPlayerTeam(iPlr) == GetPlayerTeam(iMurdererPlr))
     //Punktestand erhöhen
-    DoTeamScore(GetPlayerTeam(iMurdererPlr),+1);
-  else
-    //Teamkill0r!!111einself D:
     DoTeamScore(GetPlayerTeam(iMurdererPlr),-1);
+    
+  inherited(iPlr, pClonk, iMurdererPlr);
 }
