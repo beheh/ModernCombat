d51939cc64efa01c486ca02885323a699333f6b9
Ace <ace@missile.beheh.de>
2010-04-02 19:46:14 +0000

    ! Flaggen sollten nun richtig funktionieren + Borderobjekt

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Experimental.c4d/Border.c4d/Bordershadow.c4d/Script.c
@@ -0,0 +1,8 @@
+/*-- Grenze --*/
+
+#strict 2
+
+func Initialize()
+{
+  SetPlrViewRange(-200);
+}
--- cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/Occupation.c4d/FlagChooser.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/Occupation.c4d/FlagChooser.c4d/Script.c
@@ -181,7 +181,7 @@ public func SelectFlagpole(object pObject)
   var crew = Contents();
   if(!crew) return();
   
-  if((pObject->GetTeam() != GetPlayerTeam(GetOwner(crew))) || (pObject->GetProcess() < 1))
+  if((pObject->GetTeam() != GetPlayerTeam(GetOwner(crew))) || (!pObject->IsFullyCaptured()))
   {
     SetPlrViewRange(0,crew);
     return(Sound("Error",false,crew,100,GetOwner(crew)+1));
