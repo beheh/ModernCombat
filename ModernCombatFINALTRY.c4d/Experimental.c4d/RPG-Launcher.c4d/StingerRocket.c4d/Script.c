d941d100f57746bd17247cb485394a6521f9324c
Michael14 <michael@missile.beheh.de>
2010-04-04 12:08:16 +0000

    Raketenwerfer bearbeitet.

--- ModernCombatFINALTRY.c4d/Experimental.c4d/RPG-Launcher.c4d/StingerRocket.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Experimental.c4d/RPG-Launcher.c4d/StingerRocket.c4d/Script.c
@@ -1,14 +1,18 @@
-/* Stinger Rakete */
+/* Rakete */
 
 #strict
 #include MISS
 
-public func Acceleration() { return(3); }
-public func MaxTime() { return(35*5); }
-public func MaxSpeed() { return(100); }
+public func Acceleration()	{return(3);}
+public func MaxTime()		{return(35*5);}
+public func MaxSpeed()		{return(100);}
+
+
+/* Start */
 
 public func Launch(int iAngle, int iDmg, object pFollow)
 {
+  //Werte setzen
   iSpeed = 5;
   iDamage = iDmg;
   if(!iDamage) iDamage = 35;
@@ -17,22 +21,27 @@ public func Launch(int iAngle, int iDmg, object pFollow)
   SetXDir(+Sin(iAngle,iSpeed));
   SetYDir(-Cos(iAngle,iSpeed));
   SetAction("Travel");
-  
+
+  //Effekte
   Sound("RPGP_ThrustStart.ogg");
   AddEffect("ThrustSound",this(),1,11,this());
-
   AddLight(70,RGB(255,200,200),this(),GLOW);
 
+  //Treffer- und Steuereffekt einsetzen
   AddEffect("HitCheck", this(), 1,1, 0, SHT1,shooter);
   if(pFollow)
-    AddEffect("Follow", this(), 1,1, 0, GetID(),pFollow);
+   AddEffect("Follow", this(), 1,1, 0, GetID(),pFollow);
 }
+
+/* Soundeffekt */
 
 public func FxThrustSoundTimer(object pTarget, int iEffectNumber, int iEffectTime)
 {
   Sound("RPGP_Thrust.ogg",0,0,0,0,+1);
   return(-1);
 }
+
+/* Optischer Steuerungseffekt */
 
 public func FxFollowStart(object pTarget, int iEffectNumber, int iTemp, obj)
 {
@@ -42,26 +51,28 @@ public func FxFollowStart(object pTarget, int iEffectNumber, int iTemp, obj)
 
 public func FxFollowTimer(object pTarget, int iEffectNumber, int iEffectTime)
 {
-    //HAZARD
   var obj = EffectVar(0,pTarget,iEffectNumber);
   if(!obj)
-    return(-1);
+   return(-1);
+  //Schütze nicht mehr am Zielen?
   if(!obj->~IsAiming())
-    return(-1);
+   return(-1);
 
-    var iDAngle = obj->AimAngle();
-    var iAngle = GetR(pTarget);
-    
-    var iDiff = Normalize(iDAngle - iAngle,-180);
-    var iTurn = Min(Abs(iDiff),6);
+   var iDAngle = obj->AimAngle();
+   var iAngle = GetR(pTarget);
 
-    pTarget->SetR(iAngle+iTurn*((iDiff > 0)*2-1));
+   var iDiff = Normalize(iDAngle - iAngle,-180);
+   var iTurn = Min(Abs(iDiff),6);
+
+   pTarget->SetR(iAngle+iTurn*((iDiff > 0)*2-1));
 }
 
 private func Traveling()
 {
   if(GetActTime() >= MaxTime()) return(Hit());
+  //Geschwindigkeit erhöhen
   Accelerate();
+  //Rauchspur
   Smoking();
 }
 
@@ -69,6 +80,8 @@ private func StopThrust()
 {
   Sound("RPGP_Thrust.ogg",0,0,0,0,-1);
 }
+
+/* Rauch */
 
 private func Smoking()
 {
@@ -79,27 +92,30 @@ private func Smoking()
 
   for(var i=0; i<dist; i+=ptrdist)
   {
-    var x = -maxx*i/dist;
-    var y = -maxy*i/dist;
+   var x = -maxx*i/dist;
+   var y = -maxy*i/dist;
 
-    var rand = RandomX(-10,10);
-    var xdir = +Sin(GetR()+rand,8);
-    var ydir = -Cos(GetR()+rand,8);
+   var rand = RandomX(-10,10);
+   var xdir = +Sin(GetR()+rand,8);
+   var ydir = -Cos(GetR()+rand,8);
 
-    CreateParticle("Smoke3",x,y,xdir,ydir,RandomX(50,70),RGBa(255,255,255,85),0,0);
+   CreateParticle("Smoke3",x,y,xdir,ydir,RandomX(50,70),RGBa(255,255,255,85),0,0);
   }
-  
+
   CreateParticle("MuzzleFlash2",-maxx,-maxy,+Sin(GetR()+180,500),-Cos(GetR()+180,500),RandomX(80,140),RGBa(255,200),this());
 }
 
+/* Schaden */
+
 public func Damage()
 {
-  if(GetDamage() > 1 && !exploding)
+  //Rakete abschießbar
+  if(GetDamage() > 5 && !exploding)
   {
-    SetAction("Idle");
-    FadeOut4K(4);
-    AddEffect("Damaged",this(),1,1,this());
-    Sound("RPGP_ShotDown.ogg");
+   SetAction("Idle");
+   FadeOut4K(4);
+   AddEffect("Damaged",this(),1,1,this());
+   Sound("RPGP_ShotDown.ogg");
   }
 }
 
@@ -111,18 +127,20 @@ public func FxDamagedTimer(object pTarget, int iEffectNumber, int iEffectTime)
 public func Hit()
 {
   if(GetAction() eq "Idle")
-    Explode(3,0,0,0,1);
+   Explode(3,0,0,0,1);
   else
-    HitObject();
+   HitObject();
 }
 
 private func HitObject()
 {
   if(GetAction() eq "Idle")
-    Explode(3,0,0,0,1);
+   Explode(3,0,0,0,1);
 
   exploding = true;
   Sound("GrenadeExplosion*.ogg");
+
+  //Schaden verursachen
   DamageObjects(iDamage,iDamage,this());
   Explode(iDamage,0,0,0,1);
 }
