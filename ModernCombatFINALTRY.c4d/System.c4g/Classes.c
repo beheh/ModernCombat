5c730382cf73cca4b5bd59eec4cb676e0777a9f6
Michael14 <michael@missile.beheh.de>
2010-04-05 17:35:07 +0000

    Klassen aktualisiert.

--- ModernCombatFINALTRY.c4d/System.c4g/Classes.c
+++ ModernCombatFINALTRY.c4d/System.c4g/Classes.c
@@ -66,7 +66,7 @@ private func Class3Info(int iData)
   if(iData == CData_Name)  return("<c ffff33>$support$</c>");
   if(iData == CData_Desc)  return("$support_desc$");
   if(iData == CData_Clonk) return(PCMK);
-  if(iData == CData_Ammo)  return("200x {{STAM}}10x {{MISS}}");
+  if(iData == CData_Ammo)  return("200x {{STAM}}10x {{MIAM}}");
   if(iData == CData_Items) return("1x {{MNGN}}     1x {{RTLR}}|1x {{BBTP}}     1x {{DGNN}}|1x {{PGRN}}     2x{{SGRN}}");
   if(iData == CData_Facet) return(3);
   return(Default(iData));
@@ -75,8 +75,8 @@ private func Class3Info(int iData)
 private func Class3Setup(int iPlr)
 {
   var new = CreateObject(PCMK,0,0,iPlr);
-  DoAmmo(STAM,200,new);
-  DoAmmo(MISS,10,new);
+  DoAmmo(STAM,400,new);
+  DoAmmo(MIAM,10,new);
   new->CreateContents(MNGN);
   new->CreateContents(RTLR);
   new->CreateContents(BBTP);
