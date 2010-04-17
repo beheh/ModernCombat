127c6b69b9c7b485aef52caeca7998626c2ee6eb
Ace <ace@missile.beheh.de>
2010-04-17 13:03:19 +0000

    +Ener+ Energiebalken beim Medic, Icons für alle fehlen noch

--- ModernCombatFINALTRY.c4d/Crew.c4d/Medic.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Crew.c4d/Medic.c4d/Script.c
@@ -13,3 +13,21 @@ public func HazardGearSupported(object pGear)
   if(GetID(pGear) == HARM) return(false);
   return(true);
 }
+
+func Initialize()
+{
+  UpdateMediHUD();
+  _inherited(...);
+}
+
+protected func UpdateMediHUD()
+{
+  for(var clonk in FindObjects(Find_OCF(OCF_Alive),Find_OCF(OCF_CrewMember),Find_NoContainer()))
+  {
+    if(FindObject2(Find_ID(HBAR), Find_ActionTarget(clonk), Find_Exclude()))
+      continue;
+    if(!Hostile(GetOwner(clonk),GetOwner()))
+      CreateObject(HBAR,0,0,GetOwner())->Set(clonk,RGB(80,190,255));
+  }
+  ScheduleCall(0,"UpdateMediHUD",1);
+}
