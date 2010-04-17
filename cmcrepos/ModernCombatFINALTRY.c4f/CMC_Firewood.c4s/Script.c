caf624038ad1e2345765a6a3763b35708861dfe7
Michael14 <michael@missile.beheh.de>
2010-04-17 20:31:47 +0000

    Firewood: Bootspawn für Mitte eingefügt.

--- cmcrepos/ModernCombatFINALTRY.c4f/CMC_Firewood.c4s/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4f/CMC_Firewood.c4s/Script.c
@@ -270,8 +270,9 @@ func CreateEquipment()
   PlaceSpawnpoint(RSHL, 1640, 410);
 
   //Motorboote
-  SetupVehicleSpawn([INFL],DIR_Right,CreateObject(VSPW,670,610,-1),10*10);
-  SetupVehicleSpawn([INFL],DIR_Left,CreateObject(VSPW,2590,610,-1),10*10);
+  SetupVehicleSpawn([INFL],DIR_Right,CreateObject(VSPW,670,620,-1),10*10);
+  SetupVehicleSpawn([INFL],DIR_Right,CreateObject(VSPW,1640,620,-1),10*10);
+  SetupVehicleSpawn([INFL],DIR_Left,CreateObject(VSPW,2590,620,-1),10*10);
 }
 
 /* Regelwähler */
