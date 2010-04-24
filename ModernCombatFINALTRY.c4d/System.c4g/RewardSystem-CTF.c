f141fb65b0ba233f3b7fa5d929f516a2aef6e8ab
Benedict Etzel <developer@beheh.de>
2010-04-24 19:52:52 +0000

    * Aufräumen im System.c4g + CTF-Punkte

---
+++ ModernCombatFINALTRY.c4d/System.c4g/RewardSystem-CTF.c
@@ -0,0 +1,18 @@
+/*-- CTF --*/
+
+//Punkte für Flaggenklauen
+
+#strict 2
+#appendto GCTF
+
+public func FlagScored(int iTeam, int iCTeam, object pClonk)
+{
+  DoPlayerPoints(BonusPoints("CTFSteal"), RWDS_BattlePoints, GetOwner(pClonk), pClonk, IC08);
+  return _inherited();
+}
+
+public func FlagReturned(int iTeam)
+{
+  DoPlayerPoints(BonusPoints("CTFRegain"), RWDS_BattlePoints, GetOwner(aFlagCarrier[iTeam]), aFlagCarrier[iTeam], IC09);
+  return _inherited();
+}
