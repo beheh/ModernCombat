b4c937a51f6ee6e5f97bc829e3d74da6b6eea6f6
Benedict Etzel <developer@beheh.de>
2010-04-25 14:51:52 +0000

    ! Scoreboard in CTF

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/RewardSystem-CTF.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/RewardSystem-CTF.c
@@ -8,11 +8,11 @@
 public func FlagScored(int iTeam, int iCTeam, object pClonk)
 {
   DoPlayerPoints(BonusPoints("CTFSteal"), RWDS_BattlePoints, GetOwner(pClonk), pClonk, IC08);
-  return _inherited();
+  return _inherited(...);
 }
 
 public func FlagReturned(int iTeam)
 {
   DoPlayerPoints(BonusPoints("CTFRegain"), RWDS_BattlePoints, aFlagCarrier[iTeam], GetCursor(aFlagCarrier[iTeam]), IC09);
-  return _inherited();
+  return _inherited(...);
 }
