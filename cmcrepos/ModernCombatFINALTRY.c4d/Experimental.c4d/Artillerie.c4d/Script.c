a419c1ecfccadbd1612319b0c2beac2985b3e0f8
Ace <ace@missile.beheh.de>
2010-05-02 10:27:36 +0000

    + Artilleriesounds hinzugefügt, Lichtlein, ob Artillerie feuerbereit, Artillerie etwas stärker gemacht

--- cmcrepos/ModernCombatFINALTRY.c4d/Experimental.c4d/Artillerie.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Experimental.c4d/Artillerie.c4d/Script.c
@@ -17,14 +17,21 @@ func Initialize() {
 
 func Rotation() {
 
-if(!bRotate) return(0);
+  if(!iCooldown)
+    CreateParticle("PSpark",0,-4,0,0,40,RGB(0,255,0),this());
+  else
+  {
+    iCooldown -= 3;
+    CreateParticle("PSpark",0,-4,0,0,40,RGB(255,0,0),this());
+  }
 
-if(GetR(pCannon)> 80) {bRotate=0; SetR(GetR(pCannon)-1,pCannon); Sound("CannonStop"); return(0);}
-if(GetR(pCannon)<-80) {bRotate=0; SetR(GetR(pCannon)+1,pCannon); Sound("CannonStop"); return(0);}
+  if(!bRotate) return(0);
 
-if(bDirection==0) {Sound("CannonRotation"); SetR(GetR(pCannon)+1,pCannon);}
-if(bDirection==1) {Sound("CannonRotation"); SetR(GetR(pCannon)-1,pCannon);}
+  if(GetR(pCannon)> 80) {bRotate=0; SetR(GetR(pCannon)-1,pCannon); Sound("CannonStop"); return(0);}
+  if(GetR(pCannon)<-80) {bRotate=0; SetR(GetR(pCannon)+1,pCannon); Sound("CannonStop"); return(0);}
 
+  if(bDirection==0) {Sound("CannonRotation"); SetR(GetR(pCannon)+1,pCannon);}
+  if(bDirection==1) {Sound("CannonRotation"); SetR(GetR(pCannon)-1,pCannon);}
 }
 
 func ControlRight() {
@@ -48,8 +55,9 @@ func ControlUp() {
 
 func ControlDig(object pByObj)
 {
-  var iX = -AbsX(), iY = -AbsY()-30, iXDir = Sin(GetR(pCannon),15), iYDir = -Cos(GetR(pCannon),15);
-  SimFlight(iX,iY,iXDir,iYDir,50,50,500,1);
+  var iX = -AbsX()+Sin(GetR(pCannon),34), iY = -AbsY()-Cos(GetR(pCannon),34)-3, iXDir = Sin(GetR(pCannon),150), iYDir = -Cos(GetR(pCannon),150);
+  if(!SimFlight(iX,iY,iXDir,iYDir,50,50,500,10))
+    return(PlayerMessage(GetOwner(pByObj),"$OutOfMap$"));
   var target = CreateObject(ARCR,AbsX(iX),AbsY(iY),GetOwner(pByObj));
   SetVisibility(VIS_Owner,target);
   SetPlrView(GetOwner(pByObj),target);
@@ -57,14 +65,11 @@ func ControlDig(object pByObj)
 }
 
 func ControlThrow(object pByObj) {
-   if(iCooldown > 0) return(1); 
-   iCooldown=60*35;
+   if(iCooldown > 0) return(Sound("Error",0,0,0,GetOwner(pByObj)+1)); 
+   iCooldown=50*35;
 
    byObj = pByObj;
    Sound("Info.ogg");
-
-   //Partikeleffekt
-   //Schedule("CreateParticle(\"PSpark\",5,-10,0,0,60,GetPlrColor(GetOwner(byObj)),this())",20,50);
 
    ScheduleCall(this(),"Shoot",70,10);
 }
@@ -76,18 +81,11 @@ public func Shoot()
    
    Sound("Blast2");
    var pProjectile=CreateObject(_GSS,iX,iY,GetOwner(byObj));
-   SetXDir( Sin(GetR(pCannon),RandomX(14,16)),pProjectile,1);
-   SetYDir(-Cos(GetR(pCannon),RandomX(14,16)),pProjectile,1);
+   SetXDir( Sin(GetR(pCannon),RandomX(140,160)),pProjectile,10);
+   SetYDir(-Cos(GetR(pCannon),RandomX(140,160)),pProjectile,10);
    ObjectSetAction(pCannon,"Backdraft");
    CreateParticle("LightFlash",iX,iY,0,0,500,RGBa(255,255,255,32));
    var iSmoke1=14;
-   while(iSmoke1) {CreateParticle("Smoke",iX,iY,0,RandomX(40,-40),RandomX(50,100),RGB(96,96,96)); iSmoke1--;}
+   while(iSmoke1) {CreateParticle("Smoke",iX,iY+RandomX(-20,20),0,0,RandomX(50,100),RGB(96,96,96)); iSmoke1--;}
    MuzzleFlash(RandomX(30,75),this(),iX,iY,GetR(pCannon));
-   iCooldown -= 70;
-}
-
-protected func Timer()
-{
-  if(!iCooldown)
-    CreateParticle("PSpark",5,0,0,0,70,GetPlrColor(GetOwner(byObj)),this());
 }
