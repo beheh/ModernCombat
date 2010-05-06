fdac6779420a3a8dbe54adb40fc02a596ca8fc72
Benedict Etzel <developer@beheh.de>
2010-05-06 14:25:40 +0000

    ! Kommt jedes Mal @DeathAnnounce

--- cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/InjuredClonk.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/InjuredClonk.c4d/Script.c
@@ -142,7 +142,7 @@ public func Destruction()
   //Sichtdaten zurücksetzen
   SetFoW(oldvisstate,GetOwner(clonk));
   SetPlrViewRange(oldvisrange,clonk);
-  clonk->RemoveEffect("NoAnnounce");
+  RemoveEffect("NoAnnounce", clonk);
 }
 
 public func RejectCollect(id idObj, object pObj)
