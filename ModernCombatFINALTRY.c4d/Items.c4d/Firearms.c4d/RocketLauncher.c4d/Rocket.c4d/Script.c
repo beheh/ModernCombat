5203a1997714c6dae6a8a41983d12fbf07369225
Benedict Etzel <developer@beheh.de>
2010-05-09 19:11:49 +0000

    ! Fahrzeuge?

--- ModernCombatFINALTRY.c4d/Items.c4d/Firearms.c4d/RocketLauncher.c4d/Rocket.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Items.c4d/Firearms.c4d/RocketLauncher.c4d/Rocket.c4d/Script.c
@@ -79,7 +79,7 @@ public func FxFollowTimer(object pTarget, int iEffectNumber, int iEffectTime)
   }
   //Haben wir noch ein Ziel?
   if(!EffectVar(1,pTarget,iEffectNumber)) {
-  	var pEnemies = FindObjects(Find_NoContainer(), Find_Distance(300, GetX(pTarget), GetY(pTarget)), Sort_Distance(GetX(pTarget), GetY(pTarget)));
+  	var pEnemies = FindObjects(Find_Distance(300, GetX(pTarget), GetY(pTarget)), Sort_Distance(GetX(pTarget), GetY(pTarget)));
 		for(var pEnemy in pEnemies) {
 			if(!GetEffect("TracerDart", pEnemy)) continue;
       if(GetPlayerTeam(GetController(pTarget)) != GetPlayerTeam(EffectVar(0, pTarget, GetEffect("TracerDart", pEnemy)))) continue;
