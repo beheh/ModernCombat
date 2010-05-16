52cac95431257141a573d28c28c7b89c1258e35e
Michael14 <michael@missile.beheh.de>
2010-05-16 13:25:33 +0000

    + Eigene Steuerkonsole eingeführt. * SSA nun anfälliger gegen Beschuss.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/Console.c4d/Script.c
@@ -0,0 +1,54 @@
+/*-- Konsole --*/
+
+#strict
+
+local target;
+
+public func IsMachine()	{return(1);}
+
+
+/* Timer */
+
+protected func Timer(pClonk)
+{
+  //Kein zu steuerndes Objekt mehr? Selbstzerstörung.
+  if(!target)
+  {
+   CastParticles("MetalSplinter",4,50,0,0,40,150);
+   CastParticles("Smoke3",5,10,0,0,50,200);
+   Sound("StructuralDamage*.ogg");
+   return(RemoveObject());
+  }
+
+  //Blinkeffekt
+  if(Random(2)) 
+  {CreateParticle("NoGravSpark", 3, 1, 0, 0, 50, RGBa(250, 10, 10, 50));}
+  else
+  {CreateParticle("NoGravSpark", 3, 3, 0, 0, 50, RGBa(10, 10, 250, 50));}
+}
+
+/* Bedienung */
+
+protected func ControlDig(pClonk)
+{
+  [$Control$]
+
+  //Sicht auf Zielobjekt zentrieren
+  SetPlrView(GetController(pClonk),target);
+
+  //Menü erstellen
+  CreateMenu(GetID(target), pClonk, target, 0, Format("$Control$: %s", GetName(target)), 0, 1);
+  for(var i = 1, desc ; desc = target->~ConsoleControl(i) ; i++)
+  {
+   AddMenuItem(desc, Format("ConsoleControlled(%d, %d, %d)", i, ObjectNumber(pClonk), ObjectNumber(this())), GetID(), pClonk, 0, 0, "$Control$");
+   Sound("CatapultSet");
+  }
+  return(1);
+}
+
+/* Ziel setzen */
+
+public func Set(pTarget)
+{
+  target = pTarget;
+}
