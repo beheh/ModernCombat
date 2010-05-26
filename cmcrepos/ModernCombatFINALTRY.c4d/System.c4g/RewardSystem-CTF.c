737a3ea0b589eba1f2222b6a8b41237b10983dc1
Benedict Etzel <developer@beheh.de>
2010-05-26 21:32:10 +0000

    - Helper-Visible.c ! Fix Attempt: #7412 (CTF: Scoreboard zeigt Flaggenbesitzer falsch) ! Error in Besitznahme

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/RewardSystem-CTF.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/RewardSystem-CTF.c
@@ -1,20 +1,15 @@
 /*-- CTF --*/
-
 //Punkte für Flaggen stehlen und zurückbringen.
 
 #strict 2
-
-#appendto GCTF
-
+#appendto RWDS
 
 public func FlagScored(int iTeam, int iCTeam, object pClonk)
 {
   DoPlayerPoints(BonusPoints("CTFSteal"), RWDS_BattlePoints, GetOwner(pClonk), pClonk, IC08);
-  return _inherited(...);
 }
 
 public func FlagReturned(int iTeam, object pClonk)
 {
   DoPlayerPoints(BonusPoints("CTFRegain"), RWDS_BattlePoints, GetOwner(pClonk), pClonk, IC09);
-  return _inherited(...);
 }
