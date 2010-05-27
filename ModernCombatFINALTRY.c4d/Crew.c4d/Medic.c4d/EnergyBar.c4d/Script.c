ea5fd97f15045ecdbb7d0f748cd4cc39b28d50a1
Benedict Etzel <developer@beheh.de>
2010-05-27 11:27:25 +0000

    ! Viele Fixes bei den Balken

--- ModernCombatFINALTRY.c4d/Crew.c4d/Medic.c4d/EnergyBar.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Crew.c4d/Medic.c4d/EnergyBar.c4d/Script.c
@@ -3,12 +3,16 @@
 #strict 2
 
 local obj;
+local fActive;
 
+public func IsBar() {	return true; }
+public func BarActive() {	return fActive; }
 
 /* Initalisierung */
 
 protected func Initialize()
 {
+	fActive = false;
 	SetVisibility(VIS_None);
   SetGraphics("Row", this(), GetID(), 1, 1);
 }
@@ -30,12 +34,15 @@ public func Set(object target, int color)
 
 public func Update()
 {
+	SetVisibility(VIS_None);
+	fActive = false;
   if(!obj || !(GetOCF(obj) & OCF_Alive) || Contained(obj) || Hostile(GetOwner(), GetOwner(obj)))
-   return SetVisibility(VIS_None);
+   return;
   var percent = BoundBy(GetEnergy(obj)*100/(GetDefCoreVal("Energy","Physical",GetID(obj))/1000),0,100);
   if(percent > 95)
-   return SetVisibility(VIS_None);
+   return;
   SetVisibility(VIS_Owner);
+  fActive = true;
   SetObjDrawTransform(10*percent,0,-160*(100-percent),0,1000,0,0,1);
   return true;
 }
