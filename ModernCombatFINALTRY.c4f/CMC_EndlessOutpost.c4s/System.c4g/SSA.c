a6f612577be76b9720186aae63e685b04139a373
Benedict Etzel <developer@beheh.de>
2010-06-01 19:47:24 +0000

    ! #7545 (Syntaxdoofheiten) teilweise gefixt

--- ModernCombatFINALTRY.c4f/CMC_EndlessOutpost.c4s/System.c4g/SSA.c
+++ ModernCombatFINALTRY.c4f/CMC_EndlessOutpost.c4s/System.c4g/SSA.c
@@ -5,9 +5,9 @@
 
 public func EMPShock()
 {
-    if(Damaged) return(0);
-    Damaged = 1;
-    AddEffect("EMP_Damaged", this(), 180, 50, this(), GetID() );
+    if(IsDamaged()) return(0);
+    Destroyed();
+    AddEffect("EMP_Damaged", this(), 180, 50, this(), GetID());
     Schedule("CastObjects(SPRK, 1, 15, RandomX(-10, 10), RandomX(-15, 15));", 3, 15, this());
     SetClrModulation(RGB(100,100,100));
     return(1);
