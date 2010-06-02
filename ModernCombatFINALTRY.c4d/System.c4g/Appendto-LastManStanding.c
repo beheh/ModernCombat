23c581d13b20a845604b0ffae3d52d646c61d78b
Benedict Etzel <developer@beheh.de>
2010-06-02 22:25:34 +0000

    ! Killlog beim Entfernen des Clonks ! LMS

--- ModernCombatFINALTRY.c4d/System.c4g/Appendto-LastManStanding.c
+++ ModernCombatFINALTRY.c4d/System.c4g/Appendto-LastManStanding.c
@@ -1,10 +1,31 @@
-/*-- LMS-Bugs mit Hazard --*/
+/*-- LMS-Bugs --*/
 
 #strict 2
 #appendto GLMS
 
+local init;
+
 protected func InitializePlayer(int iPlr, int iX, int iY, object pBase, int iTeam)
 {
-  if(FindObject(CHOS)) return;
-  _inherited(iPlr, iX, iY, pBase, iTeam);
+	if(!chooser) return _inherited(iPlr, iX, iY, pBase, iTeam);
+	return;
+}
+
+private func InitScoreboard()
+{
+	if(!chooser) return _inherited();
+	if(init) return ScheduleCall(this, "InitScoreboard", 1);
+	init = true;
+	_inherited();
+	ScheduleCall(this, "InitScoreboard", 1);
+ 	for(var i = 0 ; i < GetPlayerCount() ; i++) {
+	 	var iPlr = GetPlayerByIndex(i);
+		aPlayerLives[iPlr] = iWinScore * GetCrewCount(iPlr) -1;
+		
+		// anzeigen
+		if(GetTeamPlayerCount(GetPlayerTeam(iPlr)) > 1)
+			SetScoreboardData(iPlr, TEAM_GoalColumn, Format("%d",aPlayerLives[iPlr]), aPlayerLives[iPlr]);
+		else
+			SetScoreboardData(iPlr, TEAM_GoalColumn, Format("<c %x>%d</c>", GetPlrColorDw(iPlr),aPlayerLives[iPlr]), aPlayerLives[iPlr]);
+	}
 }
