da895732743a727bbba76b77209c79677a63be69
Benedict Etzel <developer@beheh.de>
2010-06-03 22:48:22 +0000

    * Ein paar kleine Changes

--- ModernCombatFINALTRY.c4d/Items.c4d/Firearms.c4d/RocketLauncher.c4d/Rocket.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Items.c4d/Firearms.c4d/RocketLauncher.c4d/Rocket.c4d/Script.c
@@ -85,7 +85,7 @@ public func FxFollowTimer(object pTarget, int iEffectNumber, int iEffectTime)
       if(Hostile(GetController(pTarget), EffectVar(0, pEnemy, iEffectTracer))) continue;
 			if(!PathFree(GetX(pTarget), GetY(pTarget), GetX(pEnemy), GetY(pEnemy))) continue;
 		  EffectVar(1,pTarget,iEffectNumber) = pEnemy;
-      Sound("BBTP_Alarm.ogg");
+      Sound("BBTP_Alarm.ogg", false, this);
 		  break;
 		}
   }
