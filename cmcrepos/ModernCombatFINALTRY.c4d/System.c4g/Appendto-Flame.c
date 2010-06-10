ae97321e0c17a74efdc01a77dd5473f4c44a543d
Michael14 <michael@missile.beheh.de>
2010-06-10 21:22:15 +0000

    * Systemscripts sortiert.

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Flame.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Flame.c
@@ -1,16 +1,21 @@
 /*-- KillIcons für Flammen --*/
 
+//Tod durch Flammen werden nun mit einem passenden Icon markiert.
+
 #strict 2
 #appendto FFLM
 
+
 local creator;
 
-func Construction(object byObj) {
-	if(byObj) creator = GetID(byObj);
-	return _inherited(byObj);
+func Construction(object byObj)
+{
+  if(byObj) creator = GetID(byObj);
+  return _inherited(byObj);
 }
 
-func GetKillIcon() {
-	if(creator) return creator;
-	return;
+func GetKillIcon()
+{
+  if(creator) return creator;
+  return;
 }
--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-LastManStanding.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-LastManStanding.c
@@ -1,34 +1,41 @@
-/*-- LMS-Bugs --*/
+/*-- LMS-Fix --*/
+
+//Last Man Standing Bugs überladen und korrigiert.
 
 #strict 2
 #appendto GLMS
 
 local init;
 
+
 protected func InitializePlayer(int iPlr, int iX, int iY, object pBase, int iTeam)
 {
-	if(!chooser) return _inherited(iPlr, iX, iY, pBase, iTeam);
-	return;
+  if(!chooser) return _inherited(iPlr, iX, iY, pBase, iTeam);
+  return;
 }
 
 private func InitScoreboard()
 {
-	if(!init && !chooser) {
-		init = true;
-		ScheduleCall(this, "InitScoreboard", 1);
-	 	for(var i = 0 ; i < GetPlayerCount() ; i++) {
-		 	var iPlr = GetPlayerByIndex(i);
-			aPlayerLives[iPlr] = iWinScore * GetCrewCount(iPlr) -1;
-		
-			// anzeigen
-			if(GetTeamPlayerCount(GetPlayerTeam(iPlr)) > 1)
-				SetScoreboardData(iPlr, TEAM_GoalColumn, Format("%d",aPlayerLives[iPlr]), aPlayerLives[iPlr]);
-			else
-				SetScoreboardData(iPlr, TEAM_GoalColumn, Format("<c %x>%d</c>", GetPlrColorDw(iPlr),aPlayerLives[iPlr]), aPlayerLives[iPlr]);
-		}
-	}
-	else if(!init) {
-		ScheduleCall(this, "InitScoreboard", 1);
-	}
-	return _inherited();
+  if(!init && !chooser)
+  {
+   init = true;
+   ScheduleCall(this, "InitScoreboard", 1);
+   for(var i = 0 ; i < GetPlayerCount() ; i++)
+   {
+    var iPlr = GetPlayerByIndex(i);
+    aPlayerLives[iPlr] = iWinScore * GetCrewCount(iPlr) -1;
+
+    //Anzeigen
+    if(GetTeamPlayerCount(GetPlayerTeam(iPlr)) > 1)
+     SetScoreboardData(iPlr, TEAM_GoalColumn, Format("%d",aPlayerLives[iPlr]), aPlayerLives[iPlr]);
+    else
+     SetScoreboardData(iPlr, TEAM_GoalColumn, Format("<c %x>%d</c>", GetPlrColorDw(iPlr),aPlayerLives[iPlr]), aPlayerLives[iPlr]);
+   }
+  }
+  else
+  if(!init)
+  {
+   ScheduleCall(this, "InitScoreboard", 1);
+  }
+  return _inherited();
 }
