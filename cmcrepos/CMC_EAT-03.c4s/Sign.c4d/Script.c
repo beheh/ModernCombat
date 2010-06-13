ca2e341df48c60ac9a967222526a6b0593600934
Michael14 <michael@missile.beheh.de>
2010-06-13 19:14:43 +0000

    + Schild eingefügt. + Sektion 2 besitzt nun Pfeile und Schilder (letztere haben trotz korrektem Text noch blanke Nachrichten).

---
+++ cmcrepos/CMC_EAT-03.c4s/Sign.c4d/Script.c
@@ -0,0 +1,40 @@
+/*-- Informations-Schild --*/
+
+#strict
+
+local szText, fNewInfo;
+
+
+/* Initalisierung */
+
+protected func Initialize()
+{
+  szText = "";
+  fNewInfo = false;
+}
+
+protected func ControlUp(object caller)
+{
+  CreateMenu(_SGN, caller, caller, 0, "", 0, C4MN_Style_Dialog);
+  AddMenuItem(Format("Portrait:%i::ffffff::1", _SGN), "", NONE, caller, 0, 0, "", 5, 0, 0);
+  AddMenuItem(Format("$TxtMsg$", GetName(caller), szText), "",
+              NONE, caller, 0, 0, "", 512, 0, 0);
+  fNewInfo = false;
+  SetAction("Still");
+}
+
+public func SetNewInfo(bool fSet, string sText)
+{
+  if(sText)
+    szText = sText;
+  if(fSet)
+  {
+    SetAction("Blink");
+    fNewInfo = true;
+  }
+  else
+  {
+    SetAction("Still");
+    fNewInfo = false;
+  }
+}
