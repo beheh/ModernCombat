000b9e1d32402ad2f55dacc29cb2b4b8eecd0d43
Michael14 <michael@missile.beheh.de>
2010-07-04 10:27:55 +0000

    + Zusätzlicher Raucheffekt bei Feuer von Artilleriebatterien. + Büsche besitzen nun eigenes Damage und können zerstört werden (sie sind nun keine "Bäume" mehr, also Pseudogewächs wie z.B. Gras)

--- ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/ArtilleryBattery.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/ArtilleryBattery.c4d/Script.c
@@ -167,6 +167,8 @@ public func Shoot()
   CreateParticle("LightFlash",iX,iY,0,0,500,RGBa(255,255,255,32));
   for(var i = 0; i < 14; i++)
     CreateParticle("Smoke",iX,iY+RandomX(-20,20),0,0,RandomX(50,100),RGB(96,96,96));
+
+  if(GetEffectData(EFSM_ExplosionEffects) > 0) CastParticles("GunSmoke",4,10,0,0,500,100);
   MuzzleFlash(RandomX(30,75),this(),iX,iY,GetR(pCannon));
 }
 
