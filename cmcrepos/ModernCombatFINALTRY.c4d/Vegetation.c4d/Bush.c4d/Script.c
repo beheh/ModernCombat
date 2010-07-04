82fd04d5588da9225a0b2798d88947ed00c383b4
Michael14 <michael@missile.beheh.de>
2010-07-04 10:27:55 +0000

    + Zusätzlicher Raucheffekt bei Feuer von Artilleriebatterien. + Büsche besitzen nun eigenes Damage und können zerstört werden (sie sind nun keine "Bäume" mehr, also Pseudogewächs wie z.B. Gras)

--- cmcrepos/ModernCombatFINALTRY.c4d/Vegetation.c4d/Bush.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Vegetation.c4d/Bush.c4d/Script.c
@@ -2,4 +2,30 @@
 
 #strict
 
-#include TREE
+
+/* Initialisierung */
+
+protected func Initialize() 
+{
+  // Zufällige Richtung
+  if (Random(2)) SetDir(DIR_Right);
+
+  SetAction("Breeze");
+}
+
+/* Zerstörung */
+
+func Damage()
+{
+  if(GetDamage() > 100)
+  {
+   //Effekte
+   if(GetEffectData(EFSM_ExplosionEffects) > 0) CastParticles("Smoke3",12,10,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+   CastParticles("GrassBlade",10,80,0,0,200,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+   CastParticles("GrassBlade",20,100,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+   Sound("TreeDown*");
+
+   //Verschwinden
+   RemoveObject();
+  }
+}
