89c6d04ec1b8936418e61cc19991fbf9cb24057d
Michael14 <michael@missile.beheh.de>
2010-08-23 18:22:10 +0000

    + Achievement Point Hunter hinzugefügt.

---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/RewardSystem.c4d/Achievements.c4d/13PointHunter.c4d/Script.c
@@ -0,0 +1,13 @@
+/*-- Point Hunter --*/
+
+#strict 2
+#include ACHH
+
+public func GetSavingSlot()
+{
+  return 13;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore() {
+	return 2000; //Benötigte Punktzahl einfügen
+}
