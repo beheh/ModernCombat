0d24cb60cbb52074db1074ff9e481ced022a936b
Michael14 <michael@missile.beheh.de>
2010-08-24 16:51:37 +0000

    + Achievement "Eat this!".

---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/RewardSystem.c4d/Achievements.c4d/14EatThis.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- Eat this! --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 14;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 3;	//Benötigte Punktzahl einfügen
+}
