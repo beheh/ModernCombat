fd0e47d6480bc0ef5660bb6da793e6e3c3673ff1
Limeox <limeox@missile.beheh.de>
2010-08-24 15:43:50 +0000

    * Globales Zeug von HTF nach TEAM gepackt

--- ModernCombatFINALTRY.c4d/System.c4g/Appendto-Team.c
+++ ModernCombatFINALTRY.c4d/System.c4g/Appendto-Team.c
@@ -30,3 +30,34 @@ private func UpdateHUD()
   if(FindObject(GOAL)) return;
   return inherited(...);
 }
+
+//Globales für Ziele und Teams
+
+global func EliminateTeam(int iTeam) {
+  for(var i; i < GetPlayerCount(); i++)
+    if(GetPlayerTeam(GetPlayerByIndex(i)) == iTeam)
+      EliminatePlayer(GetPlayerByIndex(i));
+}
+
+global func GetTaggedTeamName(int iTeam) {
+  if (!GetTeamName(iTeam)) return;
+  return Format("<c %x>%s</c>", GetTeamColor(iTeam), GetTeamName(iTeam));
+}
+
+global func GetActiveTeamCount() {
+  var aTeams = [];
+  for (var i; i < GetPlayerCount(); i++)
+    if (GetPlayerName(GetPlayerByIndex(i)))
+      aTeams[GetPlayerTeam(GetPlayerByIndex(i))] = 1;
+  i = 0;
+  for (var item in aTeams)
+    i += item;
+  return i;
+}
+
+global func GetTeamPlayerCount(int iTeam) {
+  for (var i, count; i < GetPlayerCount(); i++)
+    if (GetPlayerTeam(GetPlayerByIndex(i)) == iTeam && GetPlayerName(GetPlayerByIndex(i)))
+	  count++;
+  return count;
+}
