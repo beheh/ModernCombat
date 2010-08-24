d95b9f8987c5ad1bed4f991cab5f63eeb51506a7
Ace <ace@missile.beheh.de>
2010-08-24 08:46:27 +0000

    ! Uagh. Waffen nachgeliefert.

---
+++ ModernCombatFINALTRY.c4d/Vehicles.c4d/Black_Hawk.c4d/Gun-Attachment.c4d/MachineGun.c4d/Script.c
@@ -0,0 +1,52 @@
+/*-- Maschinengewehr --*/
+
+#strict 2
+#include WEPN
+
+public func HandSize()		{return 1000;}
+public func HandX()		{return 5000;}
+public func HandY()		{return 800;}
+public func NoWeaponChoice()	{return true;}
+
+/* Kugeln */
+
+public func FMData1(int data)
+{
+  if(data == FM_Name)		return "$Auto$";
+    
+  if(data == FM_AmmoID)		return STAM;
+  if(data == FM_AmmoLoad)	return 150;
+  
+  if(data == FM_Reload)		return 340;
+  if(data == FM_Recharge)	return 4;
+
+  if(data == FM_Auto)		return true;
+  
+  if(data == FM_Damage)		return 8;
+
+  return Default(data);
+}
+
+/* Kugeln - Schuss */
+
+public func Fire1()
+{
+  var user = GetUser();
+  var dir = GetDir(user)*2-1;
+  var angle = user->AimAngle(10,0,true);
+  var x,y;
+  user->WeaponEnd(x,y);
+  var ammo = SALaunchBullet(x,y,GetController(user),angle+RandomX(-2,+2),250,750,GetFMData(FM_Damage));
+  Sound("MNGN_Fire.ogg", 0, ammo);
+
+  // Effekte
+  SAMuzzleFlash(RandomX(35,50),user,x,y,angle);
+  SABulletCasing(x/3,y/3,-dir*14*(Random(1)+1),-(13+Random(2)),5);
+}
+
+/* Allgemein */
+
+func OnReload()
+{
+  Sound("MNGN_Reload.ogg");
+}
