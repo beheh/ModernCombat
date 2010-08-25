5b511c82157d3bd8007249c2533ac243a4e9abe3
Michael14 <michael@missile.beheh.de>
2010-08-25 13:48:36 +0000

    * Helikopterwracks faden jetzt aus.

--- cmcrepos/ModernCombatFINALTRY.c4d/Vehicles.c4d/Black_Hawk.c4d/Wreck.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Vehicles.c4d/Black_Hawk.c4d/Wreck.c4d/Script.c
@@ -2,6 +2,8 @@
 
 #strict
 
+local damaged;
+
 
 /* Initalisierung */
 
@@ -27,6 +29,8 @@ protected func Hit()
 
 func Damage(int iChange, int iPlr)
 {
+  if(damaged) return();
+
   SetController(iPlr);
   if(GetDamage() >= 60)
    DestroyWreck();
@@ -36,14 +40,22 @@ func Damage(int iChange, int iPlr)
 
 func DestroyWreck()
 {
+  if(damaged) return();
+  damaged = true;
+
   //Effekte
   CastParticles("GunSmoke",15,35,-20,0,300,500);
   CastParticles("GunSmoke",15,35,20,0,300,500);
   if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("MetalSplinter",8,200,0,0,100,50,RGB(40,20,20));
   Sound("C4EX_Detonation*.ogg", false, this());
   Sound("StructuralDamage*.ogg", false, this());
-  Explode(30);
-  return(RemoveObject());
+
+  //Explosion
+  FakeExplode(40);
+
+  //Löschen und ausblenden
+  Extinguish();
+  FadeOut();
 }
 
 func BlastPar(int X, int Y, int Size, int Color)
