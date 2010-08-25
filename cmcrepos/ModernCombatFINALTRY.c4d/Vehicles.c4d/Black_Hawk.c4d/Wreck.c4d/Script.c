7f30462dab0a1a5ebd04a553970ca7f87246ceb7
Michael14 <michael@missile.beheh.de>
2010-08-25 12:00:43 +0000

    + Weitere Blackhawk Sounds. * Wrack zerstört sich bei Schaden.

--- cmcrepos/ModernCombatFINALTRY.c4d/Vehicles.c4d/Black_Hawk.c4d/Wreck.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Vehicles.c4d/Black_Hawk.c4d/Wreck.c4d/Script.c
@@ -23,6 +23,15 @@ protected func Hit()
   Sound("Collision*");
 }
 
+/* Schaden */
+
+func Damage(int iChange, int iPlr)
+{
+  SetController(iPlr);
+  if(GetDamage() >= 60)
+   DestroyWreck();
+}
+
 /* Zerstörung */
 
 func DestroyWreck()
@@ -32,7 +41,7 @@ func DestroyWreck()
   CastParticles("GunSmoke",15,35,20,0,300,500);
   if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("MetalSplinter",8,200,0,0,100,50,RGB(40,20,20));
   Sound("C4EX_Detonation*.ogg", false, this());
-
+  Sound("StructuralDamage*.ogg", false, this());
   Explode(30);
   return(RemoveObject());
 }
