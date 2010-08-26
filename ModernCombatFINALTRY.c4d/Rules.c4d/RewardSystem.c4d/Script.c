02f8624a209426c1e5d6c1c40300a3e3c8ddb43b
Benedict Etzel <developer@beheh.de>
2010-08-26 22:22:23 +0000

    + Rewardsystem zeigt Error bei Punktkategorie-Fail

--- ModernCombatFINALTRY.c4d/Rules.c4d/RewardSystem.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Rules.c4d/RewardSystem.c4d/Script.c
@@ -128,7 +128,8 @@ global func DoPlayerPoints(int iPoints, int iType, int iPlr, object pClonk, id i
 	var db = FindObject2(Find_ID(RWDS));
   if(!db) return;
   if(!iPoints) return;
-  if(iType != RWDS_BattlePoints && iType != RWDS_TeamPoints && iType != RWDS_MinusPoints) return;
+  if(iType != RWDS_BattlePoints && iType != RWDS_TeamPoints && iType != RWDS_MinusPoints)
+  	return ErrorLog("Invalid points type for %d points at %v", iPoints, pClonk);
   if(db->SetPlayerData(db->GetPlayerPoints(iType, iPlr)+iPoints, iType, iPlr)) {
 		DoAchievementProgress(iPoints, AC13, iPlr);
 		if(pClonk) {
