08b90ffcef67c05b8cb1fa0aacfa15777c302a4e
Michael14 <michael@missile.beheh.de>
2010-08-31 10:24:29 +0000

    * 2 neue Achievements eingeführt ("Doom Boom" und "In your Face").

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/RewardSystem.c4d/Achievements.c4d/16InTheFace.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- In the Face --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 16;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
