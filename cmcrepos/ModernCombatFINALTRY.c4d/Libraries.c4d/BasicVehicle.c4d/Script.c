bb68cb40acc998aa6e57bf96a7d4854c5a316064
Michael14 <michael@missile.beheh.de>
2010-09-06 12:14:38 +0000

    * Grundfahrzeugscript ist Opfer von Micha-Reinigungsattacken geworden.

--- cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/BasicVehicle.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/BasicVehicle.c4d/Script.c
@@ -6,10 +6,10 @@ local fDestroyed;
 local iLastAttacker;
 local aDealers;
 
-public func OnDamage()		{}		//Beim Erhalten von Schaden
-public func OnDestruction()		{}		//Bei der Zerstörung des Fahzeugs
-public func MaxDamage()			{return 100;}		//Maximalschaden
-public func BonusPointCondition()			{return Hostile(GetLastAttacker(),GetController());}			//Ob bei der Zerstörung Punkte vergeben werden
+public func OnDamage()			{}							//Beim Erhalten von Schaden
+public func OnDestruction()		{}							//Bei der Zerstörung des Fahzeugs
+public func MaxDamage()			{return 100;}						//Maximalschaden
+public func BonusPointCondition()	{return Hostile(GetLastAttacker(),GetController());}			//Ob bei der Zerstörung Punkte vergeben werden
 
 public func GetLastAttacker()		{return iLastAttacker;}
 public func IsDestroyed()		{return fDestroyed;}
@@ -20,44 +20,46 @@ public func IsCMCVehicle()		{return true;}
 
 public func Initialize()
 {
-	fDestroyed = false;
-	iLastAttacker = -1;
-	aDealers = CreateArray();
-	return true;
+  fDestroyed = false;
+  iLastAttacker = -1;
+  aDealers = CreateArray();
+  return true;
 }
 
 /* Schaden */
 
 public func Damage()
 {
-	if(GetDamage() > MaxDamage() && !IsDestroyed())
-	{
-		Destroyed();
-	}
-	OnDamage();
+  if(GetDamage() > MaxDamage() && !IsDestroyed())
+  {
+    Destroyed();
+  }
+  OnDamage();
 }
 
 public func OnDmg(int iDmg, int iType)
 {
-  return 50;	//Default
+  return 50;	//Standardwert
 }
 
 public func OnHit(int iDmg, int iType, object pBy)
 {
-	var iPlr = GetController(pBy);
-	if(!IsDestroyed())
-	  iLastAttacker = iPlr;
-	if(!aDealers)
-		aDealers = CreateArray();
-	if(Hostile(iPlr, GetController())) {
-		if(!aDealers[iPlr]) aDealers[iPlr] = 0;
-		aDealers[iPlr] += iDmg;
-		while(aDealers[iPlr] >= 50) {
-			DoPlayerPoints(BonusPoints("VehicleAssist"), RWDS_BattlePoints, iPlr, pBy, IC18);
-			aDealers[iPlr] -= 50;
-		}
-	}
-	return true;
+  var iPlr = GetController(pBy);
+  if(!IsDestroyed())
+    iLastAttacker = iPlr;
+  if(!aDealers)
+    aDealers = CreateArray();
+    if(Hostile(iPlr, GetController()))
+    {
+      if(!aDealers[iPlr]) aDealers[iPlr] = 0;
+        aDealers[iPlr] += iDmg;
+        while(aDealers[iPlr] >= 50)
+        {
+          DoPlayerPoints(BonusPoints("VehicleAssist"), RWDS_BattlePoints, iPlr, pBy, IC18);
+          aDealers[iPlr] -= 50;
+        }
+    }
+  return true;
 }
 
 /* Zerstörung */
