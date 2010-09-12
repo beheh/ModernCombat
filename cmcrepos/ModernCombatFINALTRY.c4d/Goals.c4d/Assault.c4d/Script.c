2541bc2584fda0fd2b754fd7e68a0f0cd1c1a8e1
Limeox <limeox@missile.beheh.de>
2010-09-12 15:42:46 +0000

    * Assault

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/Assault.c4d/Script.c
@@ -0,0 +1,276 @@
+/*-- Assault --*/
+
+#strict 2
+#include CASS
+
+local iDefender;		//Verteidiger-Team
+local iTickets;			//Tickets für die Angreifer
+local iStartTickets;
+local aSpawns;			//Spawnpunkte
+
+/* Initialisierung */
+
+protected func Initialize()
+{
+  iDefender = -1;
+  iStartTickets = 10;
+  aSpawns = [[],[]];
+  return _inherited(...);
+}
+
+public func IsConfigurable()		{return true;}
+
+public func ConfigMenu(object pCaller)
+{
+  OpenGoalMenu();
+  return 1;
+}
+
+private func ConfigFinished()
+{
+  var chos = FindObject(CHOS);
+  if(chos)
+    chos->OpenMenu();
+}
+
+private func OpenGoalMenu(id dummy, int iSelection)
+{
+  var pClonk = GetCursor();
+  CreateMenu(GetID(),pClonk,0,0,0,0,1);
+
+  AddMenuItem(" ", "OpenGoalMenu", GetID(), pClonk, iStartTickets, 0, " ");
+  AddMenuItem("$MoreTickets$", "ChangeTickets", CHOS, pClonk, 0, +1, "$MoreTickets$",2,1);
+  AddMenuItem("$LessTickets$", "ChangeTickets", CHOS, pClonk, 0, -1, "$LessTickets$",2,2);
+  AddMenuItem("$Finished$", "ConfigFinished", CHOS, pClonk,0,0,"$Finished$",2,3);
+
+  SelectMenuItem(iSelection, pClonk);
+}
+
+protected func ChangeTickets(id dummy, int iChange)
+{
+  //Stand verändern
+  iStartTickets = BoundBy(iStartTickets+iChange,3,30);
+  //Sound
+  Sound("Grab", 1,0,0,1);
+  //Menü wieder öffnen
+  var iSel = 1;
+  if(iChange == -1) iSel = 2;
+  OpenGoalMenu(0, iSel);
+}
+
+public func ChooserFinished()
+{
+  iTickets = iStartTickets;
+  return _inherited(...);
+}
+
+/* Zeug */
+
+static const GASS_Spawn_Def = 0;
+static const GASS_Spawn_Att = 1;
+
+public func AddAssaultTarget(id idTarget, int iX, int iY, int iMaxDamage, int iTeam, string szName, int iIndex, array aSpawn, bool fNoBar)
+{
+  if (_inherited(idTarget, iX, iY, iMaxDamage, iTeam, szName, iIndex, aSpawns, fNoBar))
+  {
+    //Team setzen
+    if (iTeam && iDefender == -1)
+	  iDefender = iTeam;
+	//Spawnpunkte anders behandeln
+	aSpawns[iIndex] = aSpawn;
+	return true;
+  }
+}
+
+public func ReportAssaultTargetDestruction(object pTarget, int iTeam)
+{
+  if (GetIndexOf(pTarget, aTargets[iTeam]) == -1)
+    return;
+
+  _inherited(...);
+
+  //Und gleich mal bekanntgeben
+  EventInfo4K(0, Format("$TargetDestruction$", GetTeamColor(iTeam), GetName(pTarget)), GBAS, 0, 0, 0, "Info4.ogg");
+  GameCall("OnAssaultTargetDestruction", pTarget, iTeam, FindInArray4K(aTargets[iTeam], pTarget));
+  if (pTarget)
+    Explode(50, pTarget);
+
+  //Verteidiger haben keine Ziele mehr
+  if (!ObjectCount2(Find_InArray(aTargets[iTeam])))
+    for (var i = 0; i < GetPlayerCount(); i++)
+	  if (GetPlayerTeam(GetPlayerByIndex(i)) == iTeam)
+	    EventInfo4K(GetPlayerByIndex(i)+1, "$NoTargets$", GASS, 0, 0, 0, "Alarm.ogg");
+
+  //Tickets resetten
+  iTickets = iStartTickets;
+}
+
+public func TeamGetScore(int iTeam)
+{
+  if (iTeam == iDefender)
+    return ObjectCount2(Find_InArray(aTargets[iDefender]));
+  return iTickets;
+}
+
+/* Relaunch */
+
+public func OnClassSelection(object pCrew)
+{
+  RelaunchPlayer(GetOwner(pCrew), 0, -2);
+}
+
+public func RelaunchPlayer(int iPlr, pClonk, int iKiller)
+{
+  if (iKiller != -2)
+  {
+    aDeath[iPlr]++;
+    if (iKiller != -1 && GetPlayerTeam(iPlr) != GetPlayerTeam(iKiller))
+      aKill[iKiller]++;
+	Money(iPlr, pClonk, iKiller);
+  }
+
+  var index = GASS_Spawn_Att;
+  //Verteidiger?
+  if (GetPlayerTeam(iPlr) == iDefender)
+    index = GASS_Spawn_Def;
+
+  //Spieler darf gar nicht mehr joinen?
+  if (index == GASS_Spawn_Att)
+  {
+    //Angreifer: Keine Tickets?
+	if (!iTickets)
+	  return ScheduleCall(this, "WaitForJoin", 5, 0, iPlr);
+  }
+  else
+    //Verteidiger: Keine Ziele?
+	if (!ObjectCount2(Find_InArray(aTargets[iDefender])))
+	  return EliminatePlayer(iPlr);
+
+  //Kein Verteidiger? Ticket-Abzug
+  if (GetPlayerTeam(iPlr) != iDefender && iKiller != -2)
+	iTickets = Max(iTickets-1);	  
+	
+  //Clonk wegstecken
+  var pCrew = GetCrew(iPlr);
+  if (!pCrew)
+    return;
+  var cont = Contained(pCrew);
+  var tim = CreateObject(TIM2, 0, 0, -1);
+  Enter(tim, pCrew);
+  if (cont)
+    RemoveObject(cont);
+
+  //Spawnpunkt raussuchen
+  if (ObjectCount2(Find_InArray(aTargets[iDefender])))
+  {
+    for (var target_index; target_index < GetLength(aTargets[iDefender]); target_index++)
+      if (aTargets[iDefender][target_index])
+	    break;
+  }
+  else
+    var target_index = GetLength(aTargets[iDefender])-1;
+
+  var rand = Random(GetLength(aSpawns[target_index][index]));
+  SetPosition(aSpawns[target_index][index][rand][0], aSpawns[target_index][index][rand][1]-10, tim);
+  tim->Spawn();
+}
+
+protected func WaitForJoin(int iPlr)
+{
+  //Es gibt wieder Tickets!
+  if (iTickets)
+    return RelaunchPlayer(iPlr, GetCrew(iPlr), -2);
+
+  //Alle anderen Angreifer sind tot -> verloren!
+  var alive;
+  for (var obj in FindObjects(Find_Func("IsClonk")))
+  {
+    var iPlr = GetOwner(obj);
+    if (GetPlayerTeam(iPlr) == iDefender)
+	  continue;
+	if (GetAlive(obj) && GetID(Contained(obj)) != TIM1 && GetID(Contained(obj)) != TIM2)
+	  alive = true;
+  }
+
+  if (alive)
+    return ScheduleCall(this, "WaitForJoin", 5, 0, iPlr);
+  EliminatePlayer(iPlr);
+}
+
+/* Scoreboard */
+
+static const GASS_Icon = 0;
+static const GASS_Name = 1;
+static const GASS_Count = 2;
+
+public func UpdateScoreboard()
+{
+  //Wird noch eingestellt
+  if (FindObject(CHOS))
+    return;
+
+  //Erst das nächste Ziel raussuchen
+  var obj;
+  for (var index; index < GetLength(aTargets[iDefender]); index++)
+    if (obj = aTargets[iDefender][index])
+	  break;
+
+  //Titelzeile
+  SetScoreboardData(SBRD_Caption, SBRD_Caption, GetName());
+
+  //Zeile fürs Ziel
+  if (obj)
+  {
+    SetScoreboardData(0, GASS_Icon, Format("{{%i}}", obj->GetImitationID()));
+	SetScoreboardData(0, GASS_Name, GetName(obj));
+	var effect = GetEffect("IntAssaultTarget", obj);
+	var percent = 100-GetDamage(obj)*100/EffectVar(0, obj, effect);
+	SetScoreboardData(0, GASS_Count, Format("%d", percent));
+  }
+  else
+  { 
+    SetScoreboardData(0, GASS_Icon);
+    SetScoreboardData(0, GASS_Name);
+	SetScoreboardData(0, GASS_Count);
+  }
+
+  //Leerzeile
+  SetScoreboardData(1, 0, "<c ffffffff> </c>");
+
+  //Tickets
+  SetScoreboardData(2, GASS_Icon, "{{TIKT}}");
+  SetScoreboardData(2, GASS_Count, Format("%d", iTickets));
+}
+
+/* Ziel */
+
+protected func FxIntGoalTimer()
+{
+  UpdateScoreboard();
+  IsFulfilled();
+}
+
+local fulfilled;
+
+private func IsFulfilled()
+{
+  if (FindObject(CHOS))
+    return;
+  if (fulfilled)
+    return fulfilled;
+
+  //Nur noch ein Team übrig - Sieg!
+  if (GetActiveTeamCount() == 1)
+  {
+    //Spielende planen
+    Schedule("GameOver()", 150);
+
+    //Auswertung
+    RewardEvaluation();
+
+    RemoveAll(GOAL);
+    Sound("Cheer.ogg", true);
+    Message("@$TeamHasWon$", 0, GetTaggedTeamName(GetTeamByIndex()));
+    return fulfilled = true;
+  }
+}
---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/BasicAssault.c4d/Script.c
@@ -0,0 +1,198 @@
+/*-- Grund-Assault --*/
+
+#strict 2
+#include TEAM
+
+local aSpawn;		//Spawnpunkte
+local aTargets;		//Ziele
+
+/* Initialisierung */
+
+protected func Initialize()
+{
+  aTargets = [];
+  aKill = [];
+  aDeath = [];
+  aSpawn = [];
+}
+
+protected func ChooserFinished()
+{
+  AddEffect("IntGoal", this, 1, 5, this);
+
+  //Keine Klassenwahl? Alle relaunchen
+  if (!FindObject(MCSL))
+    for (var obj in FindObjects(Find_Func("IsClonk")))
+      ScheduleCall(this, "OnClassSelection", 1, 0, obj);
+}
+
+/* Globales Zeug */
+
+global func ReportAssaultTargetDestruction()
+{
+  var goal = FindObject2(Find_Func("IsCMCAssaultGoal"));
+  if (goal)
+    return goal->ReportAssaultTargetDestruction(...);
+}
+
+global func AddAssaultTarget()
+{
+  var goal = FindObject2(Find_Func("IsCMCAssaultGoal"));
+  if (goal)
+    return goal->AddAssaultTarget(...);
+}
+
+global func IsAssaultTarget()	{return GetEffect("IntAssaultTarget", this);}
+
+global func Find_InArray(array a)
+{
+  var end = [C4FO_Or];
+  for (var i; i < GetLength(a); i++)
+    if (GetType(a[i]) == C4V_C4Object)
+      end[i+1] = [C4FO_Not, [C4FO_Exclude, a[i]]];
+  return end;
+}
+
+/* Zeug */
+
+public func IsCMCAssaultGoal() {return GetID() != CASS;}
+
+public func AddAssaultTarget(id idTarget, int iX, int iY, int iMaxDamage, int iTeam, string szName, int iIndex, array aSpawns, bool fNoBar) {
+  //Grundobjekt erstellen
+  var fake = CreateObject(STCR, iX, iY+GetDefCoreVal("Offset", 0, idTarget, 1)+2, -1);
+  //Und Original imitieren
+  SetShape(GetDefCoreVal("Offset", 0, idTarget), GetDefCoreVal("Offset", 0, idTarget, 1), GetDefCoreVal("Width", 0, idTarget), GetDefCoreVal("Height", 0, idTarget), fake);
+  SetGraphics(0, fake, idTarget, 1, 1);
+  fake->SetImitationID(idTarget);
+  //Team setzen
+  fake->SetTeam(iTeam);
+  //Name
+  if (szName)
+    SetName(szName, fake);
+  else
+    SetName(GetName(0, idTarget), fake);
+  //Ins Array packen
+  if (!aTargets[iTeam])
+    aTargets[iTeam] = [];
+  aTargets[iTeam][iIndex] = fake;
+  //Relaunchpositionen
+  if (!aSpawn[iTeam])
+    aSpawn[iTeam] = [];
+  aSpawn[iTeam][iIndex] = aSpawns;
+  //Assault-Effekt
+  AddEffect("IntAssaultTarget", fake, 1, 1, this, 0, iMaxDamage, fNoBar, idTarget);
+  //Done.
+  return true;
+}
+
+public func ReportAssaultTargetDestruction(object pTarget, int iTeam)
+{
+  if (GetIndexOf(pTarget, aTargets[iTeam]) == -1)
+    return;
+
+  if (GetID(pTarget) == STCR)
+  {
+    var aDmg = pTarget->GetDamager();
+    var iPlr = aDmg[1];
+	aDmg = aDmg[0];
+	//Der letzte Killer bekommt 50 Punkte. -50 wenn er im selben Team war.
+	if (GetPlayerTeam(iPlr) == iTeam)
+    {
+	  DoPlayerPoints(-50, RWDS_MinusPoints, iPlr, GetCrew(iPlr), IC03);
+	  DoWealth(iPlr, -50);
+	}
+	else
+	{
+	  DoPlayerPoints(50, RWDS_TeamPoints, iPlr, GetCrew(iPlr), IC03);
+	  DoWealth(iPlr, 50);
+	}
+
+	//Array durchgehen. Jeder gegnerische Helfer bekommt 20 Punkte, jeder Teamabschuss -20
+	for (var i; i < GetLength(aDmg); i++)
+	  if (GetPlayerName(i) && i != iPlr && aDmg[i]) {
+        if (GetPlayerTeam(i) == iTeam) {
+		  DoPlayerPoints(-20, RWDS_MinusPoints, i, GetCrew(i), IC03);
+		  DoWealth(i, -20);
+		}
+		else
+                {
+		  DoPlayerPoints(20, RWDS_TeamPoints, i, GetCrew(i), IC03);
+		  DoWealth(i, 20);
+		}
+	  }
+  }
+
+  return true;
+}
+
+protected func Activate(int iPlr)
+{
+  return MessageWindow(GetDesc(), iPlr);
+}
+
+/* Assault-Effekt */
+
+//Effekt-Vars
+//0: Maximalschaden
+//1: Leiste oder nicht
+//2: Leiste
+//3: Aktuell angezeigter Schaden (für "flüssige" Leiste)
+//4: ID. Wichtig für FakeTarget
+
+protected func FxIntAssaultTargetStart(object pTarget, int iEffect, int iTemp, int iDmg, bool fNoBar, id idTarget)
+{
+  EffectVar(0, pTarget, iEffect) = iDmg;
+  EffectVar(1, pTarget, iEffect) = fNoBar;
+  EffectVar(4, pTarget, iEffect) = idTarget;
+}
+
+protected func FxIntAssaultTargetTimer(object pTarget, int iEffect)
+{
+  //Ziel will keine Leiste
+  if (EffectVar(1, pTarget, iEffect))
+    return;
+
+  //Erstmal die Leiste prüfen
+  if (!EffectVar(2, pTarget, iEffect))
+    EffectVar(2, pTarget, iEffect) = CreateObject(EBR2, GetX(pTarget), GetY(pTarget)+GetDefHeight(EffectVar(4, pTarget, iEffect))/2+10, -1);
+  var bar = EffectVar(2, pTarget, iEffect);
+
+  //Schaden
+  if (EffectVar(3, pTarget, iEffect) < GetDamage(pTarget))
+    EffectVar(3, pTarget, iEffect)++;
+  var dmg = EffectVar(3, pTarget, iEffect), maxdmg = EffectVar(0, pTarget, iEffect), team = pTarget->~GetTeam();
+
+  //Bei zu viel Schaden zerstören
+  if (GetDamage(pTarget) > maxdmg)
+    return ReportAssaultTargetDestruction(pTarget, team);
+
+  //Und die Leiste füllen...
+  SetPosition(GetX(pTarget), GetY(pTarget)+GetDefHeight(EffectVar(4, pTarget, iEffect))/2+10, bar);
+  SetGraphics("Row", bar, GetID(bar), 1, 1, 0, 4);
+  var permill = 1000*(maxdmg-dmg)/maxdmg;
+  SetObjDrawTransform(permill, 0, (permill-1000)*GetDefWidth(GetID(bar))/2, 0, 1000, 0, bar, 1);
+  SetClrModulation(GetTeamColor(team), bar, 1);
+}
+
+protected func FxIntAssaultTargetStop(object pTarget, int iEffect, int iCause, bool fTemp)
+{
+  if (!fTemp && EffectVar(2, pTarget, iEffect))
+    RemoveObject(EffectVar(2, pTarget, iEffect));
+}
+
+/* Relaunch */
+
+public func OnClassSelection() {}
+
+/* Ungenutzte Funktionen */
+
+private func InitScoreboard()			{}
+private func InitMultiplayerTeam(int iTeam)	{}
+private func RemoveMultiplayerTeam(int iTeam)	{}
+private func InitSingleplayerTeam(int iPlr)	{}
+private func RemoveSingleplayerTeam(int iPlr)	{}
+private func InitPlayer(int iPlr)		{}
+private func RemoveScoreboardPlayer(int iPlr)	{}
+public func WinScoreChange(int iNewScore)	{}
+private func SortTeamScoreboard()		{}
+private func TeamGetScore()	{}
