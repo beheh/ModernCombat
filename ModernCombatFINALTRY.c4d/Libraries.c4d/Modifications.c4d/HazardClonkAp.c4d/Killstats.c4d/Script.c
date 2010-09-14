41ea11b9de8dc3663c93708bee2b357fdd402bf9
Michael14 <michael@missile.beheh.de>
2010-09-14 13:40:10 +0000

    ! Symbole, Icons bzw. Logos nun mit eigener ID-Nennung "ICXX" (XX ist die jeweilige Zahl, z.B. 01 oder 23), identischer Namensgebung, keine Beschreibungen und weiterer Anpassungen. * Symbol Fist.c4d entpackt. * Alle Nutzer von Logos auf neue IDs ausgerichtet.

--- ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/HazardClonkAp.c4d/Killstats.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/HazardClonkAp.c4d/Killstats.c4d/Script.c
@@ -44,7 +44,7 @@ public func KMsg(int plr1, int plr2, object clonk, int plr3)
    if(type == DMG_Fire)
     typeicon = GSAM;
    else if(type == DMG_Melee)
-   	typeicon = ICFT;
+   	typeicon = IC04;
    else if(type == DMG_Explosion)
     typeicon = BOOM;
    else if(type == DMG_Energy)
