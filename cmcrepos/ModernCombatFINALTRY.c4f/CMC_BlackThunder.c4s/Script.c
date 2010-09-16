e7be7203a3367fbeb91343ee7a4253dd4849e555
Michael14 <michael@missile.beheh.de>
2010-09-16 14:10:15 +0000

    + Black Thunder eingeführt.

---
+++ cmcrepos/ModernCombatFINALTRY.c4f/CMC_BlackThunder.c4s/Script.c
@@ -0,0 +1,403 @@
+/*-- Black Thunder --*/
+
+#strict
+#include CSTD
+
+static aFlag,aSelfDefense,aArtillery;
+
+
+/* Initalisierung */
+
+func Initialize()
+{
+  //Starttitel und Musikliste zusammenstellen
+  SetPlayList("CMC_Base Groove.ogg;CMC_Firehawk.ogg;CMC_Friendly Unit.ogg;CMC_Getaway.ogg;CMC_Moving Squad.ogg;CMC_Offensive.ogg;CMC_Showtime.ogg;CMC_Slow Motion.ogg;CMC_Striking Force.ogg;CMC_No Good.ogg;CMC_Obsession.ogg");
+  Music("CMC_Getaway.ogg");
+  //Hintergrundbewegung
+  SetSkyParallax(0,15,15);
+  //Flaggen
+  aFlag = [];
+  //Selbstschussanlagen
+  aSelfDefense = [];
+  //Artillerie
+  aArtillery = [];
+  //Szenario einrichten
+  CreateFurniture();
+  //Equipment plazieren
+  CreateEquipment();
+  return(1);
+}
+
+/* Plazierungslisten */
+
+func CreateFurniture()
+{
+  var tmp;
+  Log("$CreatingFurniture$");
+  //Alle Objekte folglich von links nach rechts
+
+  //Leitern
+  CreateObject(LADR, 460, 540, -1)->Set(4);
+  CreateObject(LADR, 790, 750, -1)->Set(20);
+  CreateObject(LADR, 1250, 844, -1)->Set(27);
+
+  CreateObject(LADR, 2020, 850, -1)->Set(19);
+  CreateObject(LADR, 2220, 850, -1)->Set(19);
+
+  CreateObject(LADR, 2990, 844, -1)->Set(27);
+  CreateObject(LADR, 3450, 750, -1)->Set(20);
+  CreateObject(LADR, 3780, 540, -1)->Set(4);
+
+  //Bodenlucken
+  CreateObject(HA4K, 460, 503, -1);
+
+  CreateObject(HA4K, 1250, 623, -1);
+  CreateObject(HA4K, 1250, 673, -1);
+
+  CreateObject(HA4K, 2990, 623, -1);
+  CreateObject(HA4K, 2990, 673, -1);
+
+  CreateObject(HA4K, 3780, 503, -1);
+
+  //Stahltüren
+  CreateObject(STDR, 1125, 670, -1);
+  CreateObject(STDR, 1275, 670, -1);
+
+  CreateObject(STDR, 2965, 670, -1);
+  CreateObject(STDR, 3115, 670, -1);
+
+  //Sandsackbarrieren
+  CreateObject(SBBA, 360, 560, -1);
+  CreateObject(SBBA, 710, 760, -1);
+  CreateObject(SBBA, 780, 760, -1)->Right();
+
+  CreateObject(SBBA, 1100, 670, -1);
+  CreateObject(SBBA, 1100, 760, -1);
+  CreateObject(SBBA, 1350, 850, -1)->Right();
+  CreateObject(SBBA, 1460, 670, -1)->Right();
+
+  CreateObject(SBBA, 1960, 750, -1);
+  CreateObject(SBBA, 1960, 850, -1);
+  CreateObject(SBBA, 2280, 750, -1)->Right();
+  CreateObject(SBBA, 2280, 850, -1)->Right();
+
+  CreateObject(SBBA, 2780, 670, -1);
+  CreateObject(SBBA, 2890, 850, -1);
+  CreateObject(SBBA, 3140, 670, -1)->Right();
+  CreateObject(SBBA, 3140, 760, -1)->Right();
+
+  CreateObject(SBBA, 3460, 760, -1);
+  CreateObject(SBBA, 3530, 760, -1)->Right();
+  CreateObject(SBBA, 3880, 560, -1)->Right();
+
+  //Geländer
+  CreateObject(RAI3, 350, 560, -1)->SetRail([1,1]);
+  CreateObject(RAI1, 630, 590, -1)->SetRail([1,1,1,1,1,1,1,1]);
+  CreateObject(RAI3, 1090, 670, -1);
+  CreateObject(RAI1, 1125, 620, -1)->SetRail([1,1,1,1,1,1,1,1]);
+  CreateObject(RAI3, 1440, 670, -1);
+
+  CreateObject(RAI3, 2770, 670, -1);
+  CreateObject(RAI1, 2965, 620, -1)->SetRail([1,1,1,1,1,1,1,1]);
+  CreateObject(RAI3, 3120, 670, -1);
+  CreateObject(RAI1, 3460, 590, -1)->SetRail([1,1,1,1,1,1,1,1]);
+  CreateObject(RAI3, 3847, 560, -1)->SetRail([1,1]);
+
+  //Phosphorfässer
+  CreateObject(HBRL, 1385, 670, -1)->AutoRespawn();
+  CreateObject(HBRL, 2855, 670, -1)->AutoRespawn();
+
+  //Gasflaschen
+  CreateObject(GSBL, 720, 760, -1)->AutoRespawn();
+  CreateObject(GSBL, 3520, 760, -1)->AutoRespawn();
+
+  //Zäune
+  CreateObject(FENC, 1310, 670, -1);
+  CreateObject(FENC, 2930, 670, -1);
+
+  //Markierungsschilder
+  CreateObject(MSGN, 775, 760, -1);
+  CreateObject(MSGN, 1125, 760, -1);
+  CreateObject(MSGN, 2015, 850, -1);
+
+  CreateObject(MSGN, 2235, 850, -1);
+  CreateObject(MSGN, 3125, 760, -1);
+  CreateObject(MSGN, 3475, 760, -1);
+
+  //Orientierungslichter
+  CreateObject(OLGH, 700, 590, -1)->Set(4, 15, 1, 1, 30);
+  CreateObject(OLGH, 1200, 620, -1)->Set(4, 15, 1, 1, 30);
+
+  CreateObject(OLGH, 2120, 700, -1)->Set(2, 30, 1, 1, 30);
+
+  CreateObject(OLGH, 3040, 620, -1)->Set(4, 15, 1, 1, 30);
+  CreateObject(OLGH, 3540, 590, -1)->Set(4, 15, 1, 1, 30);
+
+  //Grenzen
+  CreateObject(BRDR, 0, 1100, -1)->Set(3);
+
+  //Verbundene Türen
+  var doorw = CreateObject(ROOM, 740, 760, -1);
+  CreateObject(ROOM, 1160, 760, -1)->Connect(doorw);
+
+  doorw = CreateObject(GAT1, 1320, 810, -1);
+  CreateObject(ROOM, 1980, 850, -1)->Connect(doorw);
+  doorw = CreateObject(GAT1, 2920, 810, -1);
+  CreateObject(ROOM, 2260, 850, -1)->Connect(doorw);
+
+  var doorw = CreateObject(ROOM, 3500, 760, -1);
+  CreateObject(ROOM, 3080, 760, -1)->Connect(doorw);
+
+  //Selbstschussanlagen
+  aSelfDefense[0] = CreateObject(SEGU, 420, 472, -1);
+    aSelfDefense[0]->SetR(180);
+    aSelfDefense[0]->Arm(MISA);
+    aSelfDefense[0]->TurnOn();
+    aSelfDefense[0]->SetAutoRepair(1000);
+
+  aSelfDefense[1] = CreateObject(SEGU, 1410, 612, -1);
+    aSelfDefense[1]->SetR(180);
+    aSelfDefense[1]->Arm(MISA);
+    aSelfDefense[1]->TurnOn();
+    aSelfDefense[1]->SetAutoRepair(1000);
+
+  aSelfDefense[2] = CreateObject(SEGU, 2830, 612, -1);
+    aSelfDefense[2]->SetR(180);
+    aSelfDefense[2]->Arm(MISA);
+    aSelfDefense[2]->TurnOn();
+    aSelfDefense[2]->SetAutoRepair(1000);
+
+  aSelfDefense[3] = CreateObject(SEGU, 3820, 472, -1);
+    aSelfDefense[3]->SetR(180);
+    aSelfDefense[3]->Arm(MISA);
+    aSelfDefense[3]->TurnOn();
+    aSelfDefense[3]->SetAutoRepair(1000);
+
+  //Sounds
+
+  //Wind
+  CreateObject(SE4K, 930, 220, -1)->Set("WindSound*.ogg",775,250);
+  CreateObject(SE4K, 2120, 220, -1)->Set("WindSound*.ogg",775,250);
+  CreateObject(SE4K, 3300, 220, -1)->Set("WindSound*.ogg",775,250);
+
+  //Hallen
+  CreateObject(SE4K, 620, 520, -1)->Set("Interior*.ogg",665,105);
+  CreateObject(SE4K, 1270, 600, -1)->Set("Interior*.ogg",670,105);
+  CreateObject(SE4K, 2970, 600, -1)->Set("Interior*.ogg",670,105);
+  CreateObject(SE4K, 3620, 520, -1)->Set("Interior*.ogg",665,105);
+
+  //Jungel
+  CreateObject(SE4K, 200, 1100, -1)->Set("Jungle*.ogg",800,200);
+  CreateObject(SE4K, 800, 1100, -1)->Set("Jungle*.ogg",300,200);
+  CreateObject(SE4K, 1400, 1100, -1)->Set("Jungle*.ogg",800,200);
+  CreateObject(SE4K, 2000, 1100, -1)->Set("Jungle*.ogg",300,200);
+  CreateObject(SE4K, 2600, 1100, -1)->Set("Jungle*.ogg",800,200);
+  CreateObject(SE4K, 3500, 1100, -1)->Set("Jungle*.ogg",300,200);
+  CreateObject(SE4K, 4100, 1100, -1)->Set("Jungle*.ogg",800,200);
+
+}
+
+func CreateEquipment()
+{
+  Log("$CreatingEquipment$");
+  //Alle Objekte folglich von links oben nach rechts unten
+
+  //Munitionskisten (Kugeln)
+  var tmp = CreateObject (AMCT, 460, 560, -1);
+  tmp->Set(ABOX);
+  tmp->SetGraphics("Normal");
+
+  var tmp = CreateObject (AMCT, 1140, 670, -1);
+  tmp->Set(ABOX);
+  tmp->SetGraphics("Normal");
+
+  var tmp = CreateObject (AMCT, 3100, 670, -1);
+  tmp->Set(ABOX);
+  tmp->SetGraphics("Normal");
+
+  var tmp = CreateObject (AMCT, 3780, 560, -1);
+  tmp->Set(ABOX);
+  tmp->SetGraphics("Normal");
+
+  //Munitionskiste (Granaten)
+  var tmp = CreateObject (AMCT, 490, 560, -1);
+  tmp->Set(GBOX);
+  tmp->SetGraphics("Normal");
+
+  var tmp = CreateObject (AMCT, 1180, 670, -1);
+  tmp->Set(GBOX);
+  tmp->SetGraphics("Normal");
+
+  var tmp = CreateObject (AMCT, 3060, 670, -1);
+  tmp->Set(GBOX);
+  tmp->SetGraphics("Normal");
+
+  var tmp = CreateObject (AMCT, 3750, 560, -1);
+  tmp->Set(GBOX);
+  tmp->SetGraphics("Normal");
+
+  //Raketen
+  PlaceSpawnpoint(MIAP, 420, 645);
+  PlaceSpawnpoint(MIAP, 1360, 665);
+
+  PlaceSpawnpoint(MIAP, 2880, 665);
+  PlaceSpawnpoint(MIAP, 3820, 645);
+
+  //Artilleriebatterie
+  aArtillery[0] = CreateObject(ATBY,2120,690,-1);
+
+  //Blackhawks
+  SetupVehicleSpawn([BKHK],DIR_Right,CreateObject(VSPW,700,560,-1),60*21,300);
+  SetupVehicleSpawn([BKHK],DIR_Right,CreateObject(VSPW,1185,590,-1),60*21,300);
+
+  SetupVehicleSpawn([BKHK],DIR_Left,CreateObject(VSPW,3055,590,-1),60*21,300);
+  SetupVehicleSpawn([BKHK],DIR_Left,CreateObject(VSPW,3540,560,-1),60*21,300);
+}
+
+/* Regelwähler */
+
+public func ChooserFinished()
+{
+  inherited();
+
+  //Teams abfragen
+  var aTeams = [false,false,false,false,false];
+  for(var i = 0; i < GetPlayerCount(); i++)
+    aTeams[GetPlayerTeam(GetPlayerByIndex(i))] = true;
+
+  //HTF-Spielziel
+  if (FindObject(GHTF))
+  {
+   var flag = CreateObject(OFPL, 2120, 690, -1);
+   flag->~Set("$Flag5$");
+
+   //Artillerie entfernen
+   RemoveObject(aArtillery[0]);
+
+   //Zusätzliche Munition
+   if(!FindObject(NOAM))
+   {
+    //Projektilmunition
+    PlaceSpawnpoint(ABOX, 2010, 845);
+
+    //Raketen
+    PlaceSpawnpoint(MIAP, 2230, 845);
+   }
+  }
+
+  //Base Assault-Spielziel
+  if(FindObject(GBAS))
+  {
+   //SSA Besitzer setzen
+   if(aTeams[1] == true)
+   {
+    aSelfDefense[0]->SetTeam(1);
+    aSelfDefense[1]->SetTeam(1);
+   }
+   if(aTeams[2] == true)
+   {
+    aSelfDefense[2]->SetTeam(2);
+    aSelfDefense[3]->SetTeam(2);
+   }
+
+   //Zielobjekte
+   AddAssaultTarget(SADH, 485, 500, 400, 1, "$Flag1$", 0, [[480, 630], [510, 720], [585, 680]]);
+   AddAssaultTarget(CMSN, 1330, 670, 300, 1, "$Flag2$", 1, [[1160, 660], [1300, 840], [1455, 660]]);
+   AddAssaultTarget(SADH, 3755, 500, 400, 2, "$Flag4$", 2, [[3760, 630], [3730, 720], [3655, 680]]);
+   AddAssaultTarget(CMSN, 2910, 670, 300, 2, "$Flag3$", 3, [[3080, 660], [2940, 840], [2785, 660]]);
+  }
+
+  //OP-Spielziel
+  if(FindObject(GOCC))
+  {
+   //SSA Besitzer setzen
+   if(aTeams[1] == true)
+   {
+    aSelfDefense[0]->SetTeam(1);
+    aSelfDefense[1]->SetTeam(1);
+   }
+   if(aTeams[2] == true)
+   {
+    aSelfDefense[2]->SetTeam(2);
+    aSelfDefense[3]->SetTeam(2);
+   }
+
+   //Grenze
+   CreateObject(BRDR, 2010, 0, -1)->Set(1);
+
+   //Flaggen
+   aFlag[0] = CreateObject(OFPL,630,590,NO_OWNER);
+   aFlag[0] -> AddSpawnPoint(460,740);
+   aFlag[0] -> AddSpawnPoint(580,680);
+   aFlag[0] -> AddSpawnPoint(380,550);
+   if(aTeams[1] == true)
+   {
+    aFlag[0]->Set("$Flag1$",100,2);
+    aFlag[0]->Capture(1,1);
+   }
+   else
+   {
+    aFlag[0]->Set("$Flag1$",0,2);
+   }
+
+   aFlag[1] = CreateObject(OFPL,1330,670,NO_OWNER);
+   aFlag[1] -> AddSpawnPoint(1190,750);
+   aFlag[1] -> AddSpawnPoint(1300,840);
+   aFlag[1] -> AddSpawnPoint(1130,610);
+   aFlag[1]->Set("$Flag2$",0,2);
+
+   aFlag[2] = CreateObject(OFPL,2910,670,NO_OWNER);
+   aFlag[2] -> AddSpawnPoint(2950,840);
+   aFlag[2] -> AddSpawnPoint(3050,750);
+   aFlag[2] -> AddSpawnPoint(3110,610);
+   aFlag[2]->Set("$Flag3$",0,2);
+
+   aFlag[3] = CreateObject(OFPL,3610,590,NO_OWNER);
+   aFlag[3] -> AddSpawnPoint(3660,680);
+   aFlag[3] -> AddSpawnPoint(3780,740);
+   aFlag[3] -> AddSpawnPoint(3860,550);
+   if(aTeams[1] == true)
+   {
+    aFlag[3]->Set("$Flag4$",100,2);
+    aFlag[3]->Capture(2,1);
+   }
+   else
+   {
+    aFlag[3]->Set("$Flag4$",0,2);
+   }
+  }
+}
+
+/* Relaunch */
+
+public func RelaunchPosition(& iX, & iY, int iTeam)
+{
+  //DM/LMS/HTF-Spielziel
+  if(FindObject(GTDM) || FindObject(GLMS) || FindObject(GHTF))
+  {
+   if(iTeam == 1)
+   {
+    var rand = Random(3);
+    if(!rand)
+     { iX = 380; iY = 550; }
+    if(!--rand)
+     { iX = 475; iY = 630; }
+    if(!--rand)
+     { iX = 585; iY = 680; }
+   }
+   if(iTeam == 2)
+   {
+    var rand = Random(2);
+    if(!rand)
+     { iX = 3655; iY = 680; }
+    if(!--rand)
+     { iX = 3765; iY = 630; }
+    if(!--rand)
+     { iX = 3860; iY = 550; }
+   }
+   return(1);
+  }
+
+  //Startsicht
+  iX = 2220; iY = 600;
+}
---
+++ cmcrepos/ModernCombatFINALTRY.c4f/CMC_BlackThunder.c4s/System.c4g/InvincibleTrees.c
@@ -0,0 +1,12 @@
+/*-- Baum --*/
+
+//Bäume sind unzerstörbar.
+
+#strict 2
+
+#appendto TREE
+
+
+public func ChopDown()		{}
+public func OnDmg()		{return 100;}
+protected func Incineration()	{Extinguish();}
