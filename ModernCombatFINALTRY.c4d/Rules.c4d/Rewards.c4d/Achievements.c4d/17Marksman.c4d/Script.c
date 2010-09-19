44addcc84e4092181e02c521901288f1654fa4f6
Michael14 <michael@missile.beheh.de>
2010-09-19 17:18:20 +0000

    + Neue Achievements: "Get to the Chopper"; "Marksman"; "Radio Warfare".

---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/17Marksman.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- Marksman --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 17;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 5;	//Benötigte Punktzahl einfügen
+}
---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/19RadioWarfare.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- Radio Warfare --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 19;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 20;	//Benötigte Punktzahl einfügen
+}
