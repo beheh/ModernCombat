7e2cde7697eb5233d02c9b7a39cec092b4b3ec55
Michael14 <michael@missile.beheh.de>
2010-10-01 20:06:08 +0000

    ! Symbole und weitere HUD-Einblendungen designtechnisch vereinheitlicht und Sichtbarkeit ingame verbessert. * Blendgranate blendet nun etwas stärker und verfügt über etwas vergrößerte Reichweite. * Blendgranaten rechnen nun einen bestimmten Wert hinzu wenn ein Ziel in die Detonation schaut, statt sie auf einen bestimmten Maximalwert anzuheben.

--- ModernCombatFINALTRY.c4d/Items.c4d/Grenades.c4d/StunGrenade.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Items.c4d/Grenades.c4d/StunGrenade.c4d/Script.c
@@ -6,6 +6,7 @@
 public func Color()		{return RGB(0,128,255);}
 public func ContainedDamage()	{return 20;}
 
+
 func HitSound()
 {
   Sound("SGRN_Hit*.ogg");
@@ -13,17 +14,24 @@ func HitSound()
 
 public func Fused()
 {
-  for(var obj in FindObjects(Find_OCF(OCF_CrewMember),Find_Distance(200)))
+  //Zu blendende Objekte suchen
+  for(var obj in FindObjects(Find_OCF(OCF_CrewMember),Find_Distance(250)))
   {
-    var intensity = ((200-ObjectDistance(this,obj))*470/200)/2;
+    //Intensität initialisieren
+    var intensity = ((250-ObjectDistance(this,obj))*470/250)/2;
 
     //Ziel ein Clonk?
     if(obj->~IsClonk())
     {
+      //Freie Sicht zum Clonk?
       if(!PathFree(GetX(),GetY(),GetX(obj),GetY(obj)-8))
         continue;
+
+      //Clonk "schaut" in Detonationsrichtung? Intensität hinzuaddieren
       if(((GetDir(obj) == DIR_Left) && (GetX() < GetX(obj))) || ((GetDir(obj) == DIR_Right) && (GetX(obj) > GetX(obj))))
-        intensity = Max(intensity,255);
+        intensity = Max(intensity+80);
+
+      //Clonk freundlich? Intensität halbieren
       if(!Hostile(GetOwner(obj), GetController()))
         intensity = intensity/2;
     }
@@ -50,11 +58,15 @@ public func Fused()
 
 public func FxIntFlashbangStart(object pTarget, int iEffectNumber, int iTemp, intensity)
 {
+  //Keine Intensität?
   if(!intensity) return -1;
+
   EffectVar(0,pTarget,iEffectNumber) = intensity;
 
+  //Blendung stark genug? Dann auch hörbar
   if(intensity > 38) Sound("STUN_Bang.ogg", false, pTarget, 100, GetOwner(pTarget)+1);
 
+  //Stärke berechnen und Blendung erstellen
   var a = BoundBy(255-(intensity*255/100),0,255);
   var flash = ScreenRGB(pTarget,RGBa(255,255,255,a), 0, 0, false, SR4K_LayerLight);
   if(!flash)
@@ -73,29 +85,33 @@ public func FxIntFlashbangTimer(object pTarget, int iEffectNumber, int iEffectTi
   EffectVar(1,pTarget,iEffectNumber)->SetAlpha(a);
   
   var val, num, pCursor, c, flag;
-  if(!Contained()) {
-		for(var i = 0; i < GetPlayerCount(); i++)
-		{
-		  pCursor = GetCursor(GetPlayerByIndex(i))->~GetRealCursor();
-		  if(!pCursor) pCursor = GetCursor(GetPlayerByIndex(i));
-		  if(!pCursor) continue;
-		  if(Contained(pCursor)) continue;
-		  num = GetEffect("IntFlashbang", pCursor);
-		  if(num) {
-		    val = 255-BoundBy((EffectVar(0,pCursor,num)-1)*255/100,0,255);
-		    if(255-a < val) val = 255-a;
-		  }
-		  else {
-		    val = 255-a;
-		  }
-		  flag = 0;
-		  if(c != 0) flag = MSG_Multiple;
-		  CustomMessage(Format("<c %x>{{ICN3}}</c>", RGBa(255,255,255,BoundBy(val, 1, 254))), pTarget, GetPlayerByIndex(i), 0, 0, 0, 0, 0, flag); 
-		  c++;
-		}
+  if(!Contained())
+  {
+    for(var i = 0; i < GetPlayerCount(); i++)
+    {
+      pCursor = GetCursor(GetPlayerByIndex(i))->~GetRealCursor();
+      if(!pCursor) pCursor = GetCursor(GetPlayerByIndex(i));
+      if(!pCursor) continue;
+      if(Contained(pCursor)) continue;
+      num = GetEffect("IntFlashbang", pCursor);
+      if(num)
+      {
+        val = 255-BoundBy((EffectVar(0,pCursor,num)-1)*255/100,0,255);
+        if(255-a < val) val = 255-a;
+      }
+      else
+      {
+        val = 255-a;
+      }
+      flag = 0;
+      if(c != 0) flag = MSG_Multiple;
+      CustomMessage(Format("<c %x>{{SM07}}</c>", RGBa(255,255,255,BoundBy(val, 1, 254))), pTarget, GetPlayerByIndex(i), 0, 0, 0, 0, 0, flag); 
+      c++;
+    }
   }
-  else {
-  	Message("@", pTarget); 
+  else
+  {
+    Message("@", pTarget); 
   }
 }
 
