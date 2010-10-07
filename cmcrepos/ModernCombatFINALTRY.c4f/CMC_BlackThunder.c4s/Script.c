ec5262f4ce8b304335d386e33d2ee1f3e0d7296a
Michael14 <michael@missile.beheh.de>
2010-10-07 16:57:01 +0000

    * Base Assault: Plazierungsscripts korrigiert.

--- cmcrepos/ModernCombatFINALTRY.c4f/CMC_BlackThunder.c4s/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4f/CMC_BlackThunder.c4s/Script.c
@@ -308,8 +308,9 @@ public func ChooserFinished()
    //Zielobjekte
    AddAssaultTarget(RADR, 485, 500, 400, 1, "$Flag1$", 0, [[480, 640], [510, 730], [585, 690]]);
    AddAssaultTarget(CMSN, 1330, 670, 300, 1, "$Flag2$", 1, [[1160, 670], [1300, 850], [1455, 670]]);
-   AddAssaultTarget(RADR, 3755, 500, 400, 2, "$Flag4$", 2, [[3760, 640], [3730, 730], [3655, 690]]);
-   AddAssaultTarget(CMSN, 2910, 670, 300, 2, "$Flag3$", 3, [[3080, 670], [2940, 850], [2785, 670]]);
+
+   AddAssaultTarget(RADR, 3755, 500, 400, 2, "$Flag4$", 0, [[3760, 640], [3730, 730], [3655, 690]]);
+   AddAssaultTarget(CMSN, 2910, 670, 300, 2, "$Flag3$", 1, [[3080, 670], [2940, 850], [2785, 670]]);
   }
 
   //OP-Spielziel
