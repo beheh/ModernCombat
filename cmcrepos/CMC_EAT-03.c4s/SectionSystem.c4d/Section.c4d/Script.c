cb9b6a94b994e1c655bfd2351e5d105e2838b948
Michael14 <michael@missile.beheh.de>
2010-11-03 16:01:18 +0000

    + Beta Grafiken eingefügt. - Musik entfernt. * Script gesäubert.

--- cmcrepos/CMC_EAT-03.c4s/SectionSystem.c4d/Section.c4d/Script.c
+++ cmcrepos/CMC_EAT-03.c4s/SectionSystem.c4d/Section.c4d/Script.c
@@ -6,49 +6,60 @@ static iScript;
 static iRespawnX;
 static iRespawnY;
 
-public func IsSection() {return true;} //Sektionscallback
+public func IsSection()	{return true;} //Sektionscallback
 
-public func Initialize() {
-	iScript = 0;
-	iRespawnX = 0;
-	iRespawnY = 0;
-	this->~SetupSection();
-	AddEffect("SectionCount", this, 25, 10, this);
+
+/* Initialisierung */
+
+public func Initialize()
+{
+  iScript = 0;
+  iRespawnX = 0;
+  iRespawnY = 0;
+  this->~SetupSection();
+  AddEffect("SectionCount", this, 25, 10, this);
 }
 
-global func FxSectionCountTimer(pTarget) {
-	var iScript = pTarget->GetScript();
-	if(iScript < 0) return;
-	pTarget->SectionGoto(iScript+1);
-	ObjectCall(pTarget, Format("Script%d", iScript));
-	return FX_OK;
+global func FxSectionCountTimer(pTarget)
+{
+  var iScript = pTarget->GetScript();
+  if(iScript < 0) return;
+  pTarget->SectionGoto(iScript+1);
+  ObjectCall(pTarget, Format("Script%d", iScript));
+  return FX_OK;
 }
 
-public func SectionGoto(int iTo) {
-	iScript = iTo;
-	return true;
+public func SectionGoto(int iTo)
+{
+  iScript = iTo;
+  return true;
 }
 
-public func GetScript() {
-	return iScript;
+public func GetScript()
+{
+  return iScript;
 }
 
-public func SectionStop() {
-	iScript = -1;
-	return true;
+public func SectionStop()
+{
+  iScript = -1;
+  return true;
 }
 
-public func SectionUnload() {
-	RemoveObject();
+public func SectionUnload()
+{
+  RemoveObject();
 }
 
-public func Relaunch(object pClonk) {
-	pClonk->SetPosition(AbsX(iRespawnX), AbsY(iRespawnY));
-	this->~OnRespawn(pClonk);	
+public func Relaunch(object pClonk)
+{
+  pClonk->SetPosition(AbsX(iRespawnX), AbsY(iRespawnY));
+  this->~OnRespawn(pClonk);	
 }
 
-public func SetRespawnPosition(int iX, int iY) {
-	iRespawnX = iX;
-	iRespawnY = iY;
-	return true;
+public func SetRespawnPosition(int iX, int iY)
+{
+  iRespawnX = iX;
+  iRespawnY = iY;
+  return true;
 }
