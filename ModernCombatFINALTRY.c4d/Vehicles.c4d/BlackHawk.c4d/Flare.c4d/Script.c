4277f9d762d30faa73e02e2ee09961bd1c39b807
Michael14 <michael@missile.beheh.de>
2010-11-12 19:42:45 +0000

    * Flare verschwindet bei Bodenkontakt (Flareabwurf bei gelandetem Blackhawk), Titelbild. * Blackhawk Sound für Flareabwurf. - Ungenutzter Raketencheck entfernt.

--- ModernCombatFINALTRY.c4d/Vehicles.c4d/BlackHawk.c4d/Flare.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Vehicles.c4d/BlackHawk.c4d/Flare.c4d/Script.c
@@ -1,26 +1,33 @@
-/*-- Flare --*/
+/*-- Täuschkörper --*/
 
 #strict 2
 
-public func IsBulletTarget() { return true; }
+public func IsBulletTarget()	{return true;}
+
+
+/* Timer */
 
 protected func Timer()
 {
-  //Langsamer runterfliegen
+  //Bei Bodenkontakt verschwinden
+  if(GetContact(this, -1, CNAT_Bottom))
+    return Hit();
+
+  //Langsamer runterfallen
   SetYDir(GetYDir()-GetGravity()/70);
+
   //Effekte
   CreateParticle("Flare",0,0,GetXDir()/3,GetYDir()/3,RandomX(15,25)*5,RGB(255,255,255));
-  if(GetEffectData(EFSM_ExplosionEffects) > 0)
-    CreateParticle("Smoke2",0,0,0,0,RandomX(20,35)*5,RGB(200,200,200));
+  if(GetEffectData(EFSM_ExplosionEffects) > 0) CreateParticle("Smoke2",0,0,0,0,RandomX(20,35)*5,RGB(200,200,200));
 }
 
 /* Schaden */
 
 public func OnDmg(int iDmg, int iType)
 {
-  if(iType == DMG_Fire)		    return 80;	//Feuer
-  if(iType == DMG_Projectile) return 80;  //Kugeln
-  if(iType == DMG_Bio)		    return 100;	//Säure und biologische Schadstoffe
+  if(iType == DMG_Fire)		return 80;	//Feuer
+  if(iType == DMG_Projectile)	return 80;	//Kugeln
+  if(iType == DMG_Bio)		return 100;	//Säure und biologische Schadstoffe
 }
 
 protected func Damage()
