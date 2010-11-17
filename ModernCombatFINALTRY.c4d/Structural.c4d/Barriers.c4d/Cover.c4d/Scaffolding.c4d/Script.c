a277d49ca6507269a1fb5d2a58972445f35d5d86
Michael14 <michael@missile.beheh.de>
2010-11-17 15:55:23 +0000

    + Gerüst eingeführt. * C4 Bestandteile korrigiert.

---
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Barriers.c4d/Cover.c4d/Scaffolding.c4d/Script.c
@@ -0,0 +1,85 @@
+/*-- Gerüst --*/
+
+#strict 2
+
+local iStatus, damaged;
+
+
+/* Aussehen festlegen */
+
+func Set(int i)
+{
+  iStatus = i;
+  SetGraphics(Format("%d", i));
+
+  if(iStatus > 3)
+    damaged = true;
+  else
+    damaged = false;
+}
+
+/* Zerstörung */
+
+func Damage(int iChange)
+{
+  if(damaged) return ;
+  if(GetDamage() < 80) return;
+  Destruct();
+}
+
+func Destruct()
+{
+  if(damaged) return ;
+  damaged = true;
+
+  if(iStatus < 2)
+  {
+    //Effekte
+    Sound("WoodCrack*.ogg");
+    if(GetEffectData(EFSM_ExplosionEffects) > 0) CastParticles("Smoke3",3,20,0,-25,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+
+    if(GetEffectData(EFSM_ExplosionEffects) > 1)
+    {
+      CastParticles("WoodSplinter",4,100,-20,-25,100,150);
+      CastParticles("WoodSplinter",4,100,20,-25,100,150);
+    }
+
+    SetGraphics("5");
+  }
+  else
+  if(iStatus < 3)
+  {
+    //Effekte
+    Sound("WoodCrack*.ogg");
+    if(GetEffectData(EFSM_ExplosionEffects) > 0) CastParticles("Smoke3",3,20,0,-25,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+
+    if(GetEffectData(EFSM_ExplosionEffects) > 1)
+    {
+      CastParticles("WoodSplinter",4,100,-20,-25,100,150);
+      CastParticles("WoodSplinter",4,100,20,-25,100,150);
+    }
+
+    SetGraphics("5");
+  }
+  else
+  {
+    //Effekte
+    Sound("WoodCrack*.ogg");
+    if(GetEffectData(EFSM_ExplosionEffects) > 0) CastParticles("Smoke3",3,20,0,-25,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+
+    if(GetEffectData(EFSM_ExplosionEffects) > 1)
+    {
+      CastParticles("WoodSplinter",4,100,-25,-5,100,150);
+      CastParticles("WoodSplinter",4,100,20,-30,100,150);
+    }
+
+    SetGraphics("5");
+  }
+}
+
+/* Schaden */
+
+public func OnDmg(int iDmg, int iType)
+{
+  if(iType == DMG_Bio)		return 100;	//Säure und biologische Schadstoffe
+}
