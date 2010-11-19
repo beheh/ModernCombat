be23b787008a163ec9aa931d553f341f74ba571d
Ace <ace@missile.beheh.de>
2010-11-19 22:07:34 +0000

    + ... Rauchgranate resetted + Peilsenderentfernung!

---
+++ ModernCombatFINALTRY.c4d/Items.c4d/Grenades.c4d/SmokeGrenade.c4d/Script.c
@@ -0,0 +1,49 @@
+/*-- Rauchgranate --*/
+
+#strict 2
+#include NADE
+
+local time;
+
+public func FuseTime()		{return 2*35;}
+public func Color()		{return RGB(128,255,0);}
+public func ContainedDamage()	{return 20;}
+
+
+/* Explosion */
+
+public func Fused()
+{
+  //Rauchen (gefährdet die Gesundheit)
+  ScheduleCall(this, "Smoke", 3, 10);
+
+  //Effekte
+  Sound("SGRN_Fused.ogg");
+  if(!GBackLiquid())
+  {
+   if(GetEffectData(EFSM_ExplosionEffects) > 0) CastParticles("Smoke3",8,50,0,0,120,150,RGBa(255,255,255,120),RGBa(255,255,255,150));
+  }
+
+  //Verschwinden
+  FadeOut();
+}
+
+/* Rauchen */
+
+func Smoke()
+{
+  CastParticles("Smoke3", 2, 5, 0, 0, 30, 100, RGBa(255, 255, 255, 130), RGBa(255, 255, 255, 200));
+
+  var smoke = CreateObject(SM4K, 0, 0, GetController());
+  SetXDir((GetXDir() / 3 * 2) + (time * RandomX(-30, +30)),smoke,100);
+  SetYDir((GetYDir() / 3 * 2) - (time * (10 + Random(20))),smoke,100);
+  
+  time++;
+}
+
+/* Aufprall */
+
+func HitSound()
+{
+  Sound("SGRN_Hit*.ogg");
+}
