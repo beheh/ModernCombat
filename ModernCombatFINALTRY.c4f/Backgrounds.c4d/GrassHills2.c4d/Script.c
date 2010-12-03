d06508b628f9d22d5dd89f16c063ee939609cd22
Benedict Etzel <developer@beheh.de>
2010-12-03 20:16:06 +0000

    * Hintergründe: #strict 2 + Hintergründe: C4D_MouseIgnore hinzugefügt

--- ModernCombatFINALTRY.c4f/Backgrounds.c4d/GrassHills2.c4d/Script.c
+++ ModernCombatFINALTRY.c4f/Backgrounds.c4d/GrassHills2.c4d/Script.c
@@ -1,12 +1,12 @@
 /*-- Grashügel --*/
 
-#strict
+#strict 2
 
 
 /* Initialisierung */
 
 public func Initialize()
 {
-  SetCategory(GetCategory() | C4D_Parallax() | C4D_Background());
+  SetCategory(GetCategory() | C4D_Parallax | C4D_Background | C4D_MouseIgnore);
   Local()=40; Local(1)=30;
 }
