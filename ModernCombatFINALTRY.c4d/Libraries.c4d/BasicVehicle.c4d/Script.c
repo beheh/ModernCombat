5afa7b5ce055c6517cb9b70de1901bbaa19383d9
Benedict Etzel <developer@beheh.de>
2010-12-04 10:52:42 +0000

    + SetColorDwFade hinzugefügt + SetOwnerFade hinzugefügt * Boot färbt sich nun mit SetOwnerFade um

--- ModernCombatFINALTRY.c4d/Libraries.c4d/BasicVehicle.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Libraries.c4d/BasicVehicle.c4d/Script.c
@@ -23,11 +23,11 @@ public func Initialize()
   fDestroyed = false;
   iLastAttacker = -1;
   aDealers = CreateArray();
-  AddEffect("BasicVehicleUnused", this, 50, 1, this, 0);
+  //AddEffect("BasicVehicleUnused", this, 50, 1, this, 0);
   return true;
 }
 
-global func FxBasicVehicleUnusedStart(object pTarget, int iEffectNumber)
+/*global func FxBasicVehicleUnusedStart(object pTarget, int iEffectNumber)
 {
   EffectVar(0, pTarget, iEffectNumber) = 38*15;
 }
@@ -48,7 +48,7 @@ global func FxBasicVehicleUnusedTimer(object pTarget, int iEffectNumber, int iTi
   if(EffectVar(0, pTarget, iEffectNumber) > 0) return FX_OK;
   SetOwner(NO_OWNER, pTarget);
   return FX_OK;
-}
+}*/
 
 /* Schaden */
 
@@ -106,4 +106,53 @@ public func Destroyed()
 
   //Letzen Angreifer zurücksetzen
   iLastAttacker = -1;
+}
+
+/* Umfärbung */
+
+global func SetOwnerFade(int iPlr, object pTarget, int iDuration) {
+	if(!pTarget) pTarget = this;
+	if(!pTarget) return 0;
+	if(GetOwner(pTarget) == iPlr) return true;
+	if(GetOwner(pTarget) == -1 && GetColorDw(pTarget) == 0) SetColorDw(RGB(0,0,255), pTarget);	
+	var clrOld = GetColorDw(pTarget);
+	SetColorDwFade(GetPlrColorDw(iPlr), pTarget, iDuration);
+  SetOwner(iPlr, pTarget);
+  SetColorDw(clrOld, pTarget);
+	return true;
+}
+
+global func SetColorDwFade(int clrNew, object pTarget, int iDuration) {
+	if(!iDuration) iDuration = 50;
+	if(!pTarget) pTarget = this;
+	if(!pTarget) return 0;
+  var clrOld = GetColorDw(pTarget);
+  RemoveEffect("UpdateOwnerColor", pTarget);
+  SetColorDw(clrOld, pTarget);
+	AddEffect("UpdateOwnerColor", pTarget, 50, 1, pTarget, 0, clrOld, clrNew, iDuration);
+	return true;
+}
+
+global func FxUpdateOwnerColorStart(pTarget, iNo, iTemp, clrOld, clrNew, iDuration)
+{
+  if(clrOld == clrNew) return -1;
+  EffectVar(0, pTarget, iNo) = clrOld;
+  EffectVar(1, pTarget, iNo) = clrNew;
+  EffectVar(2, pTarget, iNo) = iDuration;
+  return true;
+}
+    
+global func FxUpdateOwnerColorTimer(pTarget, iNo, iTime)
+{
+	var clrOld = EffectVar(0, pTarget, iNo);
+	var clrNew = EffectVar(1, pTarget, iNo);
+	var iDuration = EffectVar(2, pTarget, iNo);
+  var color = RGBa(Interpolate2(GetRGBaValue(clrOld, 1), GetRGBaValue(clrNew, 1), iTime, iDuration), Interpolate2(GetRGBaValue(clrOld, 2), GetRGBaValue(clrNew, 2), iTime, iDuration), Interpolate2(GetRGBaValue(clrOld, 3), GetRGBaValue(clrNew, 3), iTime, iDuration));
+	SetColorDw(color, pTarget);
+	if(iTime >= iDuration) return -1;
+}
+
+global func FxUpdateOwnerColorStop(pTarget, iNo, iReason, fTemp)
+{
+	SetColorDw(EffectVar(1, pTarget, iNo), pTarget);
 }
