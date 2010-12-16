93ee5297d780851d15c43a60d193acb2134df73b
Michael14 <michael@missile.beheh.de>
2010-12-16 18:15:06 +0000

    * Ticketanzahl pro Angreifer bei Assault um 1 gesenkt.

--- cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/Assault.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/Assault.c4d/Script.c
@@ -57,11 +57,11 @@ public func CalcTickets()
   //return 4 * A + ((D - A) * D);
   //return D + (A + 2 * D + D * D) / (A + 1);
 
-  //Für jeden Angreifer gibt es 5 Tickets. Gibt es mehr Angreifer als Verteidiger, werden für jeden überschüssigen
+  //Für jeden Angreifer gibt es 4 Tickets. Gibt es mehr Angreifer als Verteidiger, werden für jeden überschüssigen
   //Angreifer 3 Tickets abgezogen. Gibt es mehr Verteidiger als Angreifer, werden für jeden überschüssigen
   //Verteidiger 3 Tickets hinzugefügt.
 
-  return 5 * A + (D * 3) - (A * 3);
+  return 4 * A + (D * 3) - (A * 3);
 }
 
 public func AddAssaultTarget(id idTarget, int iX, int iY, int iMaxDamage, int iTeam, string szName, int iIndex, array aSpawn, bool fNoBar)
