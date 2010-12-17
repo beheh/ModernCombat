d387ac2001989d0bb65500e57fb920c36227b159
Michael14 <michael@missile.beheh.de>
2010-12-17 20:35:00 +0000

    * SMG Spread um 5 erhöht. * Script gesäubert.

--- ModernCombatFINALTRY.c4d/Items.c4d/Firearms.c4d/SubmachineGun.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Items.c4d/Firearms.c4d/SubmachineGun.c4d/Script.c
@@ -40,7 +40,7 @@ public func FMData1(int data)
   
   if(data == FM_Slot)		return 1;
   
-  if(data == FM_SpreadAdd)	return 15;
+  if(data == FM_SpreadAdd)	return 20;
   if(data == FM_StartSpread)	return 50;
   if(data == FM_MaxSpread)	return 350;
 
