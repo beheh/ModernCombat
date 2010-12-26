ebe5d0f6f06b11d4c3df8e6727478d604420acc8
Limeox <limeox@missile.beheh.de>
2010-12-26 13:00:49 +0000

    ! #8329 GBAS-Scoreboard

--- cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/BaseAssault.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/BaseAssault.c4d/Script.c
@@ -12,8 +12,9 @@ public func ChooserFinished()
   for(var i = 0; i < GetPlayerCount(); i++)
   {
     CreateObject(TK04, 0, 0, GetPlayerByIndex(i));
-    Sound("RadioConfirm*.ogg", true, 0, 100, GetPlayerByIndex(i));
+    Sound("RadioConfirm*.ogg", true, 0, 100, GetPlayerByIndex(i) + 1);
   }
+  return _inherited(...);
 }
 
 /* Bekanntgabe */
