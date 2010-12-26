d38f42ae5d13e7657f42745ad489863147b3e02b
Benedict Etzel <developer@beheh.de>
2010-12-26 12:38:29 +0000

    ! Nur noch WPN2s gelten für Marksman * Rauchgranateneffekte entfernen sich korrekt nach Ablauf

--- ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/System.c4g/Appendto-Clonk.c
+++ ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/System.c4g/Appendto-Clonk.c
@@ -260,7 +260,7 @@ protected func DoPoints()
     	var pClonk = GetCursor(killer);
      	if(pClonk->~GetRealCursor()) pClonk = pClonk->~GetRealCursor();
      	//Marksman
-      if(ObjectDistance(pClonk, this) >= 350 && LastDamageType() == DMG_Projectile && !Contained(pClonk)) {
+      if(ObjectDistance(pClonk, this) >= 350 && LastDamageType() == DMG_Projectile && KillIcon()->~IsWeapon2() && !Contained(pClonk)) {
         DoAchievementProgress(1,AC17,killer);
       }
      	//Helikopter-Sachen
@@ -292,7 +292,7 @@ protected func DoPoints()
 	  }
   }
   
-  //Non-swimmer
+  //Nonswimmer
   if(GBackLiquid()) DoAchievementProgress(1, AC11, GetOwner());
   
   //Selfkill
