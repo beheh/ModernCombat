84708f7cd4d27844e365071a4ee31b538c2cfb9d
Michael14 <michael@missile.beheh.de>
2010-12-26 14:18:03 +0000

    + CMC_Your Eyes eingeführt. * Musiklisten aktualisiert. * Standardtitel für Laguna und Oasis ist Your Eyes.

--- ModernCombatFINALTRY.c4f/CMC_BabelTower.c4s/Script.c
+++ ModernCombatFINALTRY.c4f/CMC_BabelTower.c4s/Script.c
@@ -18,7 +18,7 @@ public func OccupationDir()
 func Initialize()
 {
   //Starttitel und Musikliste zusammenstellen
-  SetPlayList("CMC_Base Groove.ogg;CMC_Firehawk.ogg;CMC_Friendly Unit.ogg;CMC_Getaway.ogg;CMC_Moving Squad.ogg;CMC_Offensive.ogg;CMC_Showtime.ogg;CMC_Slow Motion.ogg;CMC_Striking Force.ogg;CMC_No Good.ogg;CMC_Obsession.ogg");
+  SetPlayList("CMC_Base Groove.ogg;CMC_Firehawk.ogg;CMC_Friendly Unit.ogg;CMC_Getaway.ogg;CMC_Moving Squad.ogg;CMC_Offensive.ogg;CMC_Showtime.ogg;CMC_Slow Motion.ogg;CMC_Striking Force.ogg;CMC_No Good.ogg;CMC_Obsession.ogg;CMC_Your Eyes.ogg");
   Music("CMC_Friendly Unit.ogg");
   //Bildschirmfärbung
   SetGamma(RGB(15,15,15),RGB(118,118,118),RGB(215,215,215));
--- ModernCombatFINALTRY.c4f/CMC_Firewood.c4s/Script.c
+++ ModernCombatFINALTRY.c4f/CMC_Firewood.c4s/Script.c
@@ -11,7 +11,7 @@ static aFlag;
 func Initialize()
 {
   //Starttitel und Musikliste zusammenstellen
-  SetPlayList("CMC_Base Groove.ogg;CMC_Firehawk.ogg;CMC_Friendly Unit.ogg;CMC_Getaway.ogg;CMC_Moving Squad.ogg;CMC_Offensive.ogg;CMC_Showtime.ogg;CMC_Slow Motion.ogg;CMC_Striking Force.ogg;CMC_No Good.ogg;CMC_Obsession.ogg");
+  SetPlayList("CMC_Base Groove.ogg;CMC_Firehawk.ogg;CMC_Friendly Unit.ogg;CMC_Getaway.ogg;CMC_Moving Squad.ogg;CMC_Offensive.ogg;CMC_Showtime.ogg;CMC_Slow Motion.ogg;CMC_Striking Force.ogg;CMC_No Good.ogg;CMC_Obsession.ogg;CMC_Your Eyes.ogg");
   Music("CMC_Getaway.ogg");
   //Bildschirmfärbung
   SetGamma(RGB(2,3,10), RGB(131,135,158), RGB(252,253,255) );
