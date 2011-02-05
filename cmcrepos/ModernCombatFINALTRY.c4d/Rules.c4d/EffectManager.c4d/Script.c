da7ab41cc22094b2cffb590c555efb9dee0cefb0
Michael14 <michael@missile.beheh.de>
2011-02-05 21:15:29 +0000

    * Script gesäubert.

--- cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/EffectManager.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/EffectManager.c4d/Script.c
@@ -3,25 +3,29 @@
 #strict 2
 
 
+/* Auswahlmenü */
+
 protected func Activate(iByPlayer)
 {
   var pClonk = GetCursor(iByPlayer);
+  //Wenn Spieler 1 und keine Liga
   if(iByPlayer == 0 && !GetLeague())
   {
-   //Menü aufmachen
-   if(GetMenu(pClonk)) return;
-   CreateMenu(GetID(), pClonk, 0,0,GetName(),0, 1);
-   for(var i = 1; i <= 3; i++)
-   {
-     var szText = Format("$EffectLevel$", i);
-     if(i == EFSM_Level) szText = Format("<c ffff33>%s</c>",szText);
-     AddMenuItem(szText, "SetEffectLevel", EFMN, pClonk, 0, i, "$SetEffectLevel$");
-   }
+    //Menü öffnen
+    if(GetMenu(pClonk)) return;
+    CreateMenu(GetID(), pClonk, 0,0,GetName(),0, 1);
+    for(var i = 1; i <= 3; i++)
+    {
+      var szText = Format("$EffectLevel$", i);
+      if(i == EFSM_Level) szText = Format("<c ffff33>%s</c>",szText);
+      AddMenuItem(szText, "SetEffectLevel", EFMN, pClonk, 0, i, "$SetEffectLevel$");
+    }
   }
   else
   {
+    //Beschreibung anzeigen
     var szLevel = Format("Level %d", EFSM_Level);
-    if(EFSM_Level == 0) szLevel = "Benutzerdefiniert";
+    if(EFSM_Level == 0) szLevel = "$Custom$";
     MessageWindow(Format("$Desc$", szLevel),iByPlayer);
   }
   return(1);
@@ -29,9 +33,10 @@ protected func Activate(iByPlayer)
 
 protected func SetEffectLevel(id dummy, int iLevel)
 {
-  EFSM_SetEffects(iLevel);
   //Bei Ligaspielen nicht möglich
-  if(!GetLeague()) EventInfo4K(0, Format("$NewLevel$", iLevel), EFMN);
+  if(GetLeague()) return;
+  EFSM_SetEffects(iLevel);
+  EventInfo4K(0, Format("$NewLevel$", iLevel), EFMN);
 }
 
 protected func End(id dummy, object pClonk)
