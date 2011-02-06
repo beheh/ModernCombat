9eb62340436fbacc373481176541d3940755dd39
Limeox <limeox@missile.beheh.de>
2011-02-06 17:22:31 +0000

    * Script gesäubert ! Healthbar funktioniert mit variablen HP

--- ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/DragninDispenser.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/DragninDispenser.c4d/Script.c
@@ -16,10 +16,10 @@ public func RejectCollect(id idObj, object pObj)
   if(packet)
     return PlayerMessage(GetOwner(pObj), "$AlreadyCharging$", this);
   //Schon voll?
-  if(pObj->GetPackPoints() >= 150)
+  if(pObj->~GetPackPoints() >= pObj->~MaxPoints())
     return PlayerMessage(GetOwner(pObj), "$NoChargeNeeded$", this);
   //Nicht genug Geld?
-  if(GetWealth(GetOwner(pObj)) < GetValue(0,FAPK) - GetValue(pObj))
+  if(GetWealth(GetOwner(pObj)) < GetValue(0, FAPK) - GetValue(pObj))
     return PlayerMessage(GetOwner(pObj), "$NotEnoughFunds$", this);
 }
 
@@ -28,20 +28,20 @@ public func RejectCollect(id idObj, object pObj)
 public func Collection(pEHP)
 {
   packet = pEHP;
-  AddEffect("ChargePacket",packet,1,1,this);
-  DoWealth(GetOwner(pEHP),-(GetValue(0,FAPK) - GetValue(pEHP)));
+  AddEffect("ChargePacket", packet, 1, 1, this);
+  DoWealth(GetOwner(pEHP), GetValue(pEHP) - GetValue(0, FAPK));
   SetAction("Load");
 }
 
 public func FxChargePacketTimer(pTarget, iNo, iTime)
 {
-  if(pTarget->GetPackPoints() >= 150)
+  if(pTarget->~GetPackPoints() >= pTarget->~MaxPoints())
   {
-    Exit(pTarget,-3,5);
+    Exit(pTarget, -3, 5);
     SetAction("Idle");
     packet = 0;
     return -1;
   }
-  pTarget->DoPackPoints(1);
-  CreateParticle("FapLight", -2, +2, 0, 0, 30, RGBa(BoundBy(InvertA1(255 * (pTarget->GetPackPoints()) / 150, 255) + 10, 0, 255), 255 * (pTarget->GetPackPoints()) / 150));
+  pTarget->~DoPackPoints(1);
+  CreateParticle("FapLight", -2, +2, 0, 0, 30, pTarget->~Fill2Color());
 }
