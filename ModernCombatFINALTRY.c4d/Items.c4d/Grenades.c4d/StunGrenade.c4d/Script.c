83c042ffa32b58442ec3ddf48087ad595888d86a
Benedict Etzel <developer@beheh.de>
2011-02-20 10:02:29 +0000

    * Blendgranate: Hellerer Flash

--- ModernCombatFINALTRY.c4d/Items.c4d/Grenades.c4d/StunGrenade.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Items.c4d/Grenades.c4d/StunGrenade.c4d/Script.c
@@ -51,7 +51,7 @@ public func Fused()
   Sparks(30,RGB(255,128));
   if(GetEffectData(EFSM_ExplosionEffects) > 0) CastSmoke("Smoke3",12,40,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
   CastParticles("PxSpark",10,40,0,0,60,120,RGBa(255,255,255,70),RGBa(255,255,255,70));
-  AddLightFlash(50,0,0,RGB(255,255,128),this);
+  for(var i = 0; i <= 3; i++) AddLightFlash(250,0,0,RGB(255,255,255),this);
   FadeOut();
 }
 
