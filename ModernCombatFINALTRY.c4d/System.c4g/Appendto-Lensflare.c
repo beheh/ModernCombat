fcc82ef535d4b64175f3c1a2074bb1a1993f3472
Michael14 <michael@missile.beheh.de>
2011-05-06 18:55:36 +0000

    * Script gesäubert.

--- ModernCombatFINALTRY.c4d/System.c4g/Appendto-Lensflare.c
+++ ModernCombatFINALTRY.c4d/System.c4g/Appendto-Lensflare.c
@@ -1,27 +1,30 @@
-/* Lensflare */
+/*-- Lensflare --*/
 
 #strict 2
 #appendto LENS
 
 local fixed;
 
-public func IsMaster() {
+
+public func IsMaster()
+{
   return fMaster;
 }
 
-// Initialisierung
+/* Initialisierung */
+
 protected func Initialize()
 {
-  // Nur für den Herrn und Meister sichtbar
+  //Unsichtbar
   SetVisibility (VIS_Owner, this);
-  // Das erste Lensflare-Objekt prüft nur, dass jeder Spieler auch sein Umweltobjekt hat
+  //Das erste Lensflare-Objekt prüft nur, dass jeder Spieler ein Umweltobjekt hat
   if(!FindObject2(Find_ID(GetID()), Find_Exclude(this)))
   {
-    SetOwner(-1,this); // Unparteiisch sein!
+    SetOwner(-1,this);
     fMaster = 1;
-    // Sind wir noch nicht plaziert?
+    //Plazieren
     var iPosX = LandscapeWidth()/2;
-    // Höhe nach Landschaft
+    //Höhe nach Landschaft
     var iPosY;
     for(iPosY = 0; iPosY < LandscapeHeight(); iPosY++)
       if(GetMaterial(iPosX-GetX(), iPosY-GetY()) != -1) break;
@@ -30,57 +33,63 @@ protected func Initialize()
   }
 }
 
-// Verwaltung der Flares
+/* Flare-Verwaltung */
+
 protected func ManageFlares()
 {
-  if(!GetEffectData(EFSM_Lensflares)) {
+  if(!GetEffectData(EFSM_Lensflares))
+  {
     if(fixed) return;
     fixed = true;
+
   }
-  else {
+  else
+  {
     if(fixed) fixed = false;
   }  
-  // unskalierten Vektor ermitteln
+  //Unskalierten Vektor ermitteln
   var iVectorX = GetVectorX(GetOwner());
   var iVectorY = GetVectorY(GetOwner());
   var iVectorDist = GetVectorDist(GetOwner());
-  // Die Modulation ist für alle Flares gleich
+  //Modulation für alle Flares gleich
   var iColorModulation = RGBa(GetColorMod(),GetColorMod(),GetColorMod(),GetAlphaMod(iVectorDist));
-  // Genauso auch die Abstandsangabe
+  //Gleiche Abstandsangabe
   var iDistance = 1000-BoundBy((3000-iVectorDist*10)/3,0,1000);  // Promilleangabe 0 = nahe, 1000 = weit weg; 300 Pixel sind dabei "weit weg"
   var iAbsolutDistanc = (50*iDistance)/10; // Wieviele 1/100 Pixel zwischen den einzelnen Lensflares sind
-  // Ist unsere liebe Sonne überhaupt sichtbar (also Tag + unverdeckt)
+  //Sonnensichtbarkeit prüfen
   var fSunVisible = !fixed && IsDay() && (!GBackSemiSolid(0,0)) && SunFree(GetCursor(GetOwner()));
-  // Tagsüber die Sonne einblenden
+  //Tagsüber Sonne einblenden
   if(IsDay())
     SetVisibility (VIS_Owner);
   else
     SetVisibility (VIS_None);
-  // Alle Lensflares durchgehen
+  //Lensflares durchgehen
   var i = 0;
   for(var pFlare in aFlares)
   {
     if(fSunVisible)
     {
-      // Tags sichtbar
+      //Tags sichtbar
       SetVisibility (VIS_Owner, pFlare);
       SetClrModulation(iColorModulation,pFlare);
       var LensDist = (i*iAbsolutDistanc)/100;
       SetPosition(GibLensPosX(LensDist,iVectorX,iVectorDist), GibLensPosY(LensDist,iVectorY,iVectorDist),pFlare);
     }
     else
-      // Nachts unsichtbar ODER bei verdeckter Sonne
+      //Nachts und bei verdeckter Sonne unsichtbar
       SetVisibility (VIS_None, pFlare);
     i++;
   }
-  // Blenden über ScreenRGB
-  if(SunFree(GetCursor(GetOwner()))) {
+  //Blenden
+  if(SunFree(GetCursor(GetOwner())))
+  {
     var iYellow = BoundBy((2000-iVectorDist)/5,0,100);
     ScreenRGB(GetCursor(GetOwner()), RGB(000+(30*iYellow)/100,000+(30*iYellow)/100,000), 255, 10, true, SR4K_LayerLensflare);
   }
 }
 
-private func SunFree(object pTo) {
+private func SunFree(object pTo)
+{
   while(Contained(pTo)) pTo = Contained(pTo);
   if(Stuck(pTo)) return;
   if(GetEffectData(EFSM_Lensflares) < 2) return true;
