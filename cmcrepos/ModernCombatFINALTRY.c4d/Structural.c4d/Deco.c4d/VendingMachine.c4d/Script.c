8beccd76416ada4d95666c82c309295419848f2f
Michael14 <michael@missile.beheh.de>
2011-05-14 10:28:43 +0000

    * CMC Automat nun mit mehreren Aussehen, ersetzt nun komplett die Hazard-Versionen. * Zerstörungseffekte der Automaten verbessert.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Structural.c4d/Deco.c4d/VendingMachine.c4d/Script.c
@@ -0,0 +1,56 @@
+/*-- Automat --*/
+
+#strict 2
+
+local damaged;
+
+
+/* Initialisierung */
+
+protected func Initialize()
+{
+  //Soundeffekt
+  Sound("FreezerLoop", 0,0,0,0, +1);
+  //Aktionsschleife starten
+  SetAction("Glow");
+}
+
+/* Aussehen festlegen */
+
+public func Set(int i)
+{
+  SetGraphics(Format("%d", i));
+}
+
+/* Zerstörung */
+
+func Damage()
+{
+  if(damaged) return ;
+  if(GetDamage() > 10)
+  {
+    //Effekte
+    Sound("CokeCrash");
+    if(GetEffectData(EFSM_ExplosionEffects) > 0) CastSmoke("Smoke3",12,10,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+    Schedule(Format("CreateParticle(\"Tin\", -5, 12, RandomX(-10, 10), -1, 30, %d);", RGB(255, 25, 25)), 2, 10, this());
+  }
+  if(GetDamage() > 80)
+  {
+    damaged = true;
+
+    //Effekte
+    Sound("FreezerLoop", 0,0,0,0, -1);
+    Sound("StructuralDamage*.ogg");
+    Schedule("CastObjects(SPRK, 1, 15, RandomX(-10, 10), RandomX(-15, 15));", 3, 15, this());
+    if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("MetalSplinter", 10, 100, 0,0, 80);
+    AddFireEffect(this,100,RGB(80,80,80),true,30);
+    SetAction("Destroyed");
+  }
+}
+
+/* Schaden */
+
+public func OnDmg(int iDmg, int iType)
+{
+  if(iType == DMG_Bio)		return 100;	//Säure und biologische Schadstoffe
+}
