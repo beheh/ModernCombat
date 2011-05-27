4dd9922a9f5a8e68d8972f8d4ab15c7f7322c332
Michael14 <michael@missile.beheh.de>
2011-05-27 13:28:53 +0000

    + Protective Pro eingefügt. * Raketen: Detonation in eigene Funktion ausgelagert, um verschiedene Verhalten trotz gleichem Appendto zu ermöglichen. * Missile Artillery nun auch mit Blackhawk-Langstreckenraketen möglich. * Langstreckenraketen haben nun auch eine Fall-Aktion wie der Raketenstandard.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/24ProtectivePro.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- Protective Pro --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 24;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
