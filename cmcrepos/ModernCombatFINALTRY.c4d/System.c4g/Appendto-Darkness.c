0e4af6daf863e312352a197db02f1c43f68d9ac6
Michael14 <michael@missile.beheh.de>
2011-06-12 18:37:40 +0000

    ! Darkness-Reparatur Nr1.

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Darkness.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Darkness.c
@@ -6,6 +6,22 @@
 #appendto DARK
 
 
+/* Dunkelheit bestimmen */
+
+private func PostInitialize()
+{
+  //Nur eine Dunkelheit
+  var count = 1;
+  for(var d in FindObjects(Find_ID(GetID()),Find_Exclude(this())))
+  {
+    count++;
+    RemoveObject(d);
+  }
+
+  //Dunkelheit setzen
+  SetDarkness(count);
+}
+
 public func ChooserFinished()
 {
   for(var pClonk in FindObjects(Find_OCF(OCF_CrewMember), Find_Not(Find_Owner(NO_OWNER))))
