b1676fbb9ab389bb9db2db3f19d8770c49dc2cfd
Michael14 <michael@missile.beheh.de>
2011-07-05 11:21:46 +0000

    + Fist of Fury und First Blood Achievements eingefügt.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/26FistOfFury.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- Fist of Fury --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 26;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/27FirstBlood.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- First Blood --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 27;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
