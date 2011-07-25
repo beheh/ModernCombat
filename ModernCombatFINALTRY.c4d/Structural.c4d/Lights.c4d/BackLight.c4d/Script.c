e767b3e34f29ce93b33d4dc477a2308315d47d50
Michael14 <michael@missile.beheh.de>
2011-07-25 19:51:07 +0000

    ! Lichter bearbeitet: Können und zerschossen werden.

--- ModernCombatFINALTRY.c4d/Structural.c4d/Lights.c4d/BackLight.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Lights.c4d/BackLight.c4d/Script.c
@@ -1,108 +1,27 @@
 /*-- Ausgangslicht --*/
 
 #strict 2
-
-local pLight, bOn, broken;
+#include BLGH
 
 public func LightSize()	{return 50;}
-public func IsLamp()	{return true;}
-public func IsMachine()	{return true;}
 
 
-/* Initialisierung */
-
-protected func Initialize()
-{
-  CreateLight();
-  TurnOn();
-}
-
-public func Light() 
-{
-  return pLight; 
-}
-
-protected func CreateLight()
-{
-  pLight = AddLightAmbience(LightSize());
-}
-
-/* Ein- und Ausschalten */
-
-public func TurnOn()
-{
-  if(EMPShocked()) return;
-  if(broken) return;
-
-  bOn = true;
-  SetAction("On");
-  if(Light())
-  Light()->TurnOn();
-}
-
-public func TurnOff()
-{
-  if(!SetAction("Off"))
-
-  SetAction("Idle");
-  bOn = false;
-  if(Light())
-  Light()->TurnOff();
-}
-
-public func Switch()
-{
-  if(GetAction() == "On")
-    TurnOff();
-  else
-    TurnOn();
-}
-
-/* EMP Effekt */
-
-public func EMPShock()
-{
-  if(broken) return;
-  TurnOff();
-  EMPShockEffect(800+Random(200));
-}
-
-public func EMPShockEnd()
-{
-  if(bOn)
-  {
-    SetAction("On");
-    if(Light()) Light()->TurnOn();
-  }
-}
-
-/* Zerstörung */
-
-public func IsBulletTarget(id def)
-{
-  if(broken) return ;
-  if(def->~NoDecoDamage()) return;
-  return Random(2);
-}
+/* (Temporäre) Zerstörung */
 
 func Damage()
 {
   if(broken) return;
   broken = true;
-  SetAction("Broken");
+
+  //Licht ausschalten
   if(Light())
     Light()->TurnOff();
-  Sound("Glass");
-  Sparks(7+Random(5), RGBa(255,255,150,100));
+  SetAction("Broken");
+
+  //Effekte
+  if(GetEffectData(EFSM_ExplosionEffects) > 0) CastSmoke("Smoke3",5,5,0,0,40,100,RGBa(255,255,255,120),RGBa(255,255,255,150));
   CastParticles("SplinterGlass", 1, 35, RandomX(-10,10), -5, 20, 20, RGBa(255,255,255,0), RGBa(255,255,255,0));
-  CastParticles("Smoke3",5,5,0,0,40,100,RGBa(255,255,255,120),RGBa(255,255,255,150));
+  Sparks(7+Random(5), RGBa(255,255,150,100));
   AddLightFlash(50, 0,0, RGBa(255,255,200,100));
-}
-
-/* Serialisierung */
-
-public func Serialize(array& extra)
-{
-  if (GetAction() != "On")
-    extra[GetLength(extra)] = "TurnOff()";
+  Sound("GlassBreak*.ogg");
 }
---
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Lights.c4d/System.c4g/Appendto-Backlight.c
@@ -0,0 +1,131 @@
+/*-- Hintergrundlicht --*/
+
+//Lichter können zerschossen werden und reaktivieren sich automatisch.
+
+#strict 2
+#appendto BLGH
+
+local pLight, bOn, broken;
+
+public func LightSize()		{return 100;}	//Lichtgröße
+public func DownTime()		{return 1500;}	//Zeit bis zur Reaktivierung
+public func IsLamp()		{return true;}
+public func IsMachine()		{return true;}
+public func RejectC4Attach()	{return true;}
+
+
+/* Initialisierung */
+
+protected func Initialize()
+{
+  //Licht erstellen
+  CreateLight();
+  //Einschalten
+  TurnOn();
+  bOn = true;
+}
+
+protected func CreateLight()
+{
+  pLight = AddLightAmbience(LightSize());
+}
+
+public func Light() 
+{
+  return pLight;
+}
+
+/* Ein- und Ausschalten */
+
+public func Switch()
+{
+  if(broken) return;
+  Sound("LightSwitch.ogg");
+
+  if(GetAction() = "On")
+    TurnOff();
+  else
+    TurnOn();
+}
+
+public func TurnOn()
+{
+  if(broken) return;
+  if(EMPShocked()) return;
+  bOn = true;
+  SetAction("On");
+  if(Light())
+    Light()->TurnOn();
+}
+
+public func TurnOff()
+{
+  if(broken) return;
+  if(!SetAction("Off"))
+    SetAction("Idle");
+  bOn = false;
+  if(Light())
+    Light()->TurnOff();
+}
+
+/* EMP Effekt */
+
+public func EMPShock()
+{
+  TurnOff();
+  EMPShockEffect(800+Random(200));
+}
+
+public func EMPShockEnd()
+{
+  if(bOn)
+  {
+    SetAction("On");
+    if(Light()) Light()->TurnOn();
+    Sound("LightSwitch.ogg");
+  }
+}
+
+/* (Temporäre) Zerstörung */
+
+public func IsBulletTarget(id def)
+{
+  if(broken) return ;
+  if(!bOn) return ;
+  if(def->~NoDecoDamage()) return;
+  return Random(2);
+}
+
+func Damage()
+{
+  if(broken) return;
+  broken = true;
+
+  //Licht ausschalten
+  if(Light())
+    Light()->TurnOff();
+  SetAction("Idle");
+
+  //Reaktivierung planen
+  ScheduleCall(0, "Reactivate", DownTime());
+
+  //Effekte
+  if(GetEffectData(EFSM_ExplosionEffects) > 0) CastSmoke("Smoke3",5,5,0,0,40,100,RGBa(255,255,255,120),RGBa(255,255,255,150));
+  CastParticles("SplinterGlass", 1, 35, RandomX(-10,10), -5, 20, 20, RGBa(255,255,255,0), RGBa(255,255,255,0));
+  Sparks(7+Random(5), RGBa(255,255,150,100));
+  AddLightFlash(50, 0,0, RGBa(255,255,200,100));
+  Sound("GlassBreak*.ogg");
+}
+
+func Reactivate()
+{
+  //Licht einschalten
+  if(bOn)
+  {
+    SetAction("On");
+    if(Light()) Light()->TurnOn();
+    Sound("LightSwitch.ogg");
+  }
+
+  broken = false;
+}
