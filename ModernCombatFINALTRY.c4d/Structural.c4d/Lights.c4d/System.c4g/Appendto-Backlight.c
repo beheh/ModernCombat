24691a860cccd4d838dbf3c5d9c836d806360e27
Michael14 <michael@missile.beheh.de>
2011-07-26 22:07:55 +0000

    + Straßenlicht eingefügt. * Ausschusseffekte von Lampen verändert.

--- ModernCombatFINALTRY.c4d/Structural.c4d/Lights.c4d/System.c4g/Appendto-Backlight.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Lights.c4d/System.c4g/Appendto-Backlight.c
@@ -111,9 +111,8 @@ func Damage()
 
   //Effekte
   if(GetEffectData(EFSM_ExplosionEffects) > 0) CastSmoke("Smoke3",5,5,0,0,40,100,RGBa(255,255,255,120),RGBa(255,255,255,150));
-  CastParticles("SplinterGlass", 1, 35, RandomX(-10,10), -5, 20, 20, RGBa(255,255,255,0), RGBa(255,255,255,0));
+  CastParticles("SplinterGlass", 5, 35, RandomX(-10,10), -5, 20, 20, RGBa(255,255,255,0), RGBa(255,255,255,0));
   Sparks(7+Random(5), RGBa(255,255,150,100));
-  AddLightFlash(50, 0,0, RGBa(255,255,200,100));
   Sound("GlassBreak*.ogg");
 }
 
