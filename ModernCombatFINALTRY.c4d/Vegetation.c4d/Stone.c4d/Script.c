1def6f79c4f43f078035254fee04938dca70db23
Michael14 <michael@missile.beheh.de>
2011-08-02 13:32:00 +0000

    * Gestein nur noch minimal verschiebbar.

--- ModernCombatFINALTRY.c4d/Vegetation.c4d/Stone.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Vegetation.c4d/Stone.c4d/Script.c
@@ -20,11 +20,20 @@ func Damage()
   if(GetDamage() > 70)
   {
     //Effekte
-    if(GetEffectData(EFSM_ExplosionEffects) > 0) CastSmoke("Smoke3",12,10,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
-    if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("ConcreteSplinter",4,150,0,0,20,100);
+    if(GetEffectData(EFSM_ExplosionEffects) > 0) CastSmoke("Smoke3",12,10,0,0,100,200,RGBa(255,255,255,50),RGBa(255,255,255,130));
+    if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("ConcreteSplinter",10,100,0,0,20,100);
     Sound("RockBreak*");
+    Sound("Blast2");
+    Sound("Debris*.ogg");
 
     //Verschwinden
     RemoveObject();
   }
+}
+
+/* Aufschlag */
+
+protected func Hit()
+{
+  Sound("RockHit*");
 }
