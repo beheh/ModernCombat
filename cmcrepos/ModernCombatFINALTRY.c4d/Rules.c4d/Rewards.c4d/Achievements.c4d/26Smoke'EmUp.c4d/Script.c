a35babb48635cb2e9c8a25dcc3130a6af0876636
Michael14 <michael@missile.beheh.de>
2011-08-15 10:59:33 +0000

    + Neues Achievement: Smoke 'em up. * Intelligence: Benötige Anzahl von 10 auf 20 erhöht.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/26Smoke'EmUp.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- Smoke 'em up --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 26;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 3;	//Benötigte Punktzahl einfügen
+}
