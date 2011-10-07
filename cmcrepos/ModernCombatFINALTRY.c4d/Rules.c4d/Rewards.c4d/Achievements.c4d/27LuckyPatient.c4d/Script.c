084a55f7589499b90d5c692ec7064bc3f1e3a39a
Michael14 <michael@missile.beheh.de>
2011-10-07 21:41:46 +0000

    + Neues Achievement: Lucky Patient.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/27LuckyPatient.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- Lucky Patient --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 27;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 10;	//Benötigte Punktzahl einfügen
+}
