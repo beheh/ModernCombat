2abed1efb9694ecda3e39c40bf7bb339cbccc2e9
Michael14 <michael@missile.beheh.de>
2012-01-15 19:34:29 +0000

    + Zwei neue Achievements: Meat Grinder und Rain of Fire. + Dummyobjekt für 31 Achievement. * Highlight-Stern nun CMC-gelb.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/30RainofFire.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- Rain of Fire --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 30;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/31FinalTry.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- Final Try --*/
+
+#strict 2
+#include ACHH
+
+
+public func GetSavingSlot()
+{
+  return 31;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
