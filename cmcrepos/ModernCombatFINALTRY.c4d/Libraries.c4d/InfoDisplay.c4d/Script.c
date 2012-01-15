afe7aa37cb33132c1ab1f60216e5519d4b7982e5
Michael14 <michael@missile.beheh.de>
2012-01-15 11:40:37 +0000

    + Animierter Highlight-Hintergrund für Achievement/Spielzielinformationen.

--- cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/InfoDisplay.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/InfoDisplay.c4d/Script.c
@@ -5,6 +5,7 @@
 local iTime;
 local szDesc;
 local iSize;
+local highlight;
 
 
 /* Initialisierung */
@@ -18,6 +19,13 @@ public func Initialize()
   SetPosition(-GetDefWidth(GetID())/2-120, -GetDefHeight(GetID())/2-42);
   SetClrModulation(RGBa(255,255,255,255));
   SetObjDrawTransform(10000/iSize,0,-GetDefWidth(GetID())*10000/(2*iSize),0,10000/iSize,-GetDefHeight(GetID())*10000/(2*iSize));
+
+  //Highlight-Objekt erstellen
+  highlight = CreateObject(HTLT, 0, 0, GetOwner());
+  highlight->SetVisibility(VIS_Owner);
+  SetPosition(-GetDefWidth(GetID())/2-40, -GetDefHeight(GetID())/2-25,highlight);
+  SetClrModulation(RGBa(255,255,255,255), highlight);
+  AddEffect("Highlight", highlight, 101, 1, this, 0, this);
   //Parallax
   Local(0) = 0;
   Local(1) = 0;
@@ -32,6 +40,37 @@ public func GetAchievementScore()
 {
   return 0;
 }
+
+/* Highlight-Effekt */
+
+public func FxHighlightStart(object target, int nr, temp, object ach)
+{
+  EffectVar(0, target, nr) = 255;	//Alphawert
+  EffectVar(1, target, nr) = -5;	//Erhöhung/Verringerung des Alphawertes
+  EffectVar(3, target, nr) = ach;	//Achievementobjekt
+}
+
+public func FxHighlightTimer(object target, int nr)
+{
+  //Erst mit dem Achievement starten
+  if(!EffectVar(3, target, nr)->IsRunning())
+    return true;
+
+  SetClrModulation(RGBa(255, 255, 255, EffectVar(0, target, nr)), target);
+  if(EffectVar(0, target, nr) <= 0)
+    EffectVar(1, target, nr) = +5;
+  else
+  if(EffectVar(0, target, nr) >= 150)
+    EffectVar(1, target, nr) = -5;
+
+  //Rotation
+  SetR(EffectVar(2, target, nr)++, target);
+
+  EffectVar(0, target, nr) += EffectVar(1,target, nr);
+  return true;
+}
+
+/* Ausblenden */
 
 protected func Fade()
 {
@@ -55,9 +94,11 @@ protected func Fade()
   if(iTime < 13*32)
   {
     SetClrModulation(RGBa(255,255,255,(iTime-(9*32))*2));
+    EffectVar(0, highlight, GetEffect("Highlight", highlight)) = (iTime-(9*32))*2;
     Desc((iTime-(9*32))*2);
     return true;
   }
+  RemoveObject(highlight);
   return RemoveObject(this);
 }
 
