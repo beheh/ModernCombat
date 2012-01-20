1bc71d363b60fe4c2fdb04ea77f5eecaeed27f8b
Michael14 <michael@missile.beheh.de>
2012-01-20 20:02:13 +0000

    * Kill-Assist durch Blendung/Sichtung: Bepunktung von 10 auf 20 Punkte erhöht. * Kill-Assist durch Peilsender: Bepunktung von 20 auf 30 Punkte erhöht.

--- ModernCombatFINALTRY.c4d/System.c4g/RewardSystem-Points.c
+++ ModernCombatFINALTRY.c4d/System.c4g/RewardSystem-Points.c
@@ -22,8 +22,8 @@ global func BonusPoints(string szType, extraData)
   if(szType == "Restocking")			return extraData/3;	//Munitionierung
   if(szType == "Protection")			return 10;		//Schildabwehr/Projektilabwehr
   if(szType == "TracerSet")			return 10;		//Peilsenderplazierung
-  if(szType == "TracerAssist")			return 20;		//Kill Assist durch Peilsender
-  if(szType == "VisualAssist")			return 10;		//Kill Assist durch Blendung/Sichtung
+  if(szType == "TracerAssist")			return 30;		//Kill Assist durch Peilsender
+  if(szType == "VisualAssist")			return 20;		//Kill Assist durch Blendung/Sichtung
   if(szType == "TechnicalTask")			return 20;		//Entschärfung
   if(szType == "VehicleDamage")			return 10;		//Beschädigung
   if(szType == "Destruction")			return 50;		//Fahrzeug-/Strukturzerstörung
