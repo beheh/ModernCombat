122c7769680a70b0105d404e7826419ba5e4727a
Michael14 <michael@missile.beheh.de>
2012-04-16 15:05:55 +0000

    * Script gesäubert.

--- ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/ArtilleryBattery.c4d/Crosshair.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/ArtilleryBattery.c4d/Crosshair.c4d/Script.c
@@ -9,33 +9,33 @@ local fActive, pArtillery, pClonk;
 
 public func Construction(object pByObj)
 {
-	pArtillery = pByObj;
-	return true;
+  pArtillery = pByObj;
+  return true;
 }
 
 public func Set(object pObj)
 {
-	pClonk = pObj;
-	return true;
+  pClonk = pObj;
+  return true;
 }
 
 public func Check()
 {
-	if(GetProcedure(pClonk) != "PUSH" || GetActionTarget(0, pClonk) != pArtillery)
-		return RemoveObject();
+  if(GetProcedure(pClonk) != "PUSH" || GetActionTarget(0, pClonk) != pArtillery)
+    return RemoveObject();
 
   if(GetPlrView(GetOwner()) != this)
   {
-  	SetPlrViewRange(0);
-  	SetVisibility(VIS_None);
-  	fActive = false;
+    SetPlrViewRange(0);
+    SetVisibility(VIS_None);
+    fActive = false;
   }
   else if(!fActive)
   {
-  	SetPlrViewRange(200);
-  	SetVisibility(VIS_Owner);
-  	fActive = true;
+    SetPlrViewRange(200);
+    SetVisibility(VIS_Owner);
+    fActive = true;
   }
-  
+
   return true;
 }
