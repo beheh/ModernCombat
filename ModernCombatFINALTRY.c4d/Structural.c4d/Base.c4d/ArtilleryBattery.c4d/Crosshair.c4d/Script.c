b84369bda68b027a38111ba68dd9a834c651ac42
ClonkGeist <clonkgeist@missile.beheh.de>
2012-04-16 15:01:11 +0000

    ! #010468 ("Desync: Zielen mit der Artillerie")

--- ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/ArtilleryBattery.c4d/Crosshair.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/ArtilleryBattery.c4d/Crosshair.c4d/Script.c
@@ -5,13 +5,37 @@
 
 /* Initalisierung */
 
-func Initialize()
+local fActive, pArtillery, pClonk;
+
+public func Construction(object pByObj)
 {
-  SetPlrViewRange(200);
+	pArtillery = pByObj;
+	return true;
 }
 
-func Check()
+public func Set(object pObj)
 {
+	pClonk = pObj;
+	return true;
+}
+
+public func Check()
+{
+	if(GetProcedure(pClonk) != "PUSH" || GetActionTarget(0, pClonk) != pArtillery)
+		return RemoveObject();
+
   if(GetPlrView(GetOwner()) != this)
-    RemoveObject();
+  {
+  	SetPlrViewRange(0);
+  	SetVisibility(VIS_None);
+  	fActive = false;
+  }
+  else if(!fActive)
+  {
+  	SetPlrViewRange(200);
+  	SetVisibility(VIS_Owner);
+  	fActive = true;
+  }
+  
+  return true;
 }
