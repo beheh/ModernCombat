9d46da06e13f1e1558c080868aa931c4ee0cea49
ClonkGeist <clonkgeist@missile.beheh.de>
2012-05-09 20:44:16 +0000

    ! #010608 ("Ränge: Fehlbeförderungen bei CTF")

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/RewardSystem-CTF.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/RewardSystem-CTF.c
@@ -9,11 +9,13 @@
 public func FlagScored(int iTeam, int iCTeam, object pClonk)
 {
   //Punkte bei Belohnungssystem (Flaggendiebstahl)
-  DoPlayerPoints(BonusPoints("CTFSteal"), RWDS_TeamPoints, GetOwner(pClonk), pClonk, IC08);
+  if(pClonk)
+    DoPlayerPoints(BonusPoints("CTFSteal"), RWDS_TeamPoints, GetOwner(pClonk), pClonk, IC08);
 }
 
 public func FlagReturned(int iTeam, object pClonk)
 {
   //Punkte bei Belohnungssystem (Flaggenrückeroberung)
-  DoPlayerPoints(BonusPoints("CTFRegain"), RWDS_TeamPoints, GetOwner(pClonk), pClonk, IC09);
+  if(pClonk)
+    DoPlayerPoints(BonusPoints("CTFRegain"), RWDS_TeamPoints, GetOwner(pClonk), pClonk, IC09);
 }
