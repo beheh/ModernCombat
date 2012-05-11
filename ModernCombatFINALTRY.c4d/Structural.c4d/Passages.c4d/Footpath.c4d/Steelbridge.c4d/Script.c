f02e60964adf0148a38f369c6e14c77455e24f6a
Michael14 <michael@missile.beheh.de>
2012-05-11 17:22:17 +0000

    ! Unterstützer und Antiskill besitzen vorläufig den Schweißbrenner, bis in dem Punkt eine Entscheidung gefallen ist. * Stahlbrücken nun reparierbar.

--- ModernCombatFINALTRY.c4d/Structural.c4d/Passages.c4d/Footpath.c4d/Steelbridge.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Passages.c4d/Footpath.c4d/Steelbridge.c4d/Script.c
@@ -2,34 +2,81 @@
 
 #strict 2
 
-public func IsBridge()		{return 1;}
-public func IsLocked()		{return true;}
+public func MaxDamage()		{return 150;}
+public func IsRepairable()	{return true;}
+
+local ruins, destroyed;
 
 
 /* Initialisierung */
 
 protected func Initialize()
 {
+  //Ruinen standardm‰ﬂig eingeschaltet
+  ruins = true;
+
   return 1;
+}
+/* Ruinen (de)aktivieren */
+
+protected func SwitchRuinsMode()
+{
+  if(ruins)
+    ruins = 0;
+  else
+    ruins = 1;
 }
 
 /* Zerstˆrung */
 
-func Damage()
+func Damage(int change)
 {
-  if(GetDamage() > 150)
+  if(GetDamage() > MaxDamage() && !destroyed)
   {
-   //Effekte
-   if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("ConcreteSplinter",4,150,0,0,40,100);
-   if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("ConcreteSplinter",8,230,0,0,40,15,RGB(40,20,20));
-   CastSmoke("Smoke3",12,10,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
-   Sound("StructuralDamage*.ogg");
+    destroyed = 1;
 
-   //Zum Wrack werden
-   ChangeDef(_HBB);
-   SetSolidMask();
+    //Effekte
+    if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("ConcreteSplinter",4,150,0,0,40,100);
+    if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("ConcreteSplinter",8,230,0,0,40,15,RGB(40,20,20));
+    CastSmoke("Smoke3",12,10,0,0,100,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+    Sound("StructuralDamage*.ogg");
 
-   //Verschwinden
-   FadeOut();
+    //Soll Ruine erstellen?
+    if(ruins)
+    {
+      //Aussehen ‰ndern
+      SetGraphics("Destroyed");
+    }
+    else
+    {
+      //Zum Wrack werden
+      ChangeDef(_HBB);
+
+      //SolidMask entfernen
+      SetSolidMask();
+
+      //Verschwinden
+      FadeOut();
+    }
+  }
+
+  if(destroyed)
+  {
+    //Schaden ¸ber Maximalwert verhindern
+    if(change > 0)
+    {
+      if(GetDamage() > MaxDamage())
+        DoDamage(-(GetDamage()-MaxDamage()));
+    }
+
+    //Reparatur abschlieﬂen
+    if(GetDamage() <= 0 && destroyed)
+    {
+      //Aussehen und SolidMask wiederherstellen
+      SetGraphics();
+      SetSolidMask(0,0,72,24);
+
+      destroyed = 0;
+    }
   }
 }
