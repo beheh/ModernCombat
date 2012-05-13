e3c8595d2c5e3c138d931b84367734deef0e84dd
Michael14 <michael@missile.beheh.de>
2012-05-13 13:46:14 +0000

    * Script gesäubert.

--- cmcrepos/ModernCombatFINALTRY.c4d/Crew.c4d/Medic.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Crew.c4d/Medic.c4d/Script.c
@@ -19,7 +19,7 @@ public func HazardGearSupported(object pGear)
 func Initialize()
 {
   //UpdateMediHUD();
-	AddEffect("EnergyBars", this, 101, 1, this);
+  AddEffect("EnergyBars", this, 101, 1, this);
   _inherited(...);
 }
 
@@ -41,49 +41,49 @@ func Initialize()
 
 public func FxEnergyBarsStart(object target, int nr)
 {
-	EffectVar(0, target, nr) = [];
-	return true;
+  EffectVar(0, target, nr) = [];
+  return true;
 }
 
 public func FxEnergyBarsTimer(object target, int nr)
 {
-	// Balken updaten
-	for(var bar in EffectVar(0, target, nr))
-	{
-		if(!bar)
-			continue;
-	
-		var actTarget = GetActionTarget(0, bar);
-		if(!actTarget || !(GetOCF(actTarget) & OCF_Alive) || Hostile(GetOwner(actTarget), GetOwner(target)) || Contained(actTarget) || GetCursor(GetOwner(target)) != target)
-			bar->Update(0, true);
-		else if(!GetPlayerName(GetOwner(actTarget)))
-			RemoveObject(bar);
-		else
-		{
-			var percent = BoundBy(100000 * GetEnergy(actTarget) / GetPhysical("Energy", PHYS_Current, actTarget), 0, 100);
-			
-			bar->Update(percent, (percent >= 95));
-		}
-	}
-	
-	for(var clonk in FindObjects(Find_OCF(OCF_Alive), Find_OCF(OCF_CrewMember), Find_NoContainer(), Find_Exclude(target), Find_Not(Find_Hostile(GetOwner(target)))))
-	{
-		if(FindObject2(Find_ID(SBAR), Find_ActionTarget(clonk), Find_Owner(GetOwner(target)), Find_Func("HasBarType", BAR_Energybar))) // Hat schon einen Balken?
-			continue;
-		
-		var bar = CreateObject(SBAR, 0, 0, GetOwner(target));
+  //Balken updaten
+  for(var bar in EffectVar(0, target, nr))
+  {
+    if(!bar)
+      continue;
+
+    var actTarget = GetActionTarget(0, bar);
+    if(!actTarget || !(GetOCF(actTarget) & OCF_Alive) || Hostile(GetOwner(actTarget), GetOwner(target)) || Contained(actTarget) || GetCursor(GetOwner(target)) != target)
+      bar->Update(0, true);
+    else if(!GetPlayerName(GetOwner(actTarget)))
+      RemoveObject(bar);
+    else
+    {
+      var percent = BoundBy(100000 * GetEnergy(actTarget) / GetPhysical("Energy", PHYS_Current, actTarget), 0, 100);
+
+      bar->Update(percent, (percent >= 95));
+    }
+  }
+
+  for(var clonk in FindObjects(Find_OCF(OCF_Alive), Find_OCF(OCF_CrewMember), Find_NoContainer(), Find_Exclude(target), Find_Not(Find_Hostile(GetOwner(target)))))
+  {
+    if(FindObject2(Find_ID(SBAR), Find_ActionTarget(clonk), Find_Owner(GetOwner(target)), Find_Func("HasBarType", BAR_Energybar))) //Hat schon einen Balken?
+      continue;
+
+    var bar = CreateObject(SBAR, 0, 0, GetOwner(target));
     bar->Set(clonk, RGB(80,190,255), BAR_Energybar, true, "", IC05);
     EffectVar(0, target, nr)[GetLength(EffectVar(0, target, nr))] = bar;
-	}
-	
-	return true;
+  }
+
+  return true;
 }
 
 public func FxEnergyBarsStop(object target, int nr)
 {
-	for(var bar in EffectVar(0, target, nr))
-		if(bar)
-			RemoveObject(bar);
+  for(var bar in EffectVar(0, target, nr))
+    if(bar)
+      RemoveObject(bar);
 }
 
 public func Death()
