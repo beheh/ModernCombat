57451af6b9ef3ef3e64dda44f2c736bc96f652c0
ClonkGeist <clonkgeist@missile.beheh.de>
2012-06-02 21:41:41 +0000

    ! #010711 ("Einsatzschild: Verursacht Fehlermeldung, Clonk wirft diesen weg.")

--- cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Tools.c4d/RiotShield.c4d/DrawnShield.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Tools.c4d/RiotShield.c4d/DrawnShield.c4d/Script.c
@@ -85,7 +85,7 @@ public func ExecShove()
     {
       DoDmg(15,DMG_Melee,victim,0,GetController()+1,RSHL);
       //Achievement-Fortschritt (Fist of Fury)
-      DoAchievementProgress(1, AC36, GetOwner(GetController())); 
+      DoAchievementProgress(1, AC36, GetController());
     }
     Sound("RSHL_Shove.ogg", 0, victim);
   }
