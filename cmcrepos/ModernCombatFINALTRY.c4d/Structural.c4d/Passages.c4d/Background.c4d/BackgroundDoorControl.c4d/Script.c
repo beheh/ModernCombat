6f894f5c88e14a9b3576590412136395dafb1ca6
Michael14 <michael@missile.beheh.de>
2012-06-06 19:05:39 +0000

    * Script gesäubert.

--- cmcrepos/ModernCombatFINALTRY.c4d/Structural.c4d/Passages.c4d/Background.c4d/BackgroundDoorControl.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Structural.c4d/Passages.c4d/Background.c4d/BackgroundDoorControl.c4d/Script.c
@@ -97,8 +97,8 @@ protected func RejectCollect(id objID, object pObj)
 
 protected func FxMove2DoorStart(object pTarget, int iNr, int iTemp)
 {
-	if(iTemp)
-		return false;
+  if(iTemp)
+    return false;
 
   var cont = Contained(pTarget);
   if(cont)
