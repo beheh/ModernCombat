37430c86192790900f289e825ce9cee9f0102f60
Michael14 <michael@missile.beheh.de>
2012-06-20 19:39:41 +0000

    * MAV aktualisiert.

--- CMC_Area51.c4s/MonkstarObjects.c4d/MAVStation.c4d/MAV.c4d/Script.c
+++ CMC_Area51.c4s/MonkstarObjects.c4d/MAVStation.c4d/MAV.c4d/Script.c
@@ -1,4 +1,4 @@
-/*--- Der Stein ---*/
+/*--- MAV ---*/
 
 #strict 2
 #include CVHC
@@ -10,40 +10,43 @@ local iYTendency;
 local iSpeed;
 
 public func AttractTracer(object pTracer)	{return GetPlayerTeam(GetController(pTracer)) != GetTeam() && !fDestroyed;}
-public func IsBulletTarget() {return !fDestroyed;}
-public func IsDestroyed() {return fDestroyed;}
+public func IsBulletTarget()			{return !fDestroyed;}
+public func IsDestroyed()			{return fDestroyed;}
+
+
+/* Initialisierung */
 
 protected func Initialize()
 {
-	iSpeed = 20;
-	SetAction("Idle");
+  iSpeed = 20;
+  SetAction("Idle");
+  Sound("MAVE_Engine.ogg", true, 0, 80);
 }
 
 public func OnDestruction()
 {
-	SetAction("Idle");
-	FakeExplode(5, GetController()+1, this);
-	FadeOut();
+  SetAction("Idle");
+  FakeExplode(5, GetController()+1, this);
+  FadeOut();
 }
 
 private func FlyingTimer()
 {
-	if(iXDir < iXTendency)
-		iXDir++;
-	if(iXDir > iXTendency)
-		iXDir--;
-	
-	if(iYDir < iYTendency)
-		iYDir++;
-	if(iYDir > iYTendency)
-		iYDir--;
-	
-	SetXDir(iXDir);
-	SetYDir(iYDir);
-	
-	SetPlrView(GetController(), this);
-	
-	
+  if(iXDir < iXTendency)
+    iXDir++;
+  if(iXDir > iXTendency)
+    iXDir--;
+
+  if(iYDir < iYTendency)
+    iYDir++;
+  if(iYDir > iYTendency)
+    iYDir--;
+
+  SetXDir(iXDir);
+  SetYDir(iYDir);
+
+  SetPlrView(GetController(), this);
+
   //Blinklicht (alle 30 Frames)
   if(!(GetActTime()%30))
   {
@@ -59,92 +62,92 @@ private func FlyingTimer()
 
 public func Idle(object pClonk)
 {
-	iXTendency = 0;
-	iYTendency = 0;
-	iXDir = 0;
-	iYDir = 0;
-	
-	SetPlrView(GetController(), pClonk);
+  iXTendency = 0;
+  iYTendency = 0;
+  iXDir = 0;
+  iYDir = 0;
+
+  SetPlrView(GetController(), pClonk);
 }
 
 /* Steuerung */
 
 public func ControlLeft(pByObj)
 {
-	if(iXTendency > 0)
-		iXTendency = 0;
-	else
-			iXTendency = -iSpeed;
-		
+  if(iXTendency > 0)
+    iXTendency = 0;
+  else
+      iXTendency = -iSpeed;
+
   return true;
 }
 
 public func ControlLeftDouble(pByObj)
 {
-	iXTendency = -iSpeed;
-		
+  iXTendency = -iSpeed;
+
   return true;
 }
 
 public func ControlLeftReleased(pByObj)
 {
-	iXTendency = 0;
+  iXTendency = 0;
   return true;
 }
 
 public func ControlRight(pByObj)
 {
-	if(iXTendency < 0)
-		iXTendency = 0;
-	else
-		iXTendency = iSpeed;
-		
+  if(iXTendency < 0)
+    iXTendency = 0;
+  else
+    iXTendency = iSpeed;
+
   return true;
 }
 
 public func ControlRightDouble(pByObj)
 {
-	iXTendency = iSpeed;
-		
+  iXTendency = iSpeed;
+
   return true;
 }
 
 public func ControlRightReleased(pByObj)
 {
-	iXTendency = 0;
+  iXTendency = 0;
   return true;
 }
 
 public func ControlDown(pByObj)
 {
-	if(iYTendency < 0)
-		iYTendency = 0;
-	else
-		iYTendency = iSpeed;
-			
+  if(iYTendency < 0)
+    iYTendency = 0;
+  else
+    iYTendency = iSpeed;
+
   return true;
 }
 
 public func ControlDownDouble(pByObj)
 {
-	iYTendency = iSpeed;
-		
+  iYTendency = iSpeed;
+
   return true;
 }
 
 public func ControlUp(object pByObj)
 {
-	if(iYTendency > 0)
-		iYTendency = 0;
-	else
-		iYTendency = -iSpeed;
-		
+  if(iYTendency > 0)
+    iYTendency = 0;
+  else
+    iYTendency = -iSpeed;
+
   return true;
 }
 
 public func ControlUpDouble(pByObj)
 {
-	iYTendency = -iSpeed;
-		
+  iYTendency = -iSpeed;
+
   return true;
 }
