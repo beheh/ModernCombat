dd6f4b651cd1444e08d4d6568ed1abb32ad04c4e
Michael14 <michael@missile.beheh.de>
2012-06-24 17:07:37 +0000

    ! Arena: Effekt wird nicht unendlich oft an Objekte vergeben, ein Check nach Contained() entfernt Arena-Effekte wieder, damit beispielsweise kurz abgelegte Objekte nicht mehr sofort verschwinden, sobald sie wieder im Freien sind (beispielsweise Granaten).

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Arena.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Arena.c
@@ -12,25 +12,31 @@ protected func Timer()
   //Objekte suchen
   for(var pObj in FindObjects(Find_Category(C4D_Object), Find_NoContainer(),
   							Find_Or(Find_Func("IsWeapon"),
-  								Find_Func("IsEquipment"),
-  								Find_Func("IsUpgrade")),
+  							Find_Func("IsEquipment"),
+  							Find_Func("IsUpgrade")),
   							Find_Not(Find_Func("IsMine")),
   							Find_Not(Find_Func("NoArenaRemove"))))
-    AddEffect("Arena_Remove", pObj, 1, 400, this);
+    if(!GetEffect("Arena_Remove", pObj))
+      AddEffect("Arena_Remove", pObj, 1, 50, this);
 
   //Spezialsuche: Drohnen
   for(pObj in FindObjects(Find_ID(DRSU), Find_NoContainer()))
     if(ActIdle(pObj))
-      AddEffect("Arena_Remove", pObj, 1, 530, this);
+      if(!GetEffect("Arena_Remove", pObj))
+        AddEffect("Arena_Remove", pObj, 1, 50, this);
 }
 
-public func FxArena_RemoveTimer(object pTarget)
+public func FxArena_RemoveTimer(object pTarget, int iEffectNumber, int iEffectTime)
 {
   //Abbruch bei neuer Aktion (Drohne)
   if(!pTarget->ActIdle())
     return(-1);
-  //Ansonsten löschen sofern nicht wieder aufgehoben
-  if(!(pTarget->Contained()))
+  //Oder wenn aufgehoben
+  if((pTarget->Contained()))
+    return(-1);
+
+  //Objekt nach Wartezeit entfernen
+  if(iEffectTime >= 530)
   {
     //Effekte
     pTarget->CastSmoke("Smoke3", 8, 3, 0, 0, 60, 80);
