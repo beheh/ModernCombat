0247b9ddeb4220d50f9a526ebad91da60600e967
Michael14 <michael@missile.beheh.de>
2012-07-10 12:15:59 +0000

    * Sendemast und Co.: Diverse kleinere Verbesserungen. * Script gesäubert.

--- cmcrepos/CMC_Area51.c4s/ClonkGeistObjects.c4d/AntennaTower.c4d/TowerPart .c4d/Script.c
+++ cmcrepos/CMC_Area51.c4s/ClonkGeistObjects.c4d/AntennaTower.c4d/TowerPart .c4d/Script.c
@@ -12,75 +12,65 @@ func Initialize()
   SetOwner(-1);
 }
 
+/* Schaden verursachen */
+
 public func FxDmgObjectsTimer(object target, int nr, int time)
 {
-	if((GetXDir() < 20 && GetYDir() < 20) || Stuck())
-	{
-		if(GetEffectData(EFSM_ExplosionEffects) == 1)
-		{
-			EffectVar(1, target, nr)++;
-			if(EffectVar(1, target, nr) > 90) // nach ~15 Sekunden ohne zu bewegen ausfaden
-				FadeOut();
-		}
-		
-		return false;
-	}
-	
-	EffectVar(1, target, nr) = 0;
-	
-	if(GetType(EffectVar(0, target, nr)) != C4V_Array)
-		EffectVar(0, target, nr) = [];
-	
-	var wdt = GetObjWidth()+2;
-	var hgt = GetObjHeight()+2;
-	var objects = FindObjects(Find_AtRect(-wdt/2, -hgt/2, wdt, hgt), Find_NoContainer());
-	for(var obj in objects)
-	{
-		if(!obj)
-			continue;
-	
-		if(GetIndexOf(obj, EffectVar(0, target, nr)) > -1)
-			continue;
+  if((GetXDir() < 20 && GetYDir() < 20) || Stuck())
+  {
+    EffectVar(1, target, nr)++;
+    //Bei keiner bewegung in 15 Sekunden verschwinden
+    if(EffectVar(1, target, nr) > RandomX(90,110))
+      FadeOut();
 
-		if((GetCategory(obj) & C4D_Structure || GetCategory(obj) & C4D_Vehicle) && obj->~GetPartDamage())
-			DoDmg(obj->~GetPartDamage(), DMG_Melee, obj, 10, -1);
-		else
-			DoDmg(Max(GetXDir(),GetYDir()), DMG_Melee, obj, 10, -1);
-		
-		if(GetCategory(obj) & C4D_Living)
-			obj->SetAction("Tumble");
-	}
-	EffectVar(0, target, nr) = objects;
+    return false;
+  }
+
+  EffectVar(1, target, nr) = 0;
+
+  if(GetType(EffectVar(0, target, nr)) != C4V_Array)
+    EffectVar(0, target, nr) = [];
+
+  var wdt = GetObjWidth()+2;
+  var hgt = GetObjHeight()+2;
+  var objects = FindObjects(Find_AtRect(-wdt/2, -hgt/2, wdt, hgt), Find_NoContainer());
+  for(var obj in objects)
+  {
+    if(!obj)
+      continue;
+
+    if(GetIndexOf(obj, EffectVar(0, target, nr)) > -1)
+      continue;
+
+    if((GetCategory(obj) & C4D_Structure || GetCategory(obj) & C4D_Vehicle) && obj->~GetPartDamage())
+      DoDmg(obj->~GetPartDamage(), DMG_Melee, obj, 10, -1);
+    else
+      DoDmg(Max(GetXDir(),GetYDir()), DMG_Melee, obj, 10, -1);
+
+    if(GetCategory(obj) & C4D_Living)
+      obj->SetAction("Tumble");
+  }
+  EffectVar(0, target, nr) = objects;
 }
+
+/* Aufschlag */
 
 public func Hit(int iXDir, int iYDir)
 {
-	Sound("StructuralDamage*.ogg");
+  Sound("StructuralDamage*.ogg");
 
-	if(GetEffectData(EFSM_ExplosionEffects) > 1)
-	{
-		var x = GetX(), y = GetY(), xdir, ydir;
-		SimFlight(x, y, xdir, ydir, 50, 100);
-		var mat = GetMaterial(x, y);
-	
-		if(mat == Material("Vehicle")) mat = Material("Wall");
-  	var clr_offset = Random(3)*3;
-  	var clr = RGB(Min(GetMaterialVal("Color", "Material", mat, 0 + clr_offset), 255), 
-  								Min(GetMaterialVal("Color", "Material", mat, 1 + clr_offset), 255), 
-  								Min(GetMaterialVal("Color", "Material", mat, 2 + clr_offset), 255));
+  var x = GetX(), y = GetY(), xdir, ydir;
+  SimFlight(x, y, xdir, ydir, 50, 100);
+  var mat = GetMaterial(x, y);
 
-	
-		CastParticles("GroundSmoke", RandomX(4,12), 80, AbsX(x), AbsY(y)-1, (iXDir + iYDir) / 4, (iXDir + iYDir) / 2, clr, clr);
-		CastParticles("ConcreteSplinter", RandomX(8,16), 80, AbsX(x), AbsY(y)-1, 20, 50);
-	}
-	
-	return true;
+  if(mat == Material("Vehicle")) mat = Material("Wall");
+  var clr_offset = Random(3)*3;
+  var clr = RGB(Min(GetMaterialVal("Color", "Material", mat, 0 + clr_offset), 255), 
+  		Min(GetMaterialVal("Color", "Material", mat, 1 + clr_offset), 255), 
+  		Min(GetMaterialVal("Color", "Material", mat, 2 + clr_offset), 255));
+
+  CastParticles("GroundSmoke", RandomX(4,12), 80, AbsX(x), AbsY(y)-1, (iXDir + iYDir) / 4, (iXDir + iYDir) / 2, clr, clr);
+  if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("ConcreteSplinter", RandomX(8,16), 80, AbsX(x), AbsY(y)-1, 20, 50);
+
+  return true;
 }
-
-
-
-
-
-
-
-
