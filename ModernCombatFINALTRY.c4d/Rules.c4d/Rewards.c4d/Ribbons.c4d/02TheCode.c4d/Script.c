08999a432e946eb15df40752aec22f130c7405e9
Michael14 <michael@missile.beheh.de>
2012-07-12 20:31:59 +0000

    * Ribbons: Namen aktualisiert.

--- ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Ribbons.c4d/02TheCode.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Ribbons.c4d/02TheCode.c4d/Script.c
@@ -1,11 +1,12 @@
-/*-- Weighty Commander Ribbon --*/
+/*-- The Code --*/
 
 #strict 2
 #include RBBN
 
+
 public func GetSavingSlot()
 {
-  return 1;	//Laufende Nummer einfügen
+  return 2;	//Laufende Nummer einfügen
 }
 
 public func GetRibbonScore()
