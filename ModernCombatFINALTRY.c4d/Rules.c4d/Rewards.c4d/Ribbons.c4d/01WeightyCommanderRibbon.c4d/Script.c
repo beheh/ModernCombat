20b1acf192fa234f5c287b561dba954e7d005dde
Michael14 <michael@missile.beheh.de>
2012-07-12 20:20:18 +0000

    + Ehrenbänder eingeführt: Michael, B_E. * Beschreibungen eingefügt. * Script nicht gesäubert.

---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Ribbons.c4d/01WeightyCommanderRibbon.c4d/Script.c
@@ -0,0 +1,14 @@
+/*-- Weighty Commander Ribbon --*/
+
+#strict 2
+#include RBBN
+
+public func GetSavingSlot()
+{
+  return 1;	//Laufende Nummer einfügen
+}
+
+public func GetRibbonScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Ribbons.c4d/02DatRocket.c4d/Script.c
@@ -0,0 +1,14 @@
+/*-- Dat Rocket --*/
+
+#strict 2
+#include RBBN
+
+public func GetSavingSlot()
+{
+  return 2;	//Laufende Nummer einfügen
+}
+
+public func GetRibbonScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
