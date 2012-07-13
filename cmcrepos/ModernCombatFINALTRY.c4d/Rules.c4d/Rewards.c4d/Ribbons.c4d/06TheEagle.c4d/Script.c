9e3efceefed47bdb4471d753ab69d44918c9f5aa
Michael14 <michael@missile.beheh.de>
2012-07-13 14:34:16 +0000

    + Ehrenbänder eingeführt: Wasserwipf, Cmdr. Adler, Chris.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Ribbons.c4d/06TheEagle.c4d/Script.c
@@ -0,0 +1,15 @@
+/*-- The Eagle --*/
+
+#strict 2
+#include RBBN
+
+
+public func GetSavingSlot()
+{
+  return 6;	//Laufende Nummer einfügen
+}
+
+public func GetRibbonScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
