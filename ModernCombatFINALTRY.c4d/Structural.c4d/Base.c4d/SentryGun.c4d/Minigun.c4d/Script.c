df3535a8c008c8fb670b46b8858d4580102e9e5f
Michael14 <michael@missile.beheh.de>
2012-07-14 22:33:17 +0000

    * Minigun: Reichweite von 300 auf 350 erhöht. * Minigun: Schussfrequenz verlangsamt, Schaden pro Kugel erhöht. * Minigun: Echo eingefügt. * Script gesäubert.

--- ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/SentryGun.c4d/Minigun.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/SentryGun.c4d/Minigun.c4d/Script.c
@@ -14,38 +14,41 @@ public func NoWeaponChoice()	{return true;}
 
 public func FMData1(int data)
 {
-  if(data == FM_AmmoID)    return STAM;
-  if(data == FM_AmmoLoad)  return 9999;
+  if(data == FM_AmmoID)		return STAM;
+  if(data == FM_AmmoLoad)	return 9999;
 
-  if(data == FM_Reload)    return 2;
-  if(data == FM_Recharge)  return 3;
+  if(data == FM_Reload)		return 2;
+  if(data == FM_Recharge)	return 5;
 
-  if(data == FM_Auto)      return 1;
+  if(data == FM_Auto)		return 1;
+
+  if(data == FM_Damage)		return 7;
 
   return Default(data);
 }
 
 public func Fire1()
 {
+  //Austritt bestimmen
   var user = GetUser();
   var angle = user->AimAngle()+RandomX(-3,+3);
   var dir = GetDir(user)*2-1;
-  
   var iX;
   var iY;
-  
   user->WeaponEnd(iX,iY);
-  
-  var ammo = CreateObject(SHTX, iX, iY, GetController(user));
-  ammo->Launch(angle, 300, 600, 2, 100, 3, 1);
 
-  // Effekte
+  //Kugel abfeuern
+  var ammo = CreateObject(SHTX, iX, iY, GetController(user));
+  ammo->CustomLaunch(angle, 350, 650, 2, GetFMData(FM_Damage) * 10, GetFMData(FM_Damage));
+
+  //var ammo = CreateObject(SHTX, iX, iY, GetController(user));
+  //ammo->Launch(angle, 300, 600, 2, 100, 3, 1);
+
+  //Effekte
   MuzzleFlash(40+Random(60),user,iX,iY,angle);
   BulletCasing(dir*1,0,-dir*14*(Random(1)+1),-(13+Random(2)),7,0,true);
-
-  // Sound
   Sound("MISA_Fire.ogg",0,ammo);
-}
+  Echo("ACCN_Echo.ogg");}
 
 func OnAutoStop(int iFM)
 {
