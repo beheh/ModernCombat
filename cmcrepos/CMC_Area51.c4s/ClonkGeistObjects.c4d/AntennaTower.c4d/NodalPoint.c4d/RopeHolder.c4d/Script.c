d7a6926ff4c4e78b34921ea317db6844fdba3dc8
Michael14 <michael@missile.beheh.de>
2012-07-15 20:25:43 +0000

    * Script gesäubert.

--- cmcrepos/CMC_Area51.c4s/ClonkGeistObjects.c4d/AntennaTower.c4d/NodalPoint.c4d/RopeHolder.c4d/Script.c
+++ cmcrepos/CMC_Area51.c4s/ClonkGeistObjects.c4d/AntennaTower.c4d/NodalPoint.c4d/RopeHolder.c4d/Script.c
@@ -17,6 +17,7 @@ public func NodeDestroyed(object pRopeAttachment)
 
   //Effekte
   if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("MetalSplinter",4,100,0,0,20,70,RGB(250,0,0));
+  if(GetEffectData(EFSM_ExplosionEffects) > 0) CastSmoke("Smoke3",6,15,0,0,250,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
 }
 
 public func SetGraphic(int iGraphic)
