88a4dabd1ed057bd1285323170e4dd1fd09ef299
Michael14 <michael@missile.beheh.de>
2012-07-16 12:34:23 +0000

    * Sendemast: Echo bei Zerstörung, um global darauf hinzuweisen. - Maststück: GetPartDamage entfernt.

--- CMC_Area51.c4s/ClonkGeistObjects.c4d/System.c4g/_HBR.c
+++ CMC_Area51.c4s/ClonkGeistObjects.c4d/System.c4g/_HBR.c
@@ -1,7 +1,8 @@
-/*-- Neues Script --*/
+/*-- Neues Script --
 
 #strict 2
 
 #appendto _HBR
 
 public func GetPartDamage() { return 200; } // Schaden, den ein Maststück an diesem Objekt hier anrichtet. 
+*/
