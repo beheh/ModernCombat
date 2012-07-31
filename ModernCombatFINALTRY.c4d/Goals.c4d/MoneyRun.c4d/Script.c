f4c899677591aa8311a9e1f7c510cb1212e58e6b
Michael14 <michael@missile.beheh.de>
2012-07-31 22:59:20 +0000

    * MoneyRun: StringTbl vervollständigt. * Script gesäubert.

--- ModernCombatFINALTRY.c4d/Goals.c4d/MoneyRun.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Goals.c4d/MoneyRun.c4d/Script.c
@@ -15,7 +15,7 @@ public func GoalExtraValue()	{return iGoal;}	//Spielzielinformationen an Scorebo
 
 protected func Initialize()
 {
-	aMessage = [];
+  aMessage = [];
 
   //Standard-Credit-Anzahl ermitteln
   if(FrameCounter() < 100)
@@ -320,26 +320,26 @@ private func GoalMoney(int iPlr, int iAmount)
     string = Format("{{IC20}} <c ff0000>%d</c>", iAmount);
   if (pClonk)
     AddEffect("PointMessage", pClonk, 130, 1, pClonk, 0, string);
-    
+
   var iWarningMoney = iGoal - Min(32 * (iGoal/2) / 100, 100);
   if(aMoney[index] >= iWarningMoney - (iWarningMoney % 10))
   {
-  	if(!aMessage[index])
-  	{
-  		aMessage[index] = true;
-  		for(var i = 0; i < GetPlayerCount(); i++)
-  		{
-  			var plr = GetPlayerByIndex(i);
-  			if(Teams() && GetPlayerTeam(plr) != GetPlayerTeam(iPlr) && GetTeamPlayerCount(GetPlayerTeam(iPlr)) > 1)
-  				EventInfo4K(plr+1, Format("$EnemyTeamIsWinning$", GetTaggedTeamName(GetPlayerTeam(iPlr))));
-  			else if((!Teams() || GetTeamPlayerCount(GetPlayerTeam(iPlr)) == 1) && plr != iPlr)
-  				EventInfo4K(plr+1, Format("$EnemyPlayerIsWinning$", GetTaggedPlayerName(iPlr)));
-  		}
-  	}
+    if(!aMessage[index])
+    {
+      aMessage[index] = true;
+      for(var i = 0; i < GetPlayerCount(); i++)
+      {
+        var plr = GetPlayerByIndex(i);
+        if(Teams() && GetPlayerTeam(plr) != GetPlayerTeam(iPlr) && GetTeamPlayerCount(GetPlayerTeam(iPlr)) > 1)
+          EventInfo4K(plr+1, Format("$EnemyTeamIsWinning$", GetTaggedTeamName(GetPlayerTeam(iPlr))));
+        else if((!Teams() || GetTeamPlayerCount(GetPlayerTeam(iPlr)) == 1) && plr != iPlr)
+          EventInfo4K(plr+1, Format("$EnemyPlayerIsWinning$", GetTaggedPlayerName(iPlr)));
+      }
+    }
   }
   else
-  	aMessage[index] = false;
-  	
+    aMessage[index] = false;
+
   //Und Änderung zurückgeben
   return aMoney[index] - iMoney;
 }
