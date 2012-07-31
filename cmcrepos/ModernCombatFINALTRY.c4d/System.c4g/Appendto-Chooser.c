843cb3b0d69deeff5d4ac07983f690404bdb098f
Michael14 <michael@missile.beheh.de>
2012-07-31 19:53:37 +0000

    * Script gesäubert.

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Chooser.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Chooser.c
@@ -888,33 +888,33 @@ protected func ConfigurationFinished2()
     log = "$StdRules$";
   else
     log = "$Rules$";
-    
+
   var activated = "";
   var deactivated = "";
   var optional = "";
   var comma = "";
   for(var check in aRules)
   {
-  	def = GetDefinition(i, Chooser_Cat);
-  	i++;
-  	//Log("aRules: %v, def: %i, i: %d, check: %v, Name: %s", aRules, def, i, check, GetName(0, def));
-  	if(check)
-  		CreateObject(def, 10, 10, -1);
-  	
-  	if(!(GetCategory(0, def) & C4D_Rule))
-  		continue;
-  	
-  	var fStandard = (GetIndexOf(def, GameCall("ChooserRuleConfig")) != -1);
-  	if(!fStandard && !check)
-  		continue;
-  	else if(fStandard && !check)
-  		deactivated = Format("%s%s<c %x>%s</c>", deactivated, comma, GetRuleColor(def, true), GetName(0, def));
-  	else if(fStandard)
-  		activated = Format("%s%s<c %x>%s</c>", activated, comma, GetRuleColor(def), GetName(0, def));
-  	else
-  		optional = Format("%s%s<c %x>%s</c>", optional, comma, GetRuleColor(def), GetName(0, def));
-  	
-  	comma = ", ";
+    def = GetDefinition(i, Chooser_Cat);
+    i++;
+    //Log("aRules: %v, def: %i, i: %d, check: %v, Name: %s", aRules, def, i, check, GetName(0, def));
+    if(check)
+      CreateObject(def, 10, 10, -1);
+
+    if(!(GetCategory(0, def) & C4D_Rule))
+      continue;
+
+    var fStandard = (GetIndexOf(def, GameCall("ChooserRuleConfig")) != -1);
+    if(!fStandard && !check)
+      continue;
+    else if(fStandard && !check)
+      deactivated = Format("%s%s<c %x>%s</c>", deactivated, comma, GetRuleColor(def, true), GetName(0, def));
+    else if(fStandard)
+      activated = Format("%s%s<c %x>%s</c>", activated, comma, GetRuleColor(def), GetName(0, def));
+    else
+      optional = Format("%s%s<c %x>%s</c>", optional, comma, GetRuleColor(def), GetName(0, def));
+
+    comma = ", ";
   }
   //Zusammenfassen
   log = Format("%s%s%s%s", log, activated, deactivated, optional);
@@ -966,7 +966,7 @@ private func GetRuleColor(id idDef, bool fNotChoosed)
   if(GetIndexOf(idDef, GameCall("ChooserRuleConfig")) == -1)
     return RGB(255);
   if(fNotChoosed)
-  	return RGB(127, 127, 127);
+    return RGB(127, 127, 127);
 
   return RGB(255,255,255);
 }
