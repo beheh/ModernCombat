72162abacb10d9685c9930908fc2b5013892acf0
Michael14 <michael@missile.beheh.de>
2012-08-01 17:34:15 +0000

    + Sendemast eingeführt. + Achievement hinzugefügt: "Tower Defence". * Seil: Kann nun gefärbt werden. * Radios springen nicht mehr umher, wenn sie aufschlagen. * Beschreibungen verbessert. * Script gesäubert.

---
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Barriers.c4d/Cover.c4d/AntennaTower.c4d/TowerPart .c4d/Script.c
@@ -0,0 +1,82 @@
+/*-- Maststück --*/
+
+#strict 2
+
+
+/* Initalisierung */
+
+func Initialize()
+{
+  SetAction("Be");
+  AddEffect("DmgObjects", this, 101, 5, this);
+}
+
+/* Schaden verursachen */
+
+public func FxDmgObjectsTimer(object target, int nr, int time)
+{
+  if((GetXDir() < 20 && GetYDir() < 20) || Stuck())
+  {
+    EffectVar(1, target, nr)++;
+    //Bei keiner bewegung in 15 Sekunden verschwinden
+    if(EffectVar(1, target, nr) > 200)
+      FadeOut();
+
+    return false;
+  }
+
+  EffectVar(1, target, nr) = 0;
+
+  if(GetType(EffectVar(0, target, nr)) != C4V_Array)
+    EffectVar(0, target, nr) = [];
+
+  var wdt = GetObjWidth()+2;
+  var hgt = GetObjHeight()+2;
+  var objects = FindObjects(Find_AtRect(-wdt/2, -hgt/2, wdt, hgt), Find_NoContainer());
+  for(var obj in objects)
+  {
+    if(!obj)
+      continue;
+
+    if(GetIndexOf(obj, EffectVar(0, target, nr)) > -1)
+      continue;
+
+    //Schadenswert errechnen
+    DoDmg(Max(GetXDir(),GetYDir()), DMG_Melee, obj, 10, GetOwner());
+
+    if(GetCategory(obj) & C4D_Living)
+    {
+      obj->SetAction("Tumble");
+      if(Hostile(GetOwner(),GetOwner(obj)))
+        if(!GetAlive(obj) || IsFakeDeath(obj))
+          //Achievement-Fortschritt (Tower Defence)
+          DoAchievementProgress(1, AC40, GetOwner());
+    }
+  }
+  EffectVar(0, target, nr) = objects;
+}
+
+/* Aufschlag */
+
+public func Hit(int iXDir, int iYDir)
+{
+  Sound("StructuralDamage*.ogg");
+
+  //Spielerbildschirme schütteln
+  ShakeViewPort(50, this);
+
+  var x = GetX(), y = GetY(), xdir, ydir;
+  SimFlight(x, y, xdir, ydir, 50, 100);
+  var mat = GetMaterial(x, y);
+
+  if(mat == Material("Vehicle")) mat = Material("Wall");
+  var clr_offset = Random(3)*3;
+  var clr = RGB(Min(GetMaterialVal("Color", "Material", mat, 0 + clr_offset), 255), 
+  		Min(GetMaterialVal("Color", "Material", mat, 1 + clr_offset), 255), 
+  		Min(GetMaterialVal("Color", "Material", mat, 2 + clr_offset), 255));
+
+  CastParticles("GroundSmoke", RandomX(4,12), 80, AbsX(x), AbsY(y)-1, (iXDir + iYDir) / 4, (iXDir + iYDir) / 2, clr, clr);
+  if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("ConcreteSplinter", RandomX(8,16), 80, AbsX(x), AbsY(y)-1, 20, 50);
+
+  return true;
+}
--- ModernCombatFINALTRY.c4d/Vehicles.c4d/BlackHawk.c4d/Rope.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Vehicles.c4d/BlackHawk.c4d/Rope.c4d/Script.c
@@ -6,9 +6,21 @@ local iLength, iCalcedLength, iOCF, iVtx1, iVtx2;
 local aPointsX, aPointsY;
 local pPulley;
 local fNoPickUp_0, fNoPickUp_1;
+local dwcolor;
+
+public func SetRopeColor(int dwClr)	{return dwcolor = dwClr;}
+public func GetRopeLength()		{return iLength;}
+public func SetRopeLength(iNewLength)	{return iLength = Max(iNewLength, 0);}
+func IsAnvilProduct()			{return 1;}
+
+
+/* Initialisierung */
 
 protected func Initialize()
 {
+  //Standardfarbe des Seils
+  dwcolor = RGBa(100, 50, 0, 50);
+
   // OCF's, an die verbunden werden kann
   iOCF = OCF_Living | OCF_Grab | OCF_Chop | OCF_Collectible;
 
@@ -17,19 +29,22 @@ protected func Initialize()
   return 1;
 }
 
+/* Verbinden */
+
 public func ConnectObjects(pObj1, pObj2)
 {
-	var p1 = pObj1->~GetRopeAttach(this);
-	if(p1) pObj1 = p1;
-	var p2 = pObj2->~GetRopeAttach(this);
-	if(p2) pObj2 = p2;
+  var p1 = pObj1->~GetRopeAttach(this);
+  if(p1) pObj1 = p1;
+  var p2 = pObj2->~GetRopeAttach(this);
+  if(p2) pObj2 = p2;
   iVtx1 = GetMiddlestVertex(pObj1);
   iVtx2 = GetMiddlestVertex(pObj2);
   SetPoint(0, GetX(pObj1) + GetVertex (iVtx1, 0, pObj1), GetY(pObj1) + GetVertex(iVtx1, 1, pObj1));
   SetPoint(1, GetX(pObj2) + GetVertex (iVtx2, 0, pObj2), GetY(pObj2) + GetVertex(iVtx2, 1, pObj2));
   SetAction("Connect", pObj1, pObj2);
   iLength = CalcLength();
-  // Seil als "Hilfsobjekt" verstecken
+
+  //Seil als Hilfsobjekt verstecken
   SetCategory(C4D_StaticBack);
   SetObjDrawTransform(1, 0, 0, 0, 1, 0);
   while(Contained()) Exit();
@@ -72,29 +87,29 @@ private func SetPoint(int iIndex, int iX, int iY)
 
 public func CalcLength(pTarget1, pTarget2)
 {
-  // Anzahl Paare
+  //Anzahl Paare
   var i = GetPointNum() - 1;
   var iCount = 0;
-  // für jedes Paar berechnen
+  //für jedes Paar berechnen
   while(i-- > 0) iCount += VerticeLength(i, pTarget1, pTarget2);
   return iCount;
 }
 
 private func VerticeLength(i, pTarget1, pTarget2)
 {
-  // erster Vertex: Geschwindigkeit von 1.Actiontarget berücksichtigen
+  //Erster Vertex: Geschwindigkeit von erstem Actiontarget berücksichtigen
   if(i == 0) return Distance(
     GetPoint(0) + GetXDir(pTarget1) / 2, 
     GetPoint(0, 1) + GetYDir(pTarget1) / 2,
     GetPoint(1),
     GetPoint(1, 1));
-  // vorletzter Vertex: Geschwindigkeit von 2.Actiontarget berücksichtigen
+  //Vorletzter Vertex: Geschwindigkeit von  zweitem Actiontarget berücksichtigen
   if(i == (GetPointNum() - 2)) return Distance(
     GetPoint(i),
-    GetPoint(i, 1),          
+    GetPoint(i, 1),
     GetPoint(i + 1) + GetXDir(pTarget2) / 2, 
     GetPoint(i + 1, 1) + GetYDir(pTarget2)) / 2;
-  // Ansonsten nur die Positionen
+  //Ansonsten nur die Positionen
   return Distance(
     GetPoint(i),
     GetPoint(i, 1),
@@ -105,7 +120,7 @@ private func VerticeLength(i, pTarget1, pTarget2)
 private func Timer()
 {
   var i, iAmount;
-  // Partikel-Effekt
+  //Partikeleffekt
   while(i < GetPointNum() - 1)
   {
     var iAngle1 = Angle(GetPoint(i), GetPoint(i, 1), GetPoint(i + 1), GetPoint(i + 1, 1));
@@ -114,16 +129,18 @@ private func Timer()
     var iY1 = GetPoint(i, 1) - GetY() + Cos(iAngle1, 2);
     var iX2 = GetPoint(i + 1) - GetX() - Sin(iAngle2, 2);
     var iY2 = GetPoint(i + 1, 1) - GetY() + Cos(iAngle2, 2);
-    iAmount += DrawParticleLine2("Rope", iX1, iY1, iX2, iY2, 5, 40, RGBa(100, 50, 0, 50));
+    iAmount += DrawParticleLine2("Rope", iX1, iY1, iX2, iY2, 5, 40, dwcolor);
     i++;
   }
 }
 
+/* Seil zeichnen */
+
 private func DrawParticleLine2 (szKind, x0, y0, x1, y1, prtdist, a, b0, b1, iYDir)
 {
-  // Parameter gültig?
+  //Parameter gültig?
   if (!prtdist) return 0;
-  // Anzahl der benötigten Partikel berechnen
+  //Anzahl der benötigten Partikel berechnen
   var prtnum = Max(Distance(x0, y0, x1, y1) / prtdist, 2);
   var i = prtnum;
   var iXDir = Sin(Angle(x0, y0, x1, y1), 10);
@@ -131,7 +148,7 @@ private func DrawParticleLine2 (szKind, x0, y0, x1, y1, prtdist, a, b0, b1, iYDi
   b1 = b0;
   var i1, i2, b;
   var szPart;
-  // Partikel erzeugen!
+  //Partikel erzeugen
   while(i>-1)
   {
     i2 = i * 256 / prtnum;
@@ -144,14 +161,14 @@ private func DrawParticleLine2 (szKind, x0, y0, x1, y1, prtdist, a, b0, b1, iYDi
     if(!i) szPart = "Rope2";
     CreateParticle(szPart, x0 + (x1 - x0) * i / prtnum, y0 + (y1 - y0) * i-- / prtnum, iXDir, iYDir, a, b, this);
   }
-  // Erfolg; Anzahl erzeugter Partikel zurückgeben
+  //Erfolg; Anzahl erzeugter Partikel zurückgeben
   return prtnum;
 }
 
 func MoveRope(pObj1, pObj2)
 {
   var a, b;
-  // Wir brauchen mindesten 3 Vertices, damit die Algorithmen funktionieren
+  //Mindestens 3 Vertices, damit die Algorithmen funktionieren
   if(GetPointNum () < 3)
     InsertPoint(1, (GetPoint(0) + GetPoint(1)) / 2, (GetPoint(0, 1) + GetPoint(1, 1)) / 2);
   SetRopeEnd(pObj1, 0, iVtx1, 2);
@@ -159,21 +176,21 @@ func MoveRope(pObj1, pObj2)
   var iVtxDist = 7 + 3 * GetPointNum();
   for(var i = GetPointNum() - 2; i > 0; i--)
   {
-    // Neue Position des Vertices
+    //Neue Position des Vertices
     a = (GetPoint(i - 1) + GetPoint(i + 1)) / 2;
     b = (GetPoint(i - 1, 1) + GetPoint(i + 1, 1)) / 2 + 1;
-    // Überschüssige Vertices, die nahe genug aneinander sind entfernen
+    //Überschüssige Vertices, die nahe genug aneinander sind, entfernen
     if(GetPointNum () > 3)
       if(Distance(GetPoint(i - 1, 0), GetPoint(i - 1, 1), GetPoint(i + 1, 0), GetPoint(i + 1, 1)) < iVtxDist)
         if(PathFree(GetPoint(i - 1, 0), GetPoint(i - 1, 1), GetPoint(i + 1, 0), GetPoint(i + 1, 1)))
           { RemovePoint(i); continue; }
-    // bei zu langem Abstand einen neuen Vertex einfügen
+    //Bei zu langem Abstand einen neuen Vertex einfügen
     if(Distance(GetPoint(i - 1, 0), GetPoint(i - 1, 1), GetPoint(i, 0), GetPoint(i, 1)) > (7 + iVtxDist))
     {
       InsertPoint(i, (GetPoint(i - 1, 0) + GetPoint(i, 0)) / 2, (GetPoint(i - 1, 1) + GetPoint(i, 1)) / 2);
       continue;
     }
-    // Wenn durch verschieben des Vertex das Seil durch solides ginge, nicht verschieben, und bei genug Platz einen zusätzlichen Vertex einfügen
+    //Wenn durch Verschieben des Vertex das Seil durch solides ginge, nicht verschieben, und bei genug Platz einen zusätzlichen Vertex einfügen
     if(!PathFree(a, b, GetPoint(i - 1), GetPoint(i - 1, 1)))
     {
       if(Distance(GetPoint (i - 1, 0), GetPoint(i - 1, 1), GetPoint(i, 0), GetPoint(i, 1)) > iVtxDist)
@@ -196,9 +213,9 @@ func SetRopeEnd(pObj, iRopeVtx, iObjVtx, iWhichEnd)
   var x, y;
   x = GetX(pObj) + GetVertex(iObjVtx, 0, pObj);
   y = GetY(pObj) + GetVertex(iObjVtx, 1, pObj);
-  // Wenn durch die Bewegung des angebundenen Objekts das Seil durch solides
-  // ginge, einen Vertex an der letzten Position des Objekts einfügen
-  // wenn das Objekt selbst in fester Materie steckt, hülfe das auch nichts
+  //Wenn durch die Bewegung des angebundenen Objekts das Seil durch solides
+  //ginge, einen Vertex an der letzten Position des Objekts einfügen
+  //wenn das Objekt selbst in fester Materie steckt, hülfe das auch nichts
   if(!GBackSolid(x - GetX(), y - GetY()))
   {
     if(!PathFree(x, y, GetPoint(iRopeVtx + iWhichEnd, 0), GetPoint(iRopeVtx + iWhichEnd, 1)))
@@ -219,7 +236,7 @@ private func InsertPoint(iIndex, x, y)
 
 func Connecting()
 {
-  // Wenn angeknüpfte Objekte verloren gehen: Seil löschen
+  //Wenn angeknüpfte Objekte verloren gehen: Seil löschen
   if(!GetActionTarget(0) || !GetActionTarget(1)) return RemoveObject();
   var pTarget1 = GetRealContainer(GetActionTarget(0));
   var pTarget2 = GetRealContainer(GetActionTarget(1));
@@ -231,19 +248,19 @@ func Connecting()
   iLength = Max(iLength + GetActionTarget(1)->~RopeAskChangeLength(iDifference, this), 3);
   iDifference = iCalcedLength - iLength;
   if(iDifference <= 0) return 0;
-  // bei zu kurzer Seillänge nicht mehr ziehen, um Aufschaukeln zu verhindern
+  //Bei zu kurzer Seillänge nicht mehr ziehen, um Aufschaukeln zu verhindern
   if(iCalcedLength < 5) return 0;
   // freie, verbundene Seilrollen ziehen sich nicht gegenseitig an
   if(GetID(pTarget1) == CL5P && GetID(pTarget2) == CL5P && !Contained(pTarget1) && !Contained(pTarget2))
     return 0;
-  // wenn eine Seilrolle Seil gibt, dann in Ruhe lassen, außer, sie dämpft
-  // den Fall von Clonks oder das Seil es einfach tun soll
+  //Wenn eine Seilrolle Seil gibt, dann in Ruhe lassen, außer, sie dämpft
+  //Den Fall von Clonks oder das Seil es einfach tun soll
   if(LocalN("iPull", pPulley) > 0 && Contained(pPulley))
     if(GetProcedure(Contained(pPulley)) != "FLIGHT")
       return 0;
-  if(Local(8,this)) //Geheime lokahle.
+  if(Local(8,this))
     return 0;
-  // Ansonsten Objekte zueinander bewegen
+  //Ansonsten Objekte zueinander bewegen
   PullObject((3 * GetPoint(1) + GetPoint(2)) / 4, (3 * GetPoint(1, 1) + GetPoint(2, 1)) / 4, iDifference, pTarget1, pTarget2, iVtx1);
   PullObject((GetPoint(GetPointNum() - 3) + 3 * GetPoint(GetPointNum() - 2)) / 4, (GetPoint(GetPointNum() - 3, 1) + 3 * GetPoint(GetPointNum() - 2, 1)) / 4, iDifference, pTarget2, pTarget1, iVtx2);
 
@@ -262,30 +279,28 @@ func Connecting()
 */
 }
 
-private func PullObject(iToX, iToY, iLength, pObj, pObj2, iVtx) // pObj wird versetzt
+private func PullObject(iToX, iToY, iLength, pObj, pObj2, iVtx)	//pObj wird versetzt
 {
   if(!pObj || !pObj2) return 0;
-  // Außnahmen:
-  // will nicht :(
   if(pObj->~RejectPull(this)) return;
-  // gefesselte Clonks lassen sich abführen
+  //Gefesselte Clonks lassen sich abführen
   if(GetEffect("IntTied", pObj2))
     return 0;
-  // Gebäude (oder Inhaltsobjekte) sowie Hintergrundobjekte gar nicht ziehen
+  //Gebäude (oder Inhaltsobjekte) sowie Hintergrundobjekte gar nicht ziehen
   if(GetCategory(pObj) & C4D_Structure || GetCategory(pObj) & C4D_StaticBack)
     return 0;
-  // Objekt aus dem eigenen Grafikbereich heraus nicht ziehen, führt nur zu komischen Effekten
+  //Objekt aus dem eigenen Grafikbereich heraus nicht ziehen
   for(var pObj3 in FindObjects(Find_AtPoint(GetX(pObj2) + GetVertex(pObj2->~GetVertexToConnect(), 0, pObj2), GetY(pObj2) + GetVertex(pObj2->~GetVertexToConnect(), 1, pObj2))))
     if(pObj3 == pObj)
       return 0;
-  // Anker ziehen keine Schiffe nach unten
+  //Anker ziehen keine Schiffe nach unten
   if(pObj->~IsBoat() && GetActionTarget() && GetActionTarget(1))
   {
     if((GetActionTarget() == pObj && GetActionTarget(1)->~IsAnchor() && !Contained(GetActionTarget(1)))
     || (GetActionTarget(1) == pObj && GetActionTarget()->~IsAnchor() && !Contained(GetActionTarget())))
       return 0;
   }
-  // Floateinfluss: z.B. Luftschiffe können heben
+  //Float-Einfluss: z.B. Luftschiffe können heben
   if(GetProcedure(pObj) == "FLOAT")
     iLength = Max(0, iLength - (GetPhysical("Float", 0, pObj) / 20));
 
@@ -317,7 +332,7 @@ private func PullObject(iToX, iToY, iLength, pObj, pObj2, iVtx) // pObj wird ver
     SetRDir(GetRDir(pObj, 100) + (iAngle - iAngle2) / 10, pObj, 100);
   if(iLength > 30)
   SetPosition(GetX(pObj) + BoundBy(x, -1, 1), GetY(pObj) - BoundBy(y, -1, 1), pObj);
-  //Falls Objekt dann feststecken würde, nicht wirklich versetzen
+  //Falls das Objekt dann feststecken würde, nicht wirklich versetzen
   if(Stuck(pObj))
     SetPosition(GetX(pObj) - BoundBy(x, -1, 1), GetY(pObj) + BoundBy(y, -1, 1), pObj);
  }
@@ -333,10 +348,10 @@ private func GetMiddlestVertex(pObj)
 {
   var i, s = 1000, t, n;
   if(n = pObj->~GetVertexToConnect()) return n - 1;
-  // alle Vertices durchgehen
+  //Alle Vertices durchgehen
   for(i = GetVertexNum(pObj); i >= 0; i--)
   {
-    // x-Differenz zur Mitte + y-Differenz (eigentlich müsste ich Distance benutzen)
+    //x-Differenz zur Mitte + y-Differenz
     t = (Abs(GetVertex(i, 0, pObj)) + Abs(GetVertex(i, 1, pObj)));
     //s: kürzester Abstand bis jetzt
     if(s > t)
@@ -349,17 +364,16 @@ private func GetMiddlestVertex(pObj)
   return n;
 }
 
-// Ändert die angeknüpften Objekte
 public func SetObject(pObj, iActionTarget)
 {
-  // 1. Actiontarget
+  //1. Actiontarget
   if(!iActionTarget)
   {
-    // Action und 2.Actiontarget bleiben erhalten
+    //Action und 2.Actiontarget bleiben erhalten
     iVtx1 = GetMiddlestVertex(pObj);
     SetAction("Connect", pObj, GetActionTarget(1));
   }
-  // 2.Actiontarget
+  //2.Actiontarget
   else
   {
     iVtx2 = GetMiddlestVertex(pObj);
@@ -368,10 +382,8 @@ public func SetObject(pObj, iActionTarget)
   return 1;
 }
 
-public func GetRopeLength() { return iLength; }
-public func SetRopeLength(iNewLength) { return iLength = Max(iNewLength, 0); }
+/* Einsammeln als Hintergrundobjekt verhindenr */
 
-// nicht einsammelbar, wenn Hintergrundobjekt
 private func RejectEntrance(pObj)
 {
   if(GetCategory() & C4D_StaticBack)
@@ -384,8 +396,6 @@ protected func Hit()
   Sound("WoodHit*");
   return 1;
 }
-
-func IsAnvilProduct() { return 1; }
 
 global func GetRealContainer(pObj)
 {
