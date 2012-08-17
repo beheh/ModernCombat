64dcf2f1931497625d823f95e1af091ecb9a02b1
Michael14 <michael@missile.beheh.de>
2012-08-17 17:07:59 +0000

    * Script gesäubert.

--- ModernCombatFINALTRY.c4d/System.c4g/Appendto-CaptureTheFlag.c
+++ ModernCombatFINALTRY.c4d/System.c4g/Appendto-CaptureTheFlag.c
@@ -41,34 +41,34 @@ private func SetFlagMarker(int iPlr, int iCTeam, bool fRemove)
 
 public func InitializePlayer(int iPlr, int iX, int iY, object pBase, int iTeam)
 {
-	if(GetType(aFlagData) == C4V_Array && aFlagData[iTeam] && !aFlagData[iTeam][3])
-		CreateFlag(iTeam, aFlagData[iTeam][0], aFlagData[iTeam][1], aFlagData[iTeam][2]);
-	
-	return _inherited(iPlr, iX, iY, pBase, iTeam, ...);
+  if(GetType(aFlagData) == C4V_Array && aFlagData[iTeam] && !aFlagData[iTeam][3])
+    CreateFlag(iTeam, aFlagData[iTeam][0], aFlagData[iTeam][1], aFlagData[iTeam][2]);
+
+  return _inherited(iPlr, iX, iY, pBase, iTeam, ...);
 }
 
 public func RemovePlayer(int iPlr)
 {
-	var iTeam = GetPlayerTeam(iPlr);
+  var iTeam = GetPlayerTeam(iPlr);
 
-	if(GetTeamPlayerCount(iTeam) == 1 && GetType(aFlagData) == C4V_Array && aFlagData[iTeam])
-	{
-		if(aFlagData[iTeam][3])
-			RemoveObject(aFlagData[iTeam][3]);
-		
-		var flag = aFlagData[iTeam][4];
-		if(flag)
-		{
-			var actTarget = GetActionTarget(0, flag);
-			if(actTarget)
-				SetFlagMarker(GetOwner(actTarget), 0, true);
+  if(GetTeamPlayerCount(iTeam) == 1 && GetType(aFlagData) == C4V_Array && aFlagData[iTeam])
+  {
+    if(aFlagData[iTeam][3])
+      RemoveObject(aFlagData[iTeam][3]);
 
-			LocalN("fForcedDestruction", flag) = true;
-			RemoveObject(flag);
-		}
-	}
-	
-	return _inherited(iPlr, ...);
+    var flag = aFlagData[iTeam][4];
+    if(flag)
+    {
+      var actTarget = GetActionTarget(0, flag);
+      if(actTarget)
+        SetFlagMarker(GetOwner(actTarget), 0, true);
+
+      LocalN("fForcedDestruction", flag) = true;
+      RemoveObject(flag);
+    }
+  }
+
+  return _inherited(iPlr, ...);
 }
 
 local aFlagData;
@@ -77,30 +77,29 @@ local aFlagData;
 
 global func CreateFlag(int iTeam, int iX, int iY, int iColor) // Platziert eine Flagge für Team iTeam, Position und Farbe angeben
 {
-	var ctf = FindObject2(Find_ID(GCTF));
-	if(!ctf)
-		return;
-	
-	var flagdata = LocalN("aFlagData", ctf);
-	if(!flagdata)
-		flagdata = [];
-	
-	//Daten speichern
-	flagdata[iTeam] = [iX, iY, iColor];
-	LocalN("aFlagData", ctf) = flagdata;
-	
-	//Existiert das Team überhaupt?
-	if(!GetTeamPlayerCount(iTeam))
-		return;
+  var ctf = FindObject2(Find_ID(GCTF));
+  if(!ctf)
+    return;
+
+  var flagdata = LocalN("aFlagData", ctf);
+  if(!flagdata)
+    flagdata = [];
+
+  //Daten speichern
+  flagdata[iTeam] = [iX, iY, iColor];
+  LocalN("aFlagData", ctf) = flagdata;
+
+  //Existiert das Team überhaupt?
+  if(!GetTeamPlayerCount(iTeam))
+    return;
 
   var pBase = CreateObject(FLBS, AbsX(iX), AbsY(iY), -1);
   var pFlag = CreateObject(FLA2, AbsX(iX), AbsY(iY), -1);
   pFlag->Activate(pBase, iTeam, iColor);
   flagdata[iTeam][3] = pBase;
   flagdata[iTeam][4] = pFlag;
-  
- 	LocalN("aFlagData", ctf) = flagdata;
-  
+
+  LocalN("aFlagData", ctf) = flagdata;
+
   ctf->InitTeam(iTeam);
 }
-
