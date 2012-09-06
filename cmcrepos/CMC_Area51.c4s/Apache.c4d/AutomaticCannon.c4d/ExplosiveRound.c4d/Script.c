c4e68f642bff85bf4baa91246b163a615a209668
Michael14 <michael@missile.beheh.de>
2012-09-06 20:29:19 +0000

    * MAV: StringTbl korrigiert. - Apache: Doppelte Sounds entfernt. * Apache: Gewehrgranaten der Sekundärwaffe mit Mindestabstand, um Selbstschädigung zu verhindern.

--- cmcrepos/CMC_Area51.c4s/Apache.c4d/AutomaticCannon.c4d/ExplosiveRound.c4d/Script.c
+++ cmcrepos/CMC_Area51.c4s/Apache.c4d/AutomaticCannon.c4d/ExplosiveRound.c4d/Script.c
@@ -5,5 +5,5 @@
 
 public func Color()		{return RGB(255,0,0);}
 public func BlastRadius()	{return 10;}
-protected func SecureDistance()	{return 0;}
+protected func SecureDistance()	{return 20;}
 func ExplodeDelay()		{return 25*3;}
