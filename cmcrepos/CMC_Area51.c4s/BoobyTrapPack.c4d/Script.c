e2c6d5f98d1d726d171bf003452c66da44e20903
Monkstar <monkstar@missile.beheh.de>
2012-09-23 11:00:49 +0000

    Sprengfallenpack eingebaut.

---
+++ cmcrepos/CMC_Area51.c4s/BoobyTrapPack.c4d/Script.c
@@ -0,0 +1,100 @@
+/*-- Sprengfallenpack --*/
+
+#strict 2
+#include PACK
+
+local controller, thrown;
+
+
+public func IsDrawable()		{return true;}
+public func CanAim()			{return Contained();}
+public func Color()			{return RGB(200,200,200);}
+public func HandX()			{return 5000;}
+public func HandSize()			{return 1000;}
+public func BarrelXOffset()		{return -850;}
+public func IsEquipment()		{return true;}
+public func NoArenaRemove()		{return true;}
+public func AttractTracer()		{return false;}
+public func LimitationCount()		{return 1;}
+public func MaxPoints()		{return 2;}
+public func StartPoints()	{return 2;}
+
+/* Steuerung */
+
+public func ControlThrow(object caller)
+{
+	if(GetPlrDownDouble(GetOwner(caller)))
+    return _inherited(...);
+
+  //Wird nicht getragen: Werfen gesperrt
+  if(!Contained() || !GetPackPoints()) return true;
+  
+  CreateContents(BBTP, this, 1)->ControlThrow(this);
+  DoPackPoints(-1);
+  
+  return true;
+}
+
+/* Schaden */
+
+protected func Damage(int iChange) 
+{
+  //Kein Schaden nehmen wenn gehalten und eventuelles Feuer löschen
+  if(Contained())
+  {
+    if(OnFire())
+    {
+      Extinguish();
+      return;
+    }
+    else
+    return;
+  }
+}
+
+public func OnDmg(int iDmg, int iType)
+{
+  if(iType == DMG_Fire) return 90;	//Feuer
+  if(iType == DMG_Bio)	return 100;	//Säure und biologische Schadstoffe
+}
+
+/* Allgemein */
+
+protected func Selection()
+{
+  Sound("BBTP_Charge.ogg");
+  return 1;
+}
+
+func Hit()
+{
+  Sound("BBTP_Hit*.ogg");
+}
+
+public func JoinPack(object pInto, object pMsgObj)
+{
+  var i = _inherited(pInto, pMsgObj);
+
+  pInto->~Check();
+  Check();
+  return i;
+}
+
+protected func Check()
+{
+  if(thrown) return;
+
+  //Grund zum Existieren?
+  if (!GetPackPoints())
+  {
+    thrown = true;
+
+    RemoveObject(this);
+  }
+}
+
+public func RejectEntrance()
+{
+  if(thrown)
+    return true;
+}
---
+++ cmcrepos/CMC_Area51.c4s/System.c4g/ClassSelection-Classes.c
@@ -0,0 +1,20 @@
+/*-- Standardklassen des Klassenwählers --*/
+
+//Stellt die Standardklassen für CMC bereit.
+
+#strict 2
+#appendto MCSL
+
+/* Der Artillerist */
+
+private func Class5Info(int iData, int iPlr)
+{
+  if(iData == CData_Name)       return "$artillerist$";
+  if(iData == CData_Desc)       return "$artillerist_desc$";
+  if(iData == CData_Clonk)      return PCMK;
+  if(iData == CData_Ammo)       return [[STAM, 50], [GRAM, 25]];
+  if(iData == CData_Items)      return [[SGST, 1], [PSTL, 1], [BTPK, 1]];
+  if(iData == CData_Grenades)   return [[PGRN, 2], [FGRN, 2]];
+  if(iData == CData_Facet)      return 5;
+  return Default(iData);
+}
