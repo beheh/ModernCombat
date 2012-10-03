d64e289c56dba5d6c3caca0144bab2865d633b04
Michael14 <michael@missile.beheh.de>
2012-10-03 12:23:29 +0000

    * Achievements hinzugefügt: Fly Swatter (42) und Can't Touch This (41). * Script gesäubert.

---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/41Can'tTouchThis.c4d/Script.c
@@ -0,0 +1,14 @@
+/*-- Can't Touch This --*/
+
+#strict 2
+#include ACHV
+
+public func GetSavingSlot()
+{
+  return 41;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
