497d87b091aac80801c0cf88b2bb31b9fca964eb
ClonkGeist <clonkgeist@missile.beheh.de>
2012-10-13 22:45:23 +0000

    ! #011600 ("Beförderung: Scoreboard korrumpiert")

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Team.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Team.c
@@ -224,14 +224,3 @@ public func TeamGetScore(int iTeam)
 {
   return aPoints[iTeam];
 }
-
-public func OnPlayerRankUp(int iPlr)
-{
-  //Scoreboard-Spielerränge aktualisieren
-  if(GetTeamPlayerCount(GetPlayerTeam(iPlr)) > 1)
-    SetScoreboardData(iPlr, TEAM_PlayerColumn, GetTaggedPlayerName(iPlr, true));
-  else
-    SetScoreboardData(iPlr, TEAM_TeamColumn, GetTaggedPlayerName(iPlr, true), GetPlayerTeam(iPlr));
-
-  return true;
-}
