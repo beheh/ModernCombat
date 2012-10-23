9e30e75349827e454f0330b049b01cc34bfd700c
Michael14 <michael@missile.beheh.de>
2012-10-23 20:46:54 +0000

    * Script gesäubert.

--- cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Jumppad.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/System.c4g/Appendto-Jumppad.c
@@ -1,13 +1,25 @@
-/*--  Jumpad-Kompatibilität mit Fallschirmen und Wandsprung --*/
+/*--  Sprungschanze --*/
+
+//Jumpad-Kompatibilität mit Fallschirmen und Wandsprung.
 
 #strict 2
 
 #appendto JPTP
 
-private func Bounce(object pClonk) {
+
+private func Bounce(object pClonk)
+{
   var pParachute = FindObject2(Find_ID(PARA), Find_ActionTarget(pClonk));
-  if(pParachute) pParachute->Close(); //alten Fallschirm schließen
-  RemoveEffect("Flying", pClonk); //neuen Fallschirm erlauben
-  RemoveEffect("IntWallJump", pClonk); //Wandsprung verhindern
+
+  //Fallschirm schließen sofern vorhanden
+  if(pParachute)
+    pParachute->Close();
+
+  //Neuen Fallschirm erlauben
+  RemoveEffect("Flying", pClonk);
+
+  //Eventuell machbaren Wandsprung verhindern
+  RemoveEffect("IntWallJump", pClonk);
+
   return inherited(pClonk);
 }
