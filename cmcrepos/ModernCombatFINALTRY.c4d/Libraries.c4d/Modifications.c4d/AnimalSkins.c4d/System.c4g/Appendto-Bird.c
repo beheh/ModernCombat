3aad2a874ad99217c7beb831577663b0a393029d
Michael14 <michael@missile.beheh.de>
2012-10-24 20:51:18 +0000

    * Script gesäubert. * Tier-Skin-Grafiken komprimiert. * Neuen Ordner mit Beschreibungen versehen.

---
+++ cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/AnimalSkins.c4d/System.c4g/Appendto-Bird.c
@@ -0,0 +1,18 @@
+/*-- Vogel --*/
+
+//Vögeln wird zufällig eine andere Grafik zugewiesen.
+
+#strict 2
+
+#appendto BIRD
+
+
+/* Initialisierung */
+
+public func Initialize()
+{
+  if(!Random(20))
+    SetGraphics(0, this, BDSN);
+
+  return inherited();
+}
