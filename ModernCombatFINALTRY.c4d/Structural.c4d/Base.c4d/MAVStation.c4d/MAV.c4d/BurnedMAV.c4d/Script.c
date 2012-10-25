9dfcd2c6b0c8c27fa05ff0db7ad9d43d47a7f5db
Monkstar <monkstar@missile.beheh.de>
2012-10-25 18:43:44 +0000

    MAV: Zerstörtes MAV hinzugefügt -> kein Anfassen mehr.

---
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/MAVStation.c4d/MAV.c4d/BurnedMAV.c4d/Script.c
@@ -0,0 +1,39 @@
+/*--- Burned MAV ---*/
+
+#strict 2
+#include CVHC
+
+local cur_Attachment;
+local iXDir, iYDir;
+local iXTendency, iYTendency;
+local iBank;
+local iBankMax;
+local iSpeed;
+local fIsAiming;
+local iAimAngle;
+local iPat_Dir;
+local crosshair;
+local pLaser;
+local pBeam;
+local iC4Count;
+local pItem;
+local iItemType;		//0: Standard (Sensorball), 1: MTP, 2: EHP, 3: Schweißbrenner, 4: Einsatzschild, 5: Sprengfalle, 6: Defibrillator
+local iHKShots;
+local living_dmg_cooldown;	//Gehört zum Schweißbrenner, ist aber praktischer, wenn direkt hier gespeichert
+local ChargeBar;
+
+public func IsDestroyed()				{return true;}
+public func IsMachine()					{return true;}
+public func IsMAV()					{return true;}
+
+protected func Hit2()
+{
+  //Effekte
+  Sound("HeavyHit*.ogg");
+}
+
+protected func Initialize()
+{
+  SetAction("Destroyed");
+  FadeOut();
+}
