94813da32cf025b14924f3a1f8d7d3f216c9397c
Michael14 <michael@missile.beheh.de>
2012-12-01 18:00:13 +0000

    * Script gesäubert. - Szenario in Training Center umbenannt.

--- CMC_EAT-03_REMAKE.c4s/Script.c
+++ CMC_EAT-03_REMAKE.c4s/Script.c
@@ -1,8 +1,9 @@
-/*-- EAT-03 --*/
+/*-- Training Center --*/
 
 #strict 2
 
 static const TUTORIAL_Lobby = TLBY;
+
 
 /* Initialisierung */
 
@@ -12,64 +13,69 @@ public func Initialize()
 
   //Nachrichtensystem aktivieren
   SetTutorialMessagePos(MSG_Top | MSG_Left | MSG_WidthRel | MSG_XRel, 15, 50, 50);
-  
- 	Schedule(Format("LoadTutorial(%i)", TUTORIAL_Lobby), 15);
+
+  //Lade Sektion: Lobby
+  Schedule(Format("LoadTutorial(%i)", TUTORIAL_Lobby), 15);
 
   return(1);
 }
 
+/* Auswahlmenü */
+
 public func TutorialChooser(object pByObj)
 {
-	// Tutorialauswahlmenü
-	
-	CreateMenu(CSTR, pByObj, 0, C4MN_Extra_None, "Tutorialwahl", 0, C4MN_Style_Context);
-	var i, def;
-	while(def = GetDefinition(i++, C4D_Goal))
-	{
-		if(def->~IsCMCTutorialGoal() && !def->~HideInMenu())
-		{
-			if(!def->Condition(pByObj))
-				AddMenuItem(Format("<c 777777>%s</c>", GetName(0, def)), 0, def, pByObj);
-			else
-				AddMenuItem(GetName(0, def), Format("LoadTutorial(%i, Object(%d))", def, ObjectNumber(pByObj)), def, pByObj, 0, 0, GetDesc(0, def));
-		}
-	}
-	AddMenuItem("$Cancel$", Format("CloseMenu(Object(%d))", ObjectNumber(pByObj)), CHOS, pByObj);
-	
-	return true;
+  //Tutorial-Auswahlmenü öffnen
+
+  CreateMenu(CSTR, pByObj, 0, C4MN_Extra_None, "Tutorialwahl", 0, C4MN_Style_Context);
+  var i, def;
+  while(def = GetDefinition(i++, C4D_Goal))
+  {
+    if(def->~IsCMCTutorialGoal() && !def->~HideInMenu())
+    {
+      if(!def->Condition(pByObj))
+        AddMenuItem(Format("<c 777777>%s</c>", GetName(0, def)), 0, def, pByObj);
+      else
+        AddMenuItem(GetName(0, def), Format("LoadTutorial(%i, Object(%d))", def, ObjectNumber(pByObj)), def, pByObj, 0, 0, GetDesc(0, def));
+    }
+  }
+  AddMenuItem("$Cancel$", Format("CloseMenu(Object(%d))", ObjectNumber(pByObj)), CHOS, pByObj);
+
+  return true;
 }
 
-static g_TutorialObject; // object - Das Tutorialobjekt was momentan aktiviert ist.
+/* Sektion laden */
+
+static g_TutorialObject;	//object - Das Tutorialobjekt, welches aktuell aktiviert ist
 
 public func LoadTutorial(id idTutObj, object pByObj)
 {
-	while(GetEffect("FakeScriptCounter"))
-		RemoveEffect("FakeScriptCounter");
-		
-	g_fakescriptcounter = 0;
+  while(GetEffect("FakeScriptCounter"))
+    RemoveEffect("FakeScriptCounter");
 
-	if(!(idTutObj->Condition(pByObj)))
-		return false;
-	
-	if(!idTutObj->SectionName())
-		return FatalError("No section name given!");
+  g_fakescriptcounter = 0;
 
-	SECT_ChangeSection(idTutObj->SectionName(), FindObjects(Find_ID(PCMK)));
-	g_TutorialObject = CreateObject(idTutObj);
-	
-	for(var i = 0; i < GetPlayerCount(); i++)
-		g_TutorialObject->InitializePlayer(i);
-		
-	for(var id in g_TutorialObject->UsedRules())
-		CreateObject(id);
-		
-	g_TutorialObject->CreateInterior();
-	g_TutorialObject->CreateEquipment();
-	g_TutorialObject->CreateDecoration();
-	g_TutorialObject->CreateOptionalFeatures();
-	g_TutorialObject->CreateTriggers();
-	
-	return true;
+  if(!(idTutObj->Condition(pByObj)))
+    return false;
+
+  if(!idTutObj->SectionName())
+    return FatalError("No section name given!");
+
+  SECT_ChangeSection(idTutObj->SectionName(), FindObjects(Find_ID(PCMK)));
+  g_TutorialObject = CreateObject(idTutObj);
+
+  for(var i = 0; i < GetPlayerCount(); i++)
+    g_TutorialObject->InitializePlayer(i);
+
+  for(var id in g_TutorialObject->UsedRules())
+    CreateObject(id);
+
+  g_TutorialObject->CreateInterior();
+  g_TutorialObject->CreateEquipment();
+  g_TutorialObject->CreateDecoration();
+  g_TutorialObject->CreateOptionalFeatures();
+  g_TutorialObject->CreateTriggers();
+
+  return true;
 }
 
 //public func RelaunchPlayer(int iPlr, object pCrew, object pKiller, int iTeam, bool bFirst) { g_TutorialObject->RelaunchPlayer(iPlr, pCrew, pKiller, iTeam, bFirst); }
