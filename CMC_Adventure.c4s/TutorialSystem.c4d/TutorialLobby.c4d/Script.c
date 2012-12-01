69284ba63675c90967b7d976c64251f03744c861
Michael14 <michael@missile.beheh.de>
2012-12-01 20:19:21 +0000

    * Lobby erneuert.

--- CMC_Adventure.c4s/TutorialSystem.c4d/TutorialLobby.c4d/Script.c
+++ CMC_Adventure.c4s/TutorialSystem.c4d/TutorialLobby.c4d/Script.c
@@ -4,35 +4,84 @@
 
 #include TSYS
 
-public func UsedRules() { return [NOFD, NOFF, NOAM]; } 
-public func SectionName() { return "Lobby"; } 
-public func HideInMenu() { return true; } 
+public func UsedRules()		{return [NOFD, NOFF, NOAM];}
+public func SectionName()	{return "$SectionName$";}
+public func HideInMenu()	{return true;}
+
+
+/* Initialisierung */
 
 public func Initialize()
 {
-	SetPlayList("CMC_Base Groove.ogg;CMC_Moving Squad.ogg");
+  //Starttitel und Musikliste zusammenstellen
+  SetPlayList("CMC_Base Groove.ogg;CMC_Firehawk.ogg;CMC_Friendly Unit.ogg;CMC_Getaway.ogg;CMC_Deep Universe.ogg;CMC_Eurocorps.ogg;CMC_Moving Squad.ogg;CMC_Offensive.ogg;CMC_Rock Go On.ogg;CMC_Showtime.ogg;CMC_Slow Motion.ogg;CMC_Striking Force.ogg;CMC_No Good.ogg;CMC_Obsession.ogg;CMC_Your Eyes.ogg");
   Music("CMC_Base Groove.ogg");
+
+  //Script starten
   FakeScriptGo(true);
-  
+
   return _inherited(...);
 }
 
-public func InitializePlayer(int iPlr)
-{
-	if(GetType(arRespawn[iPlr]) != C4V_Array)
-		arRespawn[iPlr] = [];
-	
-	SetRespawnPosition(RandomX(120, 180), 280, iPlr);
-	
-	SetArrow(265, 265, iPlr);
-	
-	return _inherited(iPlr, ...);
-}
+/* Plazierungslisten */
 
 public func CreateInterior()
 {
-	AutoRespawn(1500,0,CreateObject(PBRL,705,53,-1));
-  AutoRespawn(1500,0,CreateObject(TRGT,728,51,-1));
+  //Wahlraum
+  CreateObject(EXIT,1855,510,-1);
+
+  //Aufzüge
+  CreateObject(LFTP, 1935, 445, -1);
+  CreateObject(LFTP, 2585, 285, -1);
+
+  //Leitern
+  CreateObject(LADR, 1855, 750, -1)->Set(19);
+  CreateObject(LADR, 2525, 750, -1)->Set(39);
+
+  //Schutztüren
+  CreateObject(GDDR, 1690, 590, -1);
+  CreateObject(GDDR, 2100, 590, -1);
+  CreateObject(GDDR, 2550, 430, -1);
+  CreateObject(GDDR, 2750, 430, -1);
+
+  //Glasscheiben
+  CreateObject(_WIN, 1788, 481, -1);
+  CreateObject(_WIN, 1788, 510, -1);
+  CreateObject(_WIN, 2102, 481, -1);
+  CreateObject(_WIN, 2102, 510, -1);
+
+  CreateObject(_WIN, 2548, 321, -1);
+  CreateObject(_WIN, 2548, 350, -1);
+  CreateObject(_WIN, 2752, 321, -1);
+  CreateObject(_WIN, 2752, 350, -1);
+
+  //Sandsackbarriere
+  CreateObject(SBBA, 1300, 590, -1);
+
+  //Grenzen
+  CreateObject(BRDR, 1060, 0, -1)->Set(0);
+  CreateObject(BRDR, 3270, 0, -1)->Set(1);
+
+  //Verbundene Räume
+  var doorw = CreateObject(ROM2, 1990, 510, -1);
+  CreateObject(ROM2, 2640, 350, -1)->Connect(doorw);
+
+  var doorw = CreateObject(ROM2, 1990, 590, -1);
+  CreateObject(ROM2, 2640, 430, -1)->Connect(doorw);
+
+  //Sounds
+
+  //Wind
+  CreateObject(SE4K, 1430, 370, -1)->Set("WindSound*.ogg",775,250);
+  CreateObject(SE4K, 2200, 220, -1)->Set("WindSound*.ogg",775,250);
+  CreateObject(SE4K, 3200, 160, -1)->Set("WindSound*.ogg",775,250);
+
+  //Hallen
+  CreateObject(SE4K, 1980, 480, -1)->Set("Interior*.ogg",665,105);
+  CreateObject(SE4K, 2650, 320, -1)->Set("Interior*.ogg",670,105);
+
+/*
+  AutoRespawn(1500,0,CreateObject(PBRL,705,53,-1));
   CreateObject(LBDR,96,90,-1);
   CreateObject(LADR,241,121,-1)->Set(4);
   CreateObject(HA4K,241,91,-1);
@@ -89,13 +138,39 @@ public func CreateInterior()
   AutoRespawn(1500,0,CreateObject(HBRL,644,346,-1));
   AutoRespawn(1500,0,CreateObject(HBRL,658,344,-1));
   AutoRespawn(1500,0,CreateObject(TRGT,690,345,-1));
+*/
   
   return true;
 }
 
 public func CreateEquipment()
 {
-	PlaceSpawnpoint(PSTL,220,280);
+  //Zielscheiben
+  AutoRespawn(1500,0,CreateObject(TRGT,2340,490,-1));
+  AutoRespawn(1500,0,CreateObject(TRGT,2370,610,-1));
+  AutoRespawn(1500,0,CreateObject(TRGT,2410,540,-1));
+
+  AutoRespawn(1500,0,CreateObject(TRGT,3090,230,-1));
+  AutoRespawn(1500,0,CreateObject(TRGT,3100,300,-1));
+  AutoRespawn(1500,0,CreateObject(TRGT,3150,350,-1));
+  AutoRespawn(1500,0,CreateObject(TRGT,3200,410,-1));
+
+  //Versorgungskiste (Sturmgewehr)
+  var crate = CreateObject(AMCT, 2120, 590, -1);
+  crate->Set(ASTR);
+
+  //Versorgungskiste (Schrotflinte)
+  crate = CreateObject(AMCT, 2770, 430, -1);
+  crate->Set(PPGN);
+
+  //Versorgungskisten (Dragnin)
+  crate = CreateObject(AMCT, 1990, 430, -1);
+  crate->Set(DGNN);
+  crate = CreateObject(AMCT, 2640, 270, -1);
+  crate->Set(DGNN);
+
+/*
+  PlaceSpawnpoint(PSTL,220,280);
 
   PlaceSpawnpoint(SMGN,120,160);
   PlaceSpawnpoint(SGST,150,160);
@@ -108,17 +183,48 @@ public func CreateEquipment()
 
   CreateObject(AMCT, 260, 130, -1)->Set(ABOX);
   CreateObject(AMCT, 300, 130, -1)->Set(GBOX);
+*/
   
   return true;
 }
 
+func CreateDecoration()
+{
+  //Denkmal
+  CreateObject(MUMT, 1735, 510, -1);
+
+  //Radio
+  CreateObject(RDIO, 1700, 510, -1);
+
+  //Radare
+  CreateObject(RADR, 1935, 330, -1);
+  CreateObject(RADR, 2585, 170, -1);
+
+  return true;
+}
+
+/* Spielerinitialisierung */
+
+public func InitializePlayer(int iPlr)
+{
+  if(GetType(arRespawn[iPlr]) != C4V_Array)
+    arRespawn[iPlr] = [];
+
+  SetRespawnPosition(RandomX(1740, 1820), 585, iPlr);
+
+  SetArrow(1865, 485, iPlr);
+
+  return _inherited(iPlr, ...);
+}
+
+/* Script */
+
 public func Script1()
 {
-	TutorialMessage("$TxtWelcome$");
+  TutorialMessage("$TxtWelcome$");
   Sound("EI4K_NewMessage.ogg");
   Sound("RadioConfirm*.ogg");
   FakeScriptGo(0);
   
   return true;
 }
-
