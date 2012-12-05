93955a7e98971322b504b75396aff110effd8820
Michael14 <michael@missile.beheh.de>
2012-12-05 21:09:41 +0000

    * Granaten umbenannt. * Keine Munition-Regel entfernt, Fallschaden hinzugefügt. * Clonks werden nur noch nach Regelwahl ausgerüstet. * Clonkausrüstung verändert. * Umweltobjekte ergänzt. * Script gesäubert.

--- cmcrepos/ModernCombatTheLostMissionsFINALTRY.c4f/CMC_Stylands.c4s/Script.c
+++ cmcrepos/ModernCombatTheLostMissionsFINALTRY.c4f/CMC_Stylands.c4s/Script.c
@@ -1,6 +1,6 @@
 /*-- Stylands --*/
 
-#strict 2
+#strict
 #include CSTD
 
 
@@ -8,9 +8,9 @@
 
 func ChooserRuleConfig()
 {
-  //Abgewandelter Regelsatz: Kein FriendlyFire, Keine Munition, Keine Schwerverletzten, Flagge abnehmbar
-  var array = [NOFF, NOAM, NOFD, FGRV];
-  return array;
+  //Abgewandelter Regelsatz: Kein FriendlyFire, Fallschaden, Keine Schwerverletzten, Flagge abnehmbar
+  var array = [NOFF, FDMG, NOFD, FGRV];
+  return(array);
 }
 
 /* Initialisierung */
@@ -22,10 +22,6 @@ func Initialize()
   Music("CMC_WaitingforSpawn.ogg",1);
   //Einrichtung plazieren
   CreateInterior();
-  //Ausrüstung plazieren
-  CreateEquipment();
-  //Dekoration plazieren
-  CreateDecoration();
   return(1);
 }
 
@@ -41,61 +37,74 @@ func CreateInterior()
 
   //Erdmaterialien
   //Ausrüstung
-  EarthMaterials(FAPK,3,-1);
-  EarthMaterials(DGNN,2,-1);
-  EarthMaterials(BBTP,2,-1);
-  EarthMaterials(C4PA,2,-1);
+  EarthMaterials(DGNN,3,-1);
+  EarthMaterials(FAPK,2,-1);
+  EarthMaterials(BTBG,2,-1);
   EarthMaterials(RSHL,2,-1);
-  EarthMaterials(CDBT,2,-1);
+  EarthMaterials(CDBT,1,-1);
+  EarthMaterials(C4PA,1,-1);
 
   //Granaten
-  EarthMaterials(FGRN,3,-1);
-  EarthMaterials(FRAG,4,-1);
   EarthMaterials(SGRN,4,-1);
   EarthMaterials(STUN,4,-1);
-  EarthMaterials(PGRN,2,-1);
+  EarthMaterials(FGRN,3,-1);
+  EarthMaterials(FRAG,3,-1);
   EarthMaterials(EGRN,3,-1);
   EarthMaterials(VGRN,3,-1);
+  EarthMaterials(PGRN,2,-1);
 
   //Waffen
-  EarthMaterials(ASTR,1,-1);
+  EarthMaterials(ATWN,2,-1);
+  EarthMaterials(ASTR,2,-1);
   EarthMaterials(PPGN,1,-1);
   EarthMaterials(MNGN,1,-1);
   EarthMaterials(RTLR,1,-1);
   EarthMaterials(SGST,1,-1);
   EarthMaterials(SMGN,1,-1);
+
+  //Munition
+  if(!FindObject(NOAM))
+  {
+   EarthMaterials(ABOX,4,-1);
+   EarthMaterials(GBOX,3,-1);
+   EarthMaterials(MBOX,2,-1);
+  }
 }
 
-func CreateEquipment()
-{
-  Log("$CreatingEquipment$");
-}
+/* Bei Clonkausrüstung */
 
-func CreateDecoration()
+public func OnClonkEquip(object pClonk)
 {
-  Log("$CreatingDecoration$");
-}
+  //Nicht bei Regelwahl
+  if(!g_chooserFinished) 
+   return;
 
-/* Bei Ausrüstung */
-
-public func OnClassSelection(object pClonk)
-{
   //Alte Flagge entfernen
   for(var flag in FindObjects(Find_ID(FLAG),Find_Owner(GetOwner(pClonk))))
   {
-    CreateParticle("Blast",GetX(flag),GetY(flag),0,0,10*10);
-    RemoveObject(flag);
+   CreateParticle("Blast",GetX(flag),GetY(flag),0,0,10*10);
+   RemoveObject(flag);
   }
 
   //Fallschirm erstellen
   CreateObject(PARA,0,0,GetOwner(pClonk))->Set(pClonk);
 
-  //Ausrüstung: Bausatz, Flagge, Erdgranate, Erdbebengranate, Jetpack
+  //Ausrüstung: Revolver, Bausatz, Flagge, Erdgranate, Erdbebengranate, Fallschirmrucksack
+  var wpn = CreateContents(RVLR, pClonk);
+  wpn->DoAmmo(wpn->GetFMData(FM_AmmoID),wpn->GetFMData(FM_AmmoLoad));
   CreateContents(CNKT,pClonk);
   CreateContents(FLAG,pClonk);
   CreateContents(EGRN,pClonk)->Activate(pClonk);
   CreateContents(VGRN,pClonk)->Activate(pClonk);
-  CreateContents(JTPK, pClonk)->Activate(pClonk);
+  CreateContents(PPAR,pClonk)->Activate(pClonk);
+
+  //Munition: 90 Kugeln, 12 Granaten, 3 Raketen
+  if(!FindObject(NOAM))
+  {
+   DoAmmo(STAM, 90, pClonk);
+   DoAmmo(GRAM, 12, pClonk);
+   DoAmmo(MIAM, 3, pClonk);
+  }
 }
 
 /* Regelwähler */
@@ -119,28 +128,29 @@ global func EarthMaterials(def,amount,mat,x,y,wdt,hgt,fsave)
   var i,rndx,rndy,obj,j;
   while(i<amount)
   {
-    if(wdt) rndx=x+Random(wdt);
-      else rndx=x+Random(LandscapeWidth()-x);
-    if(hgt) rndy=y+Random(hgt);
-      else rndy=y+Random(LandscapeHeight()-y);
+   if(wdt) rndx=x+Random(wdt);
+    else rndx=x+Random(LandscapeWidth()-x);
+   if(hgt) rndy=y+Random(hgt);
+    else rndy=y+Random(LandscapeHeight()-y);
 
-    if((mat!=-1 && GetMaterial(rndx,rndy)==mat) || (mat==-1 && GBackSolid(rndx,rndy)))
+   if((mat!=-1 && GetMaterial(rndx,rndy)==mat) || (mat==-1 && GBackSolid(rndx,rndy)))
+   {
+    obj=CreateObject(def,rndx,rndy+GetDefCoreVal("Height","DefCore",def)/2, NO_OWNER);
+    SetR(Random(360),obj);
+    if(obj->~IsWeapon())
     {
-      obj=CreateObject(def,rndx,rndy+GetDefCoreVal("Height","DefCore",def)/2, NO_OWNER);
-      SetR(Random(360),obj);
-      if(obj->~IsWeapon())
-      {
-        var ammo = obj->GetFMData(FM_AmmoID);
-        var load = obj->GetFMData(FM_AmmoLoad);
-        DoAmmo(ammo,-load, obj);
-        DoAmmo(ammo, load, obj);
-      }
-      if(Stuck(obj)) i++;
-        else RemoveObject(obj);
-    } 
+     var ammo = obj->GetFMData(FM_AmmoID);
+     var load = obj->GetFMData(FM_AmmoLoad);
+     DoAmmo(ammo,-load, obj);
+     DoAmmo(ammo, load, obj);
+    }
+    if(Stuck(obj)) i++;
+     else RemoveObject(obj);
+   } 
 
-    if(!fsave && j++>5000) return(0);
+   if(!fsave && j++>5000) return(0);
   }
+
   return(j);
 }
 
