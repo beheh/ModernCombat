f4156203ca34d81bdd868f3559073579a997e9d7
Michael14 <michael@missile.beheh.de>
2012-12-08 13:55:29 +0000

    + Production Line eingeführt. * Foldermap aktualisiert.

---
+++ cmcrepos/ModernCombatTheLostMissionsFINALTRY.c4f/CMC_ProductionLine.c4s/ProductionLine.c4d/Mid.c4d/Script.c
@@ -0,0 +1,51 @@
+/*-- Flieﬂbandteil --*/
+
+#strict 2
+
+local father, x, y, speed;
+
+public func IgnoreProductionLine()	{return true;}
+public func Speed()			{return speed;}
+public func SetSpeed(int iSpeed)	{return speed = iSpeed;}
+
+
+/* Initialisierung */
+
+func Initialize()
+{
+  //Voreinstellungen
+  SetAction("Stop");
+  x = GetX() - GetX(father);
+  y = GetY() - GetY(father);
+  speed = 2;
+  return 1;
+}
+
+/* Position aktuell halten */
+
+func CheckPosition()
+{
+  SetPosition(GetX(father)+x, GetY(father)+y);
+  return;
+}
+
+func Push()
+{
+  var dir = (GetAction() == "Right") * 2 - 1;
+
+  var objects = FindObjects(Find_OnLine(-19, -12, 19, -12), Find_Category(C4D_Living|C4D_Object|C4D_Vehicle), Find_Not(Find_Func("IgnoreProductionLine")));
+  for(obj in objects)
+  	SetPosition(GetX(obj)+Speed()*dir, GetY(obj), obj);
+  
+  var objects = FindObjects(Find_OnLine(-20, 12, 19, 12), Find_Category(C4D_Living|C4D_Object|C4D_Vehicle), Find_Not(Find_Func("IgnoreProductionLine")));
+  for(obj in objects)
+  	SetPosition(GetX(obj)+Speed()*(-dir), GetY(obj), obj);
+
+  CheckEnds(dir);
+  return;
+}
+
+func CheckEnds(dir)
+{
+  return;
+}
---
+++ cmcrepos/ModernCombatTheLostMissionsFINALTRY.c4f/CMC_ProductionLine.c4s/System.c4g/ProductionLine.c
@@ -0,0 +1,13 @@
+/*-- Förderband --*/
+
+//Bestimmte Objekte werden vom Förderband nicht bewegt.
+
+#strict 2
+
+#appendto RSLH
+#appendto ESHL
+#appendto SHTX
+#appendto SHT1
+#appendto SM4K
+
+public func IgnoreProductionLine()	{return true;}
