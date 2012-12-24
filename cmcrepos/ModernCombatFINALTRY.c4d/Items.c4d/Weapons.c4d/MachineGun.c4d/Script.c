27847db5a6a060be585ca3171349d981c42137f3
Michael14 <michael@missile.beheh.de>
2012-12-24 15:08:38 +0000

    ! Alle Waffen mit Kommentaren beschildert, um Anpassungen der Werte und Entwicklern, die sich die Scripts ansehen, das einlesen zu erleichtern. (Teil 1, betroffen sind Sturmgewehr, APW, Schrotflinte) * Script gesäubert.

--- cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Weapons.c4d/MachineGun.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Weapons.c4d/MachineGun.c4d/Script.c
@@ -8,18 +8,20 @@ public func HandX()		{return 5000;}
 public func HandY()		{return 800;}
 public func BarrelXOffset()	{return -1000;}
 public func BarrelYOffset()	{return -2000;}
-public func SelectionTime()	{return 36;}
 public func IsPrimaryWeapon()	{return true;}
+
+public func SelectionTime()	{return 36;}	//Anwahlzeit
+
 
 /* Nahkampfangriff */
 
 public func GetMCData(int data)
 {
-  if(data == MC_CanStrike)	return 1;
-  if(data == MC_Damage)		return 20;
-  if(data == MC_Recharge)	return 45;
-  if(data == MC_Power)		return 20;
-  if(data == MC_Angle)		return 45;
+  if(data == MC_CanStrike)	return 1;	//Waffe kann Kolbenschlag ausführen
+  if(data == MC_Damage)		return 20;	//Schaden eines Kolbenschlages
+  if(data == MC_Recharge)	return 45;	//Zeit nach Kolbenschlag bis erneut geschlagen oder gefeuert werden kann
+  if(data == MC_Power)		return 20;	//Wie weit das Ziel durch Kolbenschläge geschleudert wird
+  if(data == MC_Angle)		return 45;	//Mit welchem Winkel das Ziel durch Kolbenschläge geschleudert wird
 }
 
 /* Kugeln */
@@ -27,20 +29,20 @@ public func GetMCData(int data)
 public func FMData1(int data)
 {
   if(data == FM_Name)		return "$Bullets$";
-    
-  if(data == FM_AmmoID)		return STAM;
-  if(data == FM_AmmoLoad)	return 60;
-  
-  if(data == FM_Reload)		return 220;
-  if(data == FM_Recharge)	return 4;
 
-  if(data == FM_Auto)		return true;
-  
-  if(data == FM_Damage)		return 8;
-  
-  if(data == FM_SpreadAdd)	return 30;
-  if(data == FM_StartSpread)	return 200;
-  if(data == FM_MaxSpread)	return 400;
+  if(data == FM_AmmoID)		return STAM;	//ID der Munition
+  if(data == FM_AmmoLoad)	return 60;	//Magazingröße
+
+  if(data == FM_Reload)		return 220;	//Zeit für Nachladen
+  if(data == FM_Recharge)	return 4;	//Zeit bis erneut geschossen werden kann
+
+  if(data == FM_Damage)		return 8;	//Schadenswert
+
+  if(data == FM_Auto)		return true;	//Automatikfeuer
+
+  if(data == FM_SpreadAdd)	return 30;	//Bei jedem Schuss hinzuzuaddierende Streuung
+  if(data == FM_StartSpread)	return 200;	//Bei Auswahl der Waffe gesetzte Streuung
+  if(data == FM_MaxSpread)	return 400;	//Maximaler Streuungswert
 
   return Default(data);
 }
@@ -50,6 +52,7 @@ public func FMData1(int data)
 public func FMData1T1(int data)
 {
   if(data == FT_Name)		return "$Auto$";
+
   return FMData1(data);
 }
 
@@ -72,15 +75,16 @@ public func FMData1T2(int data)
 {
   if(data == FT_Name)		return "$Burst$";
 
-  if(data == FM_Recharge)	return 30;
-  if(data == FM_Auto)		return false;
+  if(data == FM_Recharge)	return 30;	//Zeit bis erneut geschossen werden kann
 
-  if(data == FM_BurstAmount)	return 4;
-  if(data == FM_BurstRecharge)	return 4;
+  if(data == FM_BurstAmount)	return 4;	//Anzahl Schussabrufe pro Burst
+  if(data == FM_BurstRecharge)	return 4;	//Zeit zwischen einzelnen Bursts
 
-  if(data == FM_Damage)		return 9;
+  if(data == FM_Damage)		return 9;	//Schadenswert
 
-  if(data == FM_SpreadAdd)	return 30;
+  if(data == FM_Auto)		return false;	//Kein Automatikfeuer
+
+  if(data == FM_SpreadAdd)	return 30;	//Bei jedem Schuss hinzuzuaddierende Streuung
 
   return FMData1(data);
 }
