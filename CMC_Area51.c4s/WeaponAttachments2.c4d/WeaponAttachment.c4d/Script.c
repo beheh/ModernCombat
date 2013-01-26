8e5ce04c55a8cba6d6df9ad0d66c6a0fdf46e256
Monkstar <monkstar@missile.beheh.de>
2013-01-26 13:49:16 +0000

    + Waffenattachments: Weitere Verbesserungen.

--- CMC_Area51.c4s/WeaponAttachments2.c4d/WeaponAttachment.c4d/Script.c
+++ CMC_Area51.c4s/WeaponAttachments2.c4d/WeaponAttachment.c4d/Script.c
@@ -1,11 +1,13 @@
-/*-- Waffenaufsatz --*/
+/*-- Waffenattachment --*/
 
 #strict 2
 
 local iAttachment;
 
-
-/* Inhalt festlegen */
+func Activate(object pClonk)
+{
+  AttachmentMenu(pClonk);
+}
 
 func SetAttachment(int iValue)
 {
@@ -19,48 +21,26 @@ func GetAttachment()
   return iAttachment;
 }
 
-/* Steuerung */
-
-func Activate(object pClonk)
-{
-  AttachmentMenu(pClonk);
-}
-
-/* Menü */
-
 func AttachmentMenu(object pByObj)
 {
-  //Falsche Aktion?
-  if(!WildcardMatch(GetAction(pByObj), "*Walk*") && !WildcardMatch(GetAction(pByObj), "*Swim*") && !WildcardMatch(GetAction(pByObj), "*Crawl*") && !WildcardMatch(GetAction(pByObj), "*Jump*"))
-  {
-    PlayerMessage(GetOwner(pByObj), "$CantUpgrade$", pByObj);
-    return true;
-  }
-
-  //Clonk anhalten
-  SetComDir(COMD_Stop, pByObj);
-
-  //Menü erstellen
-  CreateMenu(GetID(this), pByObj, this, 0, "$AttachmentMenu$", 0, C4MN_Style_Context);
+  if(!iAttachment) return RemoveObject();
+  
+  CreateMenu(GetID(this), pByObj, this, 0, Format("$AttachmentMenu$", GetName(0, AttachmentIcon(iAttachment))), 0, C4MN_Style_Context);
 
   var fItemExists = false;
 
-  //Kompatible Waffen auflisten
+  AddMenuItem("$Nothing$", "DummyFunc", SM06, pByObj);
   for(var pWeapon in FindObjects(Find_Container(Contained()), Find_Func("IsWeapon2")))
   {
-    fItemExists = true;
-    if(pWeapon->~PermittedAtts() & iAttachment)
-      AddMenuItem(Format("$Upgrade$", GetName(0, GetID(pWeapon))), "Attach", GetID(pWeapon), pByObj, 0, pByObj);
+    if(pWeapon->~PermittedAtts() & iAttachment && LocalN("iAttachment", pWeapon) != iAttachment)
+    {
+      fItemExists = true;
+      AddMenuItem(GetName(0, GetID(pWeapon)), "Attach", GetID(pWeapon), pByObj, 0, pByObj);
+    }
   }
 
-  //Menü schließen wenn keine Waffe gefunden wurde
   if(!fItemExists)
-  {
-    PlayerMessage(GetOwner(pByObj), "$NoWeapon$", pByObj);
     CloseMenu(pByObj);
-    Sound("lol.ogg");
-  }
-
 }
 
 func DummyFunc()
@@ -68,47 +48,25 @@ func DummyFunc()
   //Nichts unternehmen; diese Func existiert für die Option "Nichts" im Menü
 }
 
-/* Waffe modifizieren */
-
 func Attach(id iItem, object pUser)
 {
-  //Waffe finden
   var pWeapon = FindContents(iItem, pUser);
   if(!pWeapon) return false;
-
-  //Waffe modifizieren
-  iAttachment = pWeapon->SetAttachment(iAttachment);
-
-  //Sound
-  Sound("WNAT_AddAttachement.ogg",0,pWeapon);
-
-  //Verschwinden
-  if(!iAttachment)
-    RemoveObject(this);
+  
+  //Ausgebautes Attachment selbst wieder anbringen
+  SetAttachment(pWeapon->SetAttachment(iAttachment));
+  
+  if(!iAttachment) RemoveObject(this);
 }
-
-/* Aufsatz-Icons */
 
 global func AttachmentIcon(int iAtt)
 {
-  if(iAtt == AT_NoAttachment)		return 0;
-  if(iAtt == AT_ExtendedMag)		return SM20;
-  if(iAtt == AT_Bayonet)		return SM21;
-  if(iAtt == AT_Laserpointer)		return SM22;
-  if(iAtt == AT_Silencer)		return SM23;
-  if(iAtt == AT_Foregrip)		return SM24;
-  if(iAtt == AT_GrenadeLauncher)	return SM25;
-  if(iAtt == AT_TracerDart)		return SM26;
-}
-
-/* Sonstiges */
-
-protected func Hit()
-{
-  Sound("AmmoBoxHit*.ogg", false, this);
-}
-
-protected func Selection()
-{
-  Sound("FAPK_Charge.ogg", false, this);
+  if(iAtt == AT_NoAttachment) return 0;
+  if(iAtt == AT_ExtendedMag) return SM20;
+  if(iAtt == AT_Bayonet) return SM21;
+  if(iAtt == AT_Laserpointer) return SM22;
+  if(iAtt == AT_Silencer) return SM23;
+  if(iAtt == AT_Foregrip) return SM24;
+  if(iAtt == AT_GrenadeLauncher) return SM25;
+  if(iAtt == AT_TracerDart) return SM26;
 }
