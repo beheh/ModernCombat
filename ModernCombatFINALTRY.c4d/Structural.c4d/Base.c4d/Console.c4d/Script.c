ad65eb38ad8f199a3ac55b3d7a41dbfdec630bab
ClonkGeist <clonkgeist@missile.beheh.de>
2013-02-01 12:24:55 +0000

    ! #011814 ("Schweißbrenner: Kein Schaden an feindlichen Konsolen")

--- ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/Console.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/Console.c4d/Script.c
@@ -6,10 +6,10 @@ local target;
 
 public func IsMachine()			{return 1;}
 public func GetRealRepairableObject()	{return target;}
-public func IsFakeRepairable(int iPlr)		
+public func IsFakeRepairable(int iPlr, object pByObj)		
 {
   var fAdd = GetDamage(target);
-  if(Hostile(iPlr, GetOwner(target)))
+  if(CheckEnemy(pByObj, target))//Hostile(iPlr, GetOwner(target)))
     fAdd = true;
 
   return (target && target->~IsRepairable() && fAdd);
