25f98f5cfe1141e98524ff9683a2856eae290194
Michael14 <michael@missile.beheh.de>
2013-02-03 11:33:20 +0000

    * Script gesäubert.

--- ModernCombatFINALTRY.c4d/Goals.c4d/Assault.c4d/BasicAssault.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Goals.c4d/Assault.c4d/BasicAssault.c4d/Script.c
@@ -175,13 +175,13 @@ protected func FxIntAssaultTargetTimer(object pTarget, int iEffect)
   if(EffectVar(1, pTarget, iEffect))
     return;
 
-	var team = pTarget->~GetTeam();
+  var team = pTarget->~GetTeam();
 
   //Erstmal die Leiste prüfen
   if(!EffectVar(2, pTarget, iEffect))
   {
     var bar = EffectVar(2, pTarget, iEffect) = CreateObject(SBAR, 0, 0, -1);
-  	bar->Set(pTarget, GetTeamColor(team), BAR_AssaultBar, 200); 
+    bar->Set(pTarget, GetTeamColor(team), BAR_AssaultBar, 200); 
   }
   var bar = EffectVar(2, pTarget, iEffect);
 
