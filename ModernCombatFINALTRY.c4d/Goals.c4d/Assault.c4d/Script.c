3710c640e5b3618a0a4de8acd78d06a85a0c30ce
Michael14 <michael@missile.beheh.de>
2013-02-12 12:49:30 +0000

    * Script gesäubert.

--- ModernCombatFINALTRY.c4d/Goals.c4d/Assault.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Goals.c4d/Assault.c4d/Script.c
@@ -293,14 +293,14 @@ protected func FxIntAssaultTargetTimer(object pTarget, int iNr)
   iNext = GetNextTarget();
 
   var fConnected = GetType(Connected[iNext]) == C4V_Array;
-  
+
   var bar = EffectVar(0, pTarget, iNr);
 
   if(iTarget != iNext && (fConnected && GetIndexOf(iTarget, Connected[iNext]) == -1 || !fConnected))
   {
-  	bar->Update(0, true);
+    bar->Update(0, true);
     return true;
-	}
+  }
 
   var idTarget = EffectVar(1, pTarget, iNr);
 
