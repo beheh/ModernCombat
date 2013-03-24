d8c818407d0b6499676c2d2e934ec07d4a3d6799
Michael14 <michael@missile.beheh.de>
2013-03-24 17:44:35 +0000

    * Script gesäubert.

--- ModernCombatFINALTRY.c4d/Items.c4d/Weapons.c4d/Slingshot.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Items.c4d/Weapons.c4d/Slingshot.c4d/Script.c
@@ -135,7 +135,7 @@ public func LaunchGrenade(id idg, int speed, int angle, int mode)
   if(!Stuck(grenade)) SetPosition(GetX(grenade)+xdir/10,GetY(grenade)+ydir/10,grenade);
   SetController(GetController(user), grenade);
   grenade->Launch(xdir+GetXDir(user)/5, ydir/*+GetYDir(user)/4*/, GetFMData(FM_Damage,2));
-  
+
   //Sicht auf Granate wenn der Schütze zielt
   if(!(user->~IsMachine()) && user->~IsAiming())
   {
