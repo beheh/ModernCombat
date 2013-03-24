1f7ed40176b590e576790c37fd8f1e2d1e63d4b6
Michael14 <michael@missile.beheh.de>
2013-03-24 23:01:45 +0000

    * Script gesäubert.

--- CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-WPN-Slingshot.c
+++ CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-WPN-Slingshot.c
@@ -19,22 +19,6 @@ public func GetMCData(int data)
   return _inherited(data);
 }
 
-func FxLaserDotTimer(object pTarget, int iEffectNumber, int iEffectTime)
-{
-	//Nutzer festlegen
-	var user = this->~GetUser();
-	var x, y, z;
-	if(!user || !user->~IsClonk() || !user->WeaponAt(x, y, z) || !user->IsAiming() || Contents(0, user) != this || iAttachment != AT_Laserpointer)
-	{
-	  RemoveTrajectory(pTarget);
-	  return;
-	}
-
-	var iAngle = EffectVar(1, user, GetEffect("ShowWeapon", user));
-	var empty = IsReloading() || !GetCharge();
-	AddTrajectory(pTarget, GetX(pTarget), GetY(pTarget), Sin(iAngle, 120), -Cos(iAngle, 120), 35*3, RGB(255*empty, 255*(!empty), 0));
-}
-
 public func FMData1(int data)
 {
   if(data == FM_AmmoLoad)	return _inherited(data) + (iAttachment == AT_ExtendedMag)*2;	//Magazingröße
@@ -44,4 +28,20 @@ public func FMData1(int data)
   if(data == FM_MaxSpread)	return 400 - (iAttachment == AT_Laserpointer)*150;	//Maximaler Streuungswert
 
   return _inherited(data);
+}
+
+func FxLaserDotTimer(object pTarget, int iEffectNumber, int iEffectTime)
+{
+  //Nutzer festlegen
+  var user = this->~GetUser();
+  var x, y, z;
+  if(!user || !user->~IsClonk() || !user->WeaponAt(x, y, z) || !user->IsAiming() || Contents(0, user) != this || iAttachment != AT_Laserpointer)
+  {
+    RemoveTrajectory(pTarget);
+    return;
+  }
+
+  var iAngle = EffectVar(1, user, GetEffect("ShowWeapon", user));
+  var empty = IsReloading() || !GetCharge();
+  AddTrajectory(pTarget, GetX(pTarget), GetY(pTarget), Sin(iAngle, 120), -Cos(iAngle, 120), 35*3, RGB(255*empty, 255*(!empty), 0));
 }
