e2902a931533fc08f63747127c7df8c9877ccbd8
ClonkGeist <clonkgeist@missile.beheh.de>
2013-03-24 22:41:57 +0000

    * Weaponattachments: Trajectory für Granatenfeuermodus beim Assault Rifle * Slingshot: Debug-for-Schleife bei Trajectory rausgenommen.

--- CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-WPN-Slingshot.c
+++ CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-WPN-Slingshot.c
@@ -21,21 +21,18 @@ public func GetMCData(int data)
 
 func FxLaserDotTimer(object pTarget, int iEffectNumber, int iEffectTime)
 {
-  for(var i = 0; i < 10; i++)
-  {
-		//Nutzer festlegen
-		var user = this->~GetUser();
-		var x, y, z;
-		if(!user || !user->~IsClonk() || !user->WeaponAt(x, y, z) || !user->IsAiming() || Contents(0, user) != this || iAttachment != AT_Laserpointer)
-		{
-		  RemoveTrajectory(pTarget);
-		  return;
-		}
+	//Nutzer festlegen
+	var user = this->~GetUser();
+	var x, y, z;
+	if(!user || !user->~IsClonk() || !user->WeaponAt(x, y, z) || !user->IsAiming() || Contents(0, user) != this || iAttachment != AT_Laserpointer)
+	{
+	  RemoveTrajectory(pTarget);
+	  return;
+	}
 
-		var iAngle = EffectVar(1, user, GetEffect("ShowWeapon", user));
-		var empty = IsReloading() || !GetCharge();
-		AddTrajectory(pTarget, GetX(pTarget), GetY(pTarget), Sin(iAngle, 120), -Cos(iAngle, 120), 35*3, RGB(255*empty, 255*(!empty), 0));
-  }
+	var iAngle = EffectVar(1, user, GetEffect("ShowWeapon", user));
+	var empty = IsReloading() || !GetCharge();
+	AddTrajectory(pTarget, GetX(pTarget), GetY(pTarget), Sin(iAngle, 120), -Cos(iAngle, 120), 35*3, RGB(255*empty, 255*(!empty), 0));
 }
 
 public func FMData1(int data)
