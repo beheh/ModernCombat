5cf8fa353ec7230e6d13c650ea661474e502cc56
ClonkGeist <clonkgeist@missile.beheh.de>
2013-03-25 11:27:33 +0000

    * DoDmg2 hinzugefügt (-> Schadenswerte lassen sich mit Präzision korrekt angeben)

--- cmcrepos/CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Overload-Damage.c
+++ cmcrepos/CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Overload-Damage.c
@@ -74,3 +74,81 @@ global func DoDmg(int iDmg, int iType, object pTarget, int iPrecision, int dmgpl
     return DoEnergy(-dmg, pTarget, true, 0, dmgplayer);
   return DoDamage(dmg/1000, pTarget, 0, dmgplayer);
 }
+
+
+/* DoDmg2, ähnlich wie DoDmg, Schadenswerte lassen sich jedoch mit Präzision korrekt angeben. 
+ * Aus Abwärtskompatibilitätsgründen bleibt DoDmg so drin */
+ 
+global func DoDmg2(int iDmg, int iType, object pTarget, int iPrecision, int dmgplayer, id idKillIcon, int iKillAttachment)
+{
+  if(!pTarget)
+    if(!(pTarget = this))
+      return;
+  if(!iPrecision)
+    iPrecision = 10;
+
+  var dmg, dmgdealer = dmgplayer-1;
+
+  if(dmgdealer < 0)
+    dmgdealer = GetController(pTarget);
+
+  var red = pTarget->~OnDmg(iDmg, iType); //reduction
+  dmg = iDmg*(100-red)/100;
+
+  if (!dmg || red == 100) return;
+
+  // Killer setzen
+  if(this && pTarget->GetOCF() & OCF_CrewMember || dmgplayer)
+    SetKiller(dmgdealer, pTarget);
+
+  if(GetOCF(pTarget) & OCF_CrewMember)
+  {
+    if(!idKillIcon && this)//Kein Killicon?
+    {
+      idKillIcon = this->~GetKillIcon();
+      
+      if(!idKillIcon)
+        idKillIcon = GetID(this);
+      
+      if(idKillIcon->~IsClonk())
+        idKillIcon = 0;      
+    }
+    if(!idKillIcon)
+      {
+        if(iType)
+        {
+          if(iType == DMG_Fire)
+            idKillIcon = GSAM;
+          else if(iType == DMG_Explosion)
+            idKillIcon = BOOM;
+          else if(iType == DMG_Energy)
+            idKillIcon = ENAM;
+          else if(iType == DMG_Bio)
+            idKillIcon = GLOB;
+          else if(iType == DMG_Projectile)
+            idKillIcon = STAM;
+        }
+        else
+          idKillIcon = KAMB;
+      }
+    
+    pTarget->~KillIcon(idKillIcon);
+    pTarget->~KillAttachment(iKillAttachment, true);
+  }
+
+  //Schaden machen
+  var pFrom;
+  if(pTarget != this)
+    pFrom = this;
+  if(!pFrom) pFrom = GetCrew(dmgdealer);
+
+	dmg = dmg * 1000 / iPrecision;
+
+  pTarget->~LastDamageType(iType);
+  pTarget->~OnHit(dmg/1000, iType, pFrom);
+  if(GetCategory(pTarget) & C4D_Living)
+    return DoEnergy(-dmg, pTarget, true, 0, dmgplayer);
+  return DoDamage(dmg/1000, pTarget, 0, dmgplayer);
+}
+
+
