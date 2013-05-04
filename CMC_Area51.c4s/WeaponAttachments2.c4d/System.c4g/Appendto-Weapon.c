0cb9856fa6f0c199d2cc156d82d4c606721a6427
ClonkGeist <clonkgeist@missile.beheh.de>
2013-05-04 15:49:07 +0000

    * Taschenlampen-Aufsatz: Schwenk- und Ruckel/Wackeleffekte beim Laufen/Springen/Landen.

--- CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-Weapon.c
+++ CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-Weapon.c
@@ -86,6 +86,66 @@ public func FxFlashlightTimer(object pTarget, int iNr, int iTime)
   }
   else if(user)
   {
+  	var angle = user->~AimAngle();
+  	
+  	//Wackel- und Schwenkeffekte beim Laufen/Springen
+		if((WildcardMatch(GetAction(user), "*Walk*") || WildcardMatch(GetAction(user), "*Kneel*") || WildcardMatch(GetAction(user), "*Flat*")))
+		{
+			//Ruckeleffekt bei Landung
+			if(EffectVar(4, pTarget, iNr))
+			{
+				if(WildcardMatch(GetAction(user), "*Kneel*") || WildcardMatch(GetAction(user), "*Flat*"))
+					EffectVar(4, pTarget, iNr) = 2;
+
+				EffectVar(5, pTarget, iNr)++;
+				EffectVar(2, pTarget, iNr) = Random(5 + 4*(EffectVar(4, pTarget, iNr)-1))-2 * EffectVar(4, pTarget, iNr);
+			
+				if(EffectVar(5, pTarget, iNr) >= (EffectVar(4, pTarget, iNr)*5))
+					EffectVar(4, pTarget, iNr) = EffectVar(5, pTarget, iNr) = 0;
+			}
+			//Leichtes Wackeln beim Laufen
+			else if(!Inside(GetXDir(user), -10, 10))
+			{
+				if(iTime % 5)
+					EffectVar(3, pTarget, iNr) = 0;
+				else
+				{
+					if(EffectVar(2, pTarget, iNr) >= 1)
+						EffectVar(3, pTarget, iNr) = -1;
+					else if(EffectVar(2, pTarget, iNr) <= -1)
+						EffectVar(3, pTarget, iNr) = +1;
+					else if(!EffectVar(3, pTarget, iNr))
+						EffectVar(3, pTarget, iNr) = -1;
+				}
+			}
+		}
+		//Taschenlampe nach oben/unten schwenken während des Sprungs
+		else if(WildcardMatch(GetAction(user), "*Jump*"))
+		{
+			if(GetYDir(user) > 0 && EffectVar(2, pTarget, iNr) > -5)
+				EffectVar(3, pTarget, iNr) = -1;
+			else if(GetYDir(user) < 0 && EffectVar(2, pTarget, iNr) < 5)
+				EffectVar(3, pTarget, iNr) = +1;
+			else
+				EffectVar(3, pTarget, iNr) = 0;
+		
+			if(!GetYDir(user))
+				EffectVar(3, pTarget, iNr) = 0;
+			
+			EffectVar(4, pTarget, iNr) = true;
+		}
+		//Zusatzrotation zurücksetzen, falls nicht am Springen/Laufen/Landen
+		else if(EffectVar(2, pTarget, iNr) != 0)
+		{
+			EffectVar(4, pTarget, iNr) = false;
+			EffectVar(3, pTarget, iNr) = BoundBy(EffectVar(2, pTarget, iNr) * -2, -1, 1);
+		}
+		else
+			EffectVar(3, pTarget, iNr) = 0;
+
+		var handr = this->~HandR() * (GetDir(user) * 2 - 1);
+		angle += handr + (EffectVar(2, pTarget, iNr) += EffectVar(3, pTarget, iNr));
+
     if(!light)
     {
       light = EffectVar(0, pTarget, iNr) = AddLightCone(1000, RGBa(255, 255, 220, 90), user);
@@ -98,7 +158,7 @@ public func FxFlashlightTimer(object pTarget, int iNr, int iTime)
     else
       SetVisibility(VIS_All, light);
 
-    light->ChangeR(user->~AimAngle());
+    light->ChangeR(angle);
   }
 
   EffectVar(1, pTarget, iNr) = user;
@@ -106,8 +166,8 @@ public func FxFlashlightTimer(object pTarget, int iNr, int iTime)
   if(deactivate || iTime % 4)
     return;
 
-  var iAngleMin = user->~AimAngle()-FlashlightAngle()/2;
-  var iAngleMax = user->~AimAngle()+FlashlightAngle()/2;
+  var iAngleMin = angle-FlashlightAngle()/2;
+  var iAngleMax = angle+FlashlightAngle()/2;
 
   //Zu markierende Gefahren suchen
   for (var pObj in FindObjects(Find_Distance(SensorDistance()),			//In Reichweite
