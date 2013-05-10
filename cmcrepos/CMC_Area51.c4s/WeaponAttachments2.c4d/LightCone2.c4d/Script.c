b24e9a50df26a5a14f0cf48238b12baedee70064
ClonkGeist <clonkgeist@missile.beheh.de>
2013-05-10 14:30:33 +0000

    * Lichtkegel hat Offset erhalten (= WeaponBegin)

---
+++ cmcrepos/CMC_Area51.c4s/WeaponAttachments2.c4d/LightCone2.c4d/Script.c
@@ -0,0 +1,12 @@
+/*-- Lichtkegel --*/
+
+#strict 2
+
+#include LGHC
+
+global func AddLightCone2(int iSize, int iColor, object pTarget) {
+  if(!pTarget && !(pTarget = this))
+    return;
+  
+  return CreateLight(LGH2, iSize, iColor, pTarget);
+}
