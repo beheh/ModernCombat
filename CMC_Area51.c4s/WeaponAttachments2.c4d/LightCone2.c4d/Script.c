08938ff1d4d0e53ca748bb2926e979a0258af621
Michael14 <michael@missile.beheh.de>
2013-05-13 23:38:25 +0000

    * Script gesäubert.

--- CMC_Area51.c4s/WeaponAttachments2.c4d/LightCone2.c4d/Script.c
+++ CMC_Area51.c4s/WeaponAttachments2.c4d/LightCone2.c4d/Script.c
@@ -1,12 +1,13 @@
 /*-- Lichtkegel --*/
 
 #strict 2
-
 #include LGHC
 
-global func AddLightCone2(int iSize, int iColor, object pTarget) {
+
+global func AddLightCone2(int iSize, int iColor, object pTarget)
+{
   if(!pTarget && !(pTarget = this))
     return;
-  
+
   return CreateLight(LGH2, iSize, iColor, pTarget);
 }
