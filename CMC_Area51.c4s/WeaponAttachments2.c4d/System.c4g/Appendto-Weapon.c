f911cbb6022dbad8ae64cc7d3e3c51c8491bed38
ClonkGeist <clonkgeist@missile.beheh.de>
2013-05-17 20:22:35 +0000

    * Reichweitenbegrenzung für Laserpointer hinzugefügt. (max. 800px)

--- CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-Weapon.c
+++ CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-Weapon.c
@@ -324,6 +324,8 @@ public func FxFlashlightBlindnessStop(object pTarget, int iNr)
 
 /* Laserpointer */
 
+static const WPN2_ATLaserpointer_Range = 800;
+
 func FxLaserDotTimer(object pTarget, int iEffectNumber, int iEffectTime)
 {
   //Nutzer festlegen
@@ -354,23 +356,24 @@ func FxLaserDotTimer(object pTarget, int iEffectNumber, int iEffectTime)
     var gravity = GetGravity();
 
     SetGravity(0);
-    if (!SimFlight(x, y, xdir, ydir))
+    
+    if(!SimFlight(x, y, xdir, ydir, 0, 0, WPN2_ATLaserpointer_Range/3))
+    {
+    	x = Sin(iAngle, WPN2_ATLaserpointer_Range) + xPos;
+    	y = -Cos(iAngle, WPN2_ATLaserpointer_Range) + yPos;
       pLaser->Stop();
-    else 
-      if(!pLaser->Active())
-        fStart = true;
+    }
+    else if(!pLaser->Active())
+      fStart = true;
+    
     SetGravity(gravity);
 
     xdir = Sin(iAngle, 3000);
     ydir = Cos(iAngle, -3000);
 
     var pEnemy;
-
-    if(pLaser->Active() || fStart)
-      pEnemy = FindObject2(Find_OnLine(0, 0, x - xPos, y - yPos), Find_Hostile(GetOwner(GetUser(this))), Find_NoContainer(), Find_Or(Find_OCF(OCF_Alive), Find_Func("IsBulletTarget", GetID(), this, this), Find_Func("IsCMCVehicle")), Sort_Distance(0, 0));
-    else
-      pEnemy = FindObject2(Find_OnLine(0, 0, xdir, ydir), Find_Hostile(GetOwner(GetUser(this))), Find_NoContainer(), Find_Or(Find_OCF(OCF_Alive), Find_Func("IsBulletTarget", GetID(), this, this), Find_Func("IsCMCVehicle")), Sort_Distance(0, 0));
-
+    pEnemy = FindObject2(Find_OnLine(0, 0, x - xPos, y - yPos), Find_Hostile(GetOwner(GetUser(this))), Find_NoContainer(), Find_Or(Find_OCF(OCF_Alive), Find_Func("IsBulletTarget", GetID(), this, this), Find_Func("IsCMCVehicle")), Sort_Distance(0, 0));
+    
     //Feinderkennung
     if(pEnemy)
     {
@@ -397,43 +400,39 @@ func FxLaserDotTimer(object pTarget, int iEffectNumber, int iEffectTime)
         x = Sin(iAngle, (yOff - yPos) * 1000 / (-Cos(iAngle, 1000))) + xPos;
         y = -Cos(iAngle, (yOff - yPos) * 1000 / (-Cos(iAngle, 1000))) + yPos;
       }
-      else
-        if(Inside(yPos, Min(y, yOff), Max(y, yOff)))
-        {
-          x = Sin(iAngle, (xOff - xPos) * 1000 / (Sin(iAngle, 1000))) + xPos;
-          y = -Cos(iAngle, (xOff - xPos) * 1000 / (Sin(iAngle, 1000))) + yPos;
-        }
-      else
-        if((Angle(xPos, yPos, xOff, yOff) >= 180 && Angle(xPos, yPos, xOff, yOff) < iAngle) || (Angle(xPos, yPos, xOff, yOff) <= 180 && Angle(xPos, yPos, xOff, yOff) > iAngle))
-        {
-          x = Sin(iAngle, (yOff - yPos) * 1000 / (-Cos(iAngle, 1000))) + xPos;
-          y = -Cos(iAngle, (yOff - yPos) * 1000 / (-Cos(iAngle, 1000))) + yPos;
-        }
+      else if(Inside(yPos, Min(y, yOff), Max(y, yOff)))
+      {
+        x = Sin(iAngle, (xOff - xPos) * 1000 / (Sin(iAngle, 1000))) + xPos;
+        y = -Cos(iAngle, (xOff - xPos) * 1000 / (Sin(iAngle, 1000))) + yPos;
+      }
+      else if((Angle(xPos, yPos, xOff, yOff) >= 180 && Angle(xPos, yPos, xOff, yOff) < iAngle) || (Angle(xPos, yPos, xOff, yOff) <= 180 && Angle(xPos, yPos, xOff, yOff) > iAngle))
+      {
+        x = Sin(iAngle, (yOff - yPos) * 1000 / (-Cos(iAngle, 1000))) + xPos;
+        y = -Cos(iAngle, (yOff - yPos) * 1000 / (-Cos(iAngle, 1000))) + yPos;
+      }
       else
       {
         x = Sin(iAngle, (xOff - xPos) * 1000 / (Sin(iAngle, 1000))) + xPos;
         y = -Cos(iAngle, (xOff - xPos) * 1000 / (Sin(iAngle, 1000))) + yPos;
       }
-  }
+  	}
 
-  //Laser zeichnen
-  if(!pBeam)  
-    pBeam = CreateObject(LRBM, 0, 0, GetOwner(GetUser(this)));
-  else
-    pBeam->SetPosition(xPos, yPos);
+		//Laser zeichnen
+		if(!pBeam)  
+		  pBeam = CreateObject(LRBM, 0, 0, GetOwner(GetUser(this)));
+		else
+		  pBeam->SetPosition(xPos, yPos);
 
-  //Sichtbarkeit nur für Besitzer und Verbündete
-  pBeam->SetVisibility(VIS_Owner | VIS_Allies);
+		//Sichtbarkeit nur für Besitzer und Verbündete
+		pBeam->SetVisibility(VIS_Owner | VIS_Allies);
 
-  //Laser passend strecken
-  if(pEnemy || pLaser->Active())
-    pBeam->SetObjDrawTransform(100 * Distance(xPos, yPos, x, y), 0, -450 * Distance(xPos, yPos, x, y), 0, 1000, 0);
-  else
-    pBeam->SetObjDrawTransform(100 * Distance(xPos, yPos, xPos + xdir/3, yPos + ydir/3), 0, -450 * Distance(xPos, yPos, xPos + xdir/3, yPos + ydir/3), 0, 1000, 0);
-
-  pBeam->SetR(iAngle+90);
-  SetPosition(x, y, pLaser);
-  if(fStart) pLaser->Start();
+		//Laser passend strecken
+		pBeam->SetObjDrawTransform(100 * Distance(xPos, yPos, x, y), 0, -450 * Distance(xPos, yPos, x, y), 0, 1000, 0);
+		pBeam->SetR(iAngle+90);
+		SetPosition(x, y, pLaser);
+		
+		if(fStart)
+			pLaser->Start();
   }
 }
 
