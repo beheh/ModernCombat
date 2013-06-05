d5e2117855965128a1457aceda4d8bbf5e47c595
Michael14 <michael@missile.beheh.de>
2013-06-05 21:17:20 +0000

    * Script gesäubert.

--- cmcrepos/CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-HazardClonk.c
+++ cmcrepos/CMC_Area51.c4s/WeaponAttachments2.c4d/System.c4g/Appendto-HazardClonk.c
@@ -6,83 +6,90 @@
 
 protected func RejectCollect(id idObj, object pObj)
 {
-	if(FindContents(idObj, this) && pObj->~IsWeapon2() && pObj->~GetAttachment() != AT_NoAttachment && Contained(pObj))
-	{
-		var att = CreateObject(WNAT, 0, 0, GetOwner());
-		if(Collect(att, this))
-		{
-			att->SetAttachment(pObj->GetAttachment());
-			Sound("Merge.ogg");
-			
-			//Schedule, um die "Holen nicht möglich"-Meldung zu überschreiben.
-			Schedule(Format("PlayerMessage(%d, \"$AttachmentCollected$\", this)", GetOwner(), GetName(0, AttachmentIcon(pObj->GetAttachment()))), 1, 0, this);
+  if(FindContents(idObj, this) && pObj->~IsWeapon2() && pObj->~GetAttachment() != AT_NoAttachment && Contained(pObj))
+  {
+    var att = CreateObject(WNAT, 0, 0, GetOwner());
+    if(Collect(att, this))
+    {
+      att->SetAttachment(pObj->GetAttachment());
+      Sound("Merge.ogg");
 
-			pObj->~SetAttachment(AT_NoAttachment);
-		}
-		else if(att)
-			RemoveObject(att);
-		
-		return true;
-	}
+      //Überschreibt die "Holen nicht möglich"-Nachricht
+      Schedule(Format("PlayerMessage(%d, \"$AttachmentCollected$\", this)", GetOwner(), GetName(0, AttachmentIcon(pObj->GetAttachment()))), 1, 0, this);
 
-	//Waffenaufsatzobjekte erhalten einen zusätzlichen Inventarslot
-	if(pObj->~IsAttachmentPack())
-		if(!CustomContentsCount("IsAttachmentPack"))
+      pObj->~SetAttachment(AT_NoAttachment);
+    }
+    else if(att)
+      RemoveObject(att);
+
+    return true;
+  }
+
+  //Waffenaufsatzobjekte erhalten einen zusätzlichen Inventarslot
+  if(pObj->~IsAttachmentPack())
+    if(!CustomContentsCount("IsAttachmentPack"))
       return;
 
-	return _inherited(idObj, pObj, ...);
+  return _inherited(idObj, pObj, ...);
 }
 
-// Ausrüstung ablegen
+/* Ausrüstung ablegen */
+
 protected func ContextUnbuckle(object pCaller)
 {
   [$CtxUnbuckleDesc$|Image=HARM|Condition=FunnyBug]
   if(!HasGear() && !HasAttachmentWeapons())
-  	return;
+    return;
 
   CreateMenu(HARM, 0, 0, 0, 0, 0, 1);
 
-  // alle Ausrüstung anzeigen
+  //Alle Ausrüstungen auflisten
   for(var i; i < GetLength(aGear); ++i) 
   {
     var gear = aGear[i];
     if(gear)
       AddMenuItem("$CtxUnbuckleItem$", Format("TakeOffGear(0, %d)", i), GetID(gear));
   }
-  
+
+  //Alle Waffenaufsätze auflisten
   var obj, i = 0;
   while(obj = Contents(i++))
-  	if(obj->~IsWeapon2() && obj->~GetAttachment() != AT_NoAttachment)
-  		AddMenuItem(Format("$CtxDetachAttachment$", GetName(0, AttachmentIcon(obj->GetAttachment()))), "DetachAttachment", 0, this, 0, obj, 0, 4, obj);
+    if(obj->~IsWeapon2() && obj->~GetAttachment() != AT_NoAttachment)
+      AddMenuItem(Format("$CtxDetachAttachment$", GetName(0, AttachmentIcon(obj->GetAttachment()))), "DetachAttachment", 0, this, 0, obj, 0, 4, obj);
 
   return true;
 }
 
 public func DetachAttachment(dummy, object pWeapon)
 {
-	if(!pWeapon->~IsWeapon2() || pWeapon->~GetAttachment() == AT_NoAttachment)
-		return;
-	
-	var att = pWeapon->~GetAttachment();
-	pWeapon->SetAttachment(AT_NoAttachment);
+  if(!pWeapon->~IsWeapon2() || pWeapon->~GetAttachment() == AT_NoAttachment)
+    return;
 
-	var obj = CreateObject(WNAT, 0, 0, GetOwner());
-	obj->SetAttachment(att);
-	Collect(obj, this);
-	//CreateContents(WNAT, this)->SetAttachment(att);
-	
-	return true;
+  var att = pWeapon->~GetAttachment();
+  pWeapon->SetAttachment(AT_NoAttachment);
+
+  //Waffenaufsatzobjekt erstellen
+  var obj = CreateObject(WNAT, 0, 0, GetOwner());
+  obj->SetAttachment(att);
+  Collect(obj, this);
+
+  //Waffen-HUD aktualisieren
+  UpdateCharge(true);
+
+  Sound("WNAT_RemoveAttachement.ogg");
+
+  return true;
 }
 
 public func HasAttachmentWeapons()
 {
-	var obj, i;
-	while(obj = Contents(i++))
-		if(obj->~IsWeapon2() && obj->~GetAttachment() != AT_NoAttachment)
-			return true;
+  var obj, i;
+  while(obj = Contents(i++))
+    if(obj->~IsWeapon2() && obj->~GetAttachment() != AT_NoAttachment)
+      return true;
 }
 
-protected func FunnyBug() { return HasGear() || HasAttachmentWeapons(); }
+protected func FunnyBug()	{return HasGear() || HasAttachmentWeapons();}
 
 //Wegen der OpenBeta: Damit der Menüeintrag nicht von dem "Ausrüstung ablegen" Eintrag verschiebt.
 
