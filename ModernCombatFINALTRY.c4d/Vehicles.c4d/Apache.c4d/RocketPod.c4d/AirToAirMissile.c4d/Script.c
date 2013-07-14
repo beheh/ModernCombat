46924ca5896e7883ba7c832c31133448c8a78984
Michael14 <michael@missile.beheh.de>
2013-07-14 12:22:37 +0000

    * MAV-Station: Erste Cooldown-Erhöhung von 100 auf 50 halbiert. * Apache: Raketenstartgeschwindigkeit von 25 auf 40 erhöht.

--- ModernCombatFINALTRY.c4d/Vehicles.c4d/Apache.c4d/RocketPod.c4d/AirToAirMissile.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Vehicles.c4d/Apache.c4d/RocketPod.c4d/AirToAirMissile.c4d/Script.c
@@ -6,11 +6,11 @@
 
 public func MaxTime()		{return 100;}			//Maximale Flugzeit
 
-public func StartSpeed()	{return 25;}			//Startgeschwindigkeit
+public func StartSpeed()	{return 40;}			//Startgeschwindigkeit
 public func Acceleration()	{return 4;}			//Beschleunigung
 public func MaxSpeed()		{return 110;}			//Maximale Geschwindigkeit
 
-public func SecureTime()	{return 18;}			//Mindestflugzeit
+public func SecureTime()	{return 14;}			//Mindestflugzeit
 
 public func ExplosionDamage()	{return 24;}			//Explosionsschaden
 
