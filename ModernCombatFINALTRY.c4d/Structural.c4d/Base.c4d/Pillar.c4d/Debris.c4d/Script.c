70c5ef0d87ff3891e1a61609e4cd5575c473934d
Michael14 <michael@missile.beheh.de>
2013-07-20 14:18:15 +0000

    + Säule und Trümmer eingeführt. * Maststück: Aufpralleffekte erweitert.

---
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/Pillar.c4d/Debris.c4d/Script.c
@@ -0,0 +1,33 @@
+/*-- Trümmer --*/
+
+#strict 2
+#include ATRP
+
+
+/* Initalisierung */
+
+func Initialize()
+{
+  //Zufälliges Aussehen
+  SetAction("Be");
+  SetPhase(RandomX(0,4));
+
+  AddEffect("DmgObjects", this, 101, 5, this);
+}
+
+/* Aufschlag */
+
+public func Hit(int iXDir, int iYDir)
+{
+  Sound("StructureHit*.ogg");
+
+  //Spielerbildschirme schütteln
+  ShakeViewPort(30, this);
+
+  //Effekte
+  if(GetEffectData(EFSM_ExplosionEffects) > 0) CastSmoke("GunSmoke",4,10,0,0,500,100,RGBa(255,255,255,0));
+  if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("ConcreteSplinter", 8, 80, 0, 0, 20, 50);
+  CastSmoke("Smoke",4,10,0,0,500,100,RGBa(255,255,255,0));
+
+  return true;
+}
