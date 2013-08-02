a59c207f71d6c407d252c267990513bcce9285a4
Michael14 <michael@missile.beheh.de>
2013-08-02 19:30:11 +0000

    * 5? Mehr 50, aber fast ;)

--- cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/Occupation.c4d/FlagPost.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Goals.c4d/Occupation.c4d/FlagPost.c4d/Script.c
@@ -245,7 +245,6 @@ public func UpdateFlag()
   {
     bar = CreateObject(SBAR, 0, 0, -1);
     bar->Set(this, RGB(255, 255, 255), BAR_FlagBar, 100, 0, SM21, 0, 0, true);
-    bar->ChangeDefOffset(-5);
     //bar->PositionToVertex(0, true);
     bar->SetIcon(0, SM21, 0, 0, 32);
     bar->Update(0, true, true);
