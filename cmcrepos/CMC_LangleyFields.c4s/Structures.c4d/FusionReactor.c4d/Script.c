000e9c36dd025cd94af75b8050f71a8f069fd5d8
Michael14 <michael@missile.beheh.de>
2013-08-05 18:45:04 +0000

    + Erstrevision.

---
+++ cmcrepos/CMC_LangleyFields.c4s/Structures.c4d/FusionReactor.c4d/Script.c
@@ -0,0 +1,21 @@
+/*-- Fusionsreactor --*/
+
+#strict 2
+#include CCBS
+
+local basement;	//Fundament
+
+
+/* Konstruktion */
+
+protected func Construction()
+{
+  basement = CreateObject(BT02,0,8,GetOwner());
+}
+
+/* Initalisierung */
+
+protected func Initialize()
+{
+  Sound("CCBS_PowerUp.ogg");
+}
