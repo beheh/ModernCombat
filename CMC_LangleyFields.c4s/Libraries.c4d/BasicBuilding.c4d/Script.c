e7dff386cc90b77c0c61c263aac19778bb741b2d
ClonkGeist <clonkgeist@missile.beheh.de>
2013-08-20 11:44:08 +0000

    * Gebäudehauptmenü eingebaut (Untermenüs fehlen)

--- CMC_LangleyFields.c4s/Libraries.c4d/BasicBuilding.c4d/Script.c
+++ CMC_LangleyFields.c4s/Libraries.c4d/BasicBuilding.c4d/Script.c
@@ -7,6 +7,7 @@ local iEnergyAmount, aObjectList, aUpgradeList;
 
 public func IsCMCBuilding()		{return true;}
 
+public func IsBase()					  {return false;}				//Heimatbasis
 public func BuildingRadius()		{return 0;}				//Bauradius
 public func NeedBuildingRadius()	{return true;}				//Nur in Bauradius
 public func TechLevel()			{return 0;}				//Benötigte Techstufe
@@ -73,6 +74,44 @@ public func Destruction()
 	
 	return true;
 }
+
+/* Gebäudemenü */
+
+public func ContainedUp(object pCaller)
+{
+	OpenBuildingMenu(pCaller);
+	return true;
+}
+
+public func OpenBuildingMenu(object pMenuObj)
+{
+	CreateMenu(GetID(), pMenuObj, this, C4MN_Extra_None, GetName(this), 0, C4MN_Style_Dialog);
+	//Gebäudespezifische Menüeinträge
+	if(AdditionalBuildingMenu())
+		AddMenuItem(" ", 0, 0, pMenuObj);
+	
+	//Upgrademenü
+	AddMenuItem("$UpgradeMenu$", "OpenUpgradeMenu", CCUS , pMenuObj);
+	//Kaufmenü
+	if(this->~IsBase())
+		AddMenuItem("$BuyMenu$", "OpenBuyMenu", GOLD, pMenuObj);
+	
+	return true;
+}
+
+public func AdditionalBuildingMenu(object pMenuObj) { }
+
+public func OpenUpgradeMenu(id dummy, object pMenuObj)
+{
+	
+}
+
+public func OpenBuyMenu(id dummy, object pMenuObj)
+{
+	
+}
+
+/* Energieversorgung */
 
 public func EnergySupply(int iAmount, object pBy)
 {
