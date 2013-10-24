fc6d5b128e8674710e516ce1ab48809860298831
Michael14 <michael@missile.beheh.de>
2013-10-24 19:51:32 +0000

    * Maximal tragbare Raketenmunition im Munitionsgürtel von 15 auf 12 gesenkt.

--- cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Ammo.c4d/MissileBox.c4d/Missiles.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Ammo.c4d/MissileBox.c4d/Missiles.c4d/Script.c
@@ -4,5 +4,5 @@
 
 
 public func IsAmmo()		{return true;}	//Ist Munitionsart
-public func MaxAmmo()		{return 15;}	//Maximal tragbar
+public func MaxAmmo()		{return 12;}	//Maximal tragbar
 public func GetPointFactor()	{return 10;}
