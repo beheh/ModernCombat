caa9fb63f0bac02e72be47c9d3b1d92919bbb0de
Michael14 <michael@missile.beheh.de>
2013-10-28 12:36:45 +0000

    * Script gesäubert.

--- ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/ArtilleryBattery.c4d/Shell.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/ArtilleryBattery.c4d/Shell.c4d/Script.c
@@ -7,7 +7,7 @@ local fHit;
 
 public func IsBulletTarget()	{return true;}
 public func AllowHitboxCheck()	{return true;}
-public func IsArtilleryShell() {return true;}
+public func IsArtilleryShell()	{return true;}
 
 
 /* Initialisierung */
