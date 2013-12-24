02e5928c8d87f8403a931a0b9e5e0ca6e3d9bbd4
Michael14 <michael@missile.beheh.de>
2013-12-24 00:09:55 +0000

    * Revolver-Kugeln können nun bis zu 3 Ziele durchschlagen.

--- cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Weapons.c4d/Revolver.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Weapons.c4d/Revolver.c4d/Script.c
@@ -38,25 +38,28 @@ public func FMData1(int data)
 {
   if(data == FM_Name)		return "$Bullets$";
 
-  if(data == FM_AmmoID)		return STAM;	//ID der Munition
-  if(data == FM_AmmoLoad)	return 6;	//Magazingröße
-  if(data == FM_AmmoUsage)	return 1;	//Munition pro Schuss
+  if(data == FM_AmmoID)		return STAM;						//ID der Munition
+  if(data == FM_AmmoLoad)	return 6;						//Magazingröße
+  if(data == FM_AmmoUsage)	return 1;						//Munition pro Schuss
 
-  if(data == FM_Reload)		return 90;	//Zeit für Nachladen
-  if(data == FM_Recharge)	return 15;	//Zeit bis erneut geschossen werden kann
+  if(data == FM_Reload)		return 90;						//Zeit für Nachladen
+  if(data == FM_Recharge)	return 15;						//Zeit bis erneut geschossen werden kann
 
-  if(data == FM_SingleReload)	return 7;	//Zeit des einzelnen Nachladens bei Revolversystemen
-  if(data == FM_PrepareReload)	return 10;	//Zeit bevor das eigentliche Nachladen beginnt
-  if(data == FM_FinishReload)	return 20;	//Zeit nach dem Nachladen
+  if(data == FM_SingleReload)	return 7;						//Zeit des einzelnen Nachladens bei Revolversystemen
+  if(data == FM_PrepareReload)	return 10;						//Zeit bevor das eigentliche Nachladen beginnt
+  if(data == FM_FinishReload)	return 20;						//Zeit nach dem Nachladen
 
-  if(data == FM_Damage)		return 21;	//Schadenswert
+  if(data == FM_Damage)		return 21;						//Schadenswert
 
-  if(data == FM_Slot)		return 1;	//Slot des Feuermodus
+  if(data == FM_Slot)		return 1;						//Slot des Feuermodus
 
   if(data == FM_SpreadAdd)	return 110 - (iAttachment == AT_Laserpointer)*4;	//Bei jedem Schuss hinzuzuaddierende Streuung
   if(data == FM_StartSpread)	return 30 - (iAttachment == AT_Laserpointer)*20;	//Bei Auswahl der Waffe gesetzte Streuung
   if(data == FM_MaxSpread)	return 220 - (iAttachment == AT_Laserpointer)*70;	//Maximaler Streuungswert
   if(data == FM_MinSpread)	return 20 - (iAttachment == AT_Laserpointer)*10;	//Kleinstmögliche Streuung
+
+  if(data == FM_MultiHit)	return 3;						//Anzahl möglicher Treffer pro Kugel
+  if(data == FM_MultiHitReduce)	return 50;						//Schadensreduzierung pro Treffer
 
   return Default(data);
 }
