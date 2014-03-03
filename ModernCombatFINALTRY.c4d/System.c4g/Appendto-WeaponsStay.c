24fca9a08f0fbade6bf85d037bd5f533e1da354f
Michael14 <michael@missile.beheh.de>
2014-03-03 22:54:39 +0000

    * Waffen bleiben-Regel um Funktion erweitert, die beitretenden Spielern die Sicht auf Spawnpunkte aktualisiert um Fehlanzeigen zu verhindern. (#012473)

---
+++ ModernCombatFINALTRY.c4d/System.c4g/Appendto-WeaponsStay.c
@@ -0,0 +1,17 @@
+/*-- Waffen bleiben --*/
+
+//Waffen bleiben-Regel aktualisiert die Sichtbarkeit von respawnenden Items für beitretende Spieler.
+
+#strict 2
+
+#appendto WPST
+
+
+protected func InitializePlayer(int iPlr)
+{  
+  for(var spwnpnt in FindObjects(Find_ID(SPNP)))
+    if(GetPlayerType(GetPlayerByIndex(iPlr)))
+      if(!(Local(0,spwnpnt) & 1<<GetPlayerByIndex(iPlr)))
+        Local(0,spwnpnt) += 1<<GetPlayerByIndex(iPlr);
+  return(1);
+}
