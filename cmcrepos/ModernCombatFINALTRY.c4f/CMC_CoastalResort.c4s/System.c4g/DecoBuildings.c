eca292dad54c33d52a741b7f7e14aa7adf778167
Michael14 <michael@missile.beheh.de>
2014-04-17 20:42:53 +0000

    + Coastal Resort eingeführt. * Foldermap aktualisiert.

---
+++ cmcrepos/ModernCombatFINALTRY.c4f/CMC_CoastalResort.c4s/System.c4g/DecoBuildings.c
@@ -0,0 +1,21 @@
+/*-- Dekorative Gebäude --*/
+
+//Bestimmte Gebäude können als unzerstörbare Landschaftsobjekte benutzt werden.
+
+#strict 2
+#appendto HUT1
+
+protected func Incineration()	{Extinguish();}
+public func OnDmg()		{return 0;}
+protected func Construction()	{return 0;}	//Kein Fundament
+
+
+/* Initialisierung */
+
+protected func Initialize()
+{
+  //In den Hintergrund verschieben
+  SetCategory(C4D_StaticBack);
+  SetCategory(C4D_Background);
+  return 1;
+}
