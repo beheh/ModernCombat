429d5a4016bfe5cb0b47d0b2b3a0498d4af9f11b
Monkstar <monkstar@missile.beheh.de>
2014-04-26 22:50:37 +0000

    * Schalldämpfer: Stochastische Repräsentation der Kommazahlen entfernt, jetzt nur noch Aufrundung der Schadenswerte bei Divisionsrest.

--- cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Weapons.c4d/Pistol.c4d/Shot.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Weapons.c4d/Pistol.c4d/Shot.c4d/Script.c
@@ -304,17 +304,17 @@ public func OnBulletHit(object pObject, int iX, int iY)
 public func BulletStrike(object pObj)
 {
   if(pObj)
-    if(iAttachment != AT_Silencer)
-      DoDmg(iDamage, DMG_Projectile, pObj, 0, 0, 0, iAttachment);
+  	if(iAttachment != AT_Silencer)
+    	DoDmg(iDamage, DMG_Projectile, pObj, 0, 0, 0, iAttachment);
     else
     {
-      //Schadensreduktion bei schallgedämpften Waffen
-      var x = 5;
-      var stepsize = 150;
-      var maxsteps = 3;
-      var step = Min(maxsteps, (Distance(GetX(), GetY(), GetX(pObj), GetY(pObj)) / stepsize));
-      var newDamage = iDamage - (((iDamage * step) / x) + (Random(x) < ((iDamage * step) % x)));
-      DoDmg(newDamage, DMG_Projectile, pObj, 0, 0, 0, iAttachment);
+    	var x = 9;
+    	var stepsize = 75;
+    	var maxsteps = 6;
+			var step = BoundBy((Distance(GetX(), GetY(), GetX(pObj), GetY(pObj)) / stepsize), 1, maxsteps) - 1;
+			var newDamage = iDamage - ((iDamage * step) / x);
+			Message(Format("%d red: %d step: %d dist: %d", newDamage, ((iDamage * step) / x), step, Distance(GetX(), GetY(), GetX(pObj), GetY(pObj))), pObj);
+			DoDmg(newDamage, DMG_Projectile, pObj, 0, 0, 0, iAttachment);
     }
   return true;
 }
