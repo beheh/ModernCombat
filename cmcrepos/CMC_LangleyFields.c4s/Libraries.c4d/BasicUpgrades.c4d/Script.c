663786b35b0435c6bd8013a25d1e5bbf4d39f6fd
Michael14 <michael@missile.beheh.de>
2014-05-14 18:51:23 +0000

    * Script gesäubert.

--- cmcrepos/CMC_LangleyFields.c4s/Libraries.c4d/BasicUpgrades.c4d/Script.c
+++ cmcrepos/CMC_LangleyFields.c4s/Libraries.c4d/BasicUpgrades.c4d/Script.c
@@ -2,33 +2,34 @@
 
 #strict 2
 
-public func IsGroupUpgrade() { return false; } //Teamglobales Gruppenupgrade
-public func ResearchCost()	 { return 1; } //Entwicklungskosten
-public func ResearchDuration() {return 100;} //Entwicklungsdauer in Frames
+public func IsGroupUpgrade()			{return false;}	//Teamglobales Gruppenupgrade
+public func ResearchCost()			{return 1;}	//Entwicklungskosten
+public func ResearchDuration()			{return 100;}	//Entwicklungsdauer in Frames
 
-public func ResearchBase()   { return []; } //Upgrades, die bereits erforscht sein müssen
-public func UpgradeConditions(object pByObj) {return true;} //Zusätzliche Bedingungen
+public func ResearchBase()			{return [];}	//Upgrades, die bereits erforscht sein müssen
+public func UpgradeConditions(object pByObj)	{return true;}	//Zusätzliche Bedingungen
+
 
 public func CanBeResearched(object pByObj)
 {
-	var upgradeList;
-	if(!IsGroupUpgrade())
-		upgradeList = GetTeamUpgradeList(GetPlayerTeam(GetOwner(pByObj)));
-	else
-		upgradeList = pByObj->~GetUpgradeList();
-	
-	for(var base in ResearchBase())
-		if(GetIndexOf(base, upgradeList) == -1)
-			return false;
-	
-	return true;
+  var upgradeList;
+  if(!IsGroupUpgrade())
+    upgradeList = GetTeamUpgradeList(GetPlayerTeam(GetOwner(pByObj)));
+  else
+    upgradeList = pByObj->~GetUpgradeList();
+
+  for(var base in ResearchBase())
+    if(GetIndexOf(base, upgradeList) == -1)
+      return false;
+
+  return true;
 }
 
 public func Researched(object pBy)
 {
-	if(!IsGroupUpgrade() && pBy && pBy->~IsCMCBuilding())
-		return pBy->AddUpgrade(GetID());
-	
-	if(IsGroupUpgrade())
-		return AddTeamUpgrade(GetPlayerTeam(GetOwner(pBy)), GetID());
+  if(!IsGroupUpgrade() && pBy && pBy->~IsCMCBuilding())
+    return pBy->AddUpgrade(GetID());
+
+  if(IsGroupUpgrade())
+    return AddTeamUpgrade(GetPlayerTeam(GetOwner(pBy)), GetID());
 }
