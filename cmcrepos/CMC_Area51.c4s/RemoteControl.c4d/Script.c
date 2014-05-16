c4b3a68d0692b54e65683f44fb42e52a0f7adff7
Monkstar <monkstar@missile.beheh.de>
2014-05-16 21:52:46 +0000

    * Fernsteuerung: Jump and Run gefixt, weitere Steuerungsverbesserungen.

--- cmcrepos/CMC_Area51.c4s/RemoteControl.c4d/Script.c
+++ cmcrepos/CMC_Area51.c4s/RemoteControl.c4d/Script.c
@@ -5,6 +5,8 @@
 local pTarget;
 local fControlling;
 local pClonk;
+local throwActive;
+local lastMovement; //1. (niedrigstes) Bit: Links 2. Bit: Rechts 3. Bit: Hoch 4. Bit: Runter
 
 public func IsDrawable()	{return true;}	//Wird sichtbar getragen
 public func HandX()		{return 4000;}
@@ -16,7 +18,7 @@ func Activate(object pCaller) {
   if(fControlling)
   {
   	Cancel();
-  	return;
+  	return true;
   }
   
   if(pTarget)
@@ -25,6 +27,7 @@ func Activate(object pCaller) {
 		Sound("Bsss.wav", 1, 0, 0, GetOwner(pCaller));
 		fControlling = true;
 		SetPlrView(GetOwner(pClonk), ViewTarget());
+		ObjectSetAction(pCaller, "Heal");
 		if(!pTarget->~ActivateEntrance(pCaller, true))
 			Cancel();
   }
@@ -45,7 +48,7 @@ func ControlThrow(object pCaller)
 	
 	var pGun = FindObject2(Find_AtPoint(), Find_Or(Find_Func("IsGunEmplacement"), Find_Func("IsMAVStation")));
 
-	if(pTarget)
+	if(pTarget && pGun)
 	{
 		var pPrevRemote = LocalN("pRemoteControl", pTarget);
 		if(pPrevRemote)
@@ -55,9 +58,11 @@ func ControlThrow(object pCaller)
 			LocalN("pTarget", pPrevRemote) = 0;
 		}
 	}
-	pTarget = pGun;
-	if(pTarget)
+	if(pGun)
+	{
+		pTarget = pGun;
 		LocalN("pRemoteControl", pTarget) = this;
+	}
 	return true;
 }
 
@@ -66,6 +71,8 @@ func Cancel()
 	fControlling = false;
 	if(pClonk)
 		SetPlrView(GetOwner(pClonk), pClonk);
+	if(GetAction(pClonk) == "Heal")
+		ObjectSetAction(pClonk, "Walk");
 	
 }
 
@@ -81,6 +88,17 @@ func ViewTarget()
 	}
 }
 
+func Check()
+{
+	if(!fControlling)
+		return;
+
+	if(WildcardMatch(GetAction(pClonk), "Walk*"))
+		return ObjectSetAction(pClonk, "Heal");
+	else if(GetAction(pClonk) != "Heal")
+		return Cancel();
+}
+
 func RelayControl(object pCaller, string szControl)
 {
 	if(!fControlling)
@@ -89,23 +107,62 @@ func RelayControl(object pCaller, string szControl)
 	if(pTarget)
 	{
 		SetPlrView(GetOwner(pClonk), ViewTarget());
-		pTarget->eval(Format("%s(%d)", szControl, pCaller));
+		if(pTarget->~IsGunEmplacement() && WildcardMatch(szControl, "*Double"))
+			return true;
+		pTarget->~eval(Format("%s(%d)", szControl, pCaller));
 		return true;
 	}
 }
 
 func ControlUpdate(object pCaller, int comdir, bool dig, bool throw) 
 {
-	return true;
+	if(fControlling)
+	{
+		if(WildcardMatch(GetAction(pClonk), "Walk*"))
+			ObjectSetAction(pClonk, "Heal");
+		else if(GetAction(pClonk) != "Heal")
+			return Cancel();
+		pTarget->~ControlUpdate(pCaller, comdir, dig, throw);
+		//1. (niedrigstes) Bit: Links 2. Bit: Rechts 3. Bit: Hoch 4. Bit: Runter
+		
+		var newMovement = 0;
+		if(comdir == COMD_Left || comdir == COMD_UpLeft|| comdir == COMD_DownLeft)
+			newMovement += 1;
+		if(comdir == COMD_Right || comdir == COMD_UpRight|| comdir == COMD_DownRight)
+			newMovement +=2;
+		if(comdir == COMD_Up || comdir == COMD_UpLeft || comdir == COMD_UpRight)
+			newMovement +=4;
+		if(comdir == COMD_Down || comdir == COMD_DownLeft || comdir == COMD_DownRight)
+			newMovement +=8;
+
+		if(lastMovement == newMovement)
+			return lastMovement = newMovement;
+		
+		if(lastMovement & 1 && !(newMovement & 1))
+			pTarget->~ControlLeftReleased(pCaller);
+			
+		if(lastMovement & 2 && !(newMovement & 2))
+			pTarget->~ControlRightReleased(pCaller);
+			
+		if(lastMovement & 4 && !(newMovement & 4))
+			pTarget->~ControlUpReleased(pCaller);
+			
+		if(lastMovement & 8 && !(newMovement & 8))
+			pTarget->~ControlDownReleased(pCaller);
+			
+		lastMovement = newMovement;
+	}
 }
 
 func ControlLeft(object pCaller)
 {
+
 	return RelayControl(pCaller, "ControlLeft");
 }
 
 func ControlLeftDouble(object pCaller)
 {
+
 	return RelayControl(pCaller, "ControlLeftDouble");
 }
 
@@ -116,11 +173,13 @@ func ControlLeftReleased(object pCaller)
 
 func ControlRight(object pCaller)
 {
+	//lastMovement = "Right";
 	return RelayControl(pCaller, "ControlRight");
 }
 
 func ControlRightDouble(object pCaller)
 {
+	//lastMovement = "Right";
 	return RelayControl(pCaller, "ControlRightDouble");
 }
 
@@ -131,11 +190,13 @@ func ControlRightReleased(object pCaller)
 
 func ControlUp(object pCaller)
 {
+	//lastMovement = "Up";
 	return RelayControl(pCaller, "ControlUp");
 }
 
 func ControlUpDouble(object pCaller)
 {
+	//lastMovement = "Up";
 	return RelayControl(pCaller, "ControlUpDouble");
 }
 
@@ -146,11 +207,13 @@ func ControlUpReleased(object pCaller)
 
 func ControlDown(object pCaller)
 {
+	//lastMovement = "Down";
 	return RelayControl(pCaller, "ControlDown");
 }
 
 func ControlDownDouble(object pCaller)
 {
+	//lastMovement = "Down";
 	return RelayControl(pCaller, "ControlDownDouble");
 }
 
@@ -164,8 +227,14 @@ func ControlDig(object pCaller)
 	return RelayControl(pCaller, "ControlDigSingle");
 }
 
+func ControlDigDouble(object pCaller)
+{
+	return Activate(pCaller);
+}
+
 func Entrance(object pContainer)
 {
+	lastMovement = 0;
 	pClonk = pContainer;
 	SetOwner(GetOwner(pClonk));
 }
