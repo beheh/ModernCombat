1a778f5ac4fe4bb643c299c23000e243ea711e74
Michael14 <michael@missile.beheh.de>
2014-05-18 00:03:15 +0000

    + Stacheldraht: Sound eingefügt. * Stacheldraht um Effekte erweitert. * Stacheldraht: Beschreibungen eingefügt. * Script gesäubert.

--- CMC_Area51.c4s/BarbedWire.c4d/Script.c
+++ CMC_Area51.c4s/BarbedWire.c4d/Script.c
@@ -5,36 +5,57 @@
 
 local iHits;
 
-public func MaxDamage()			{return 120;}
-public func AutoRepairDuration()	{return 0;}
-public func BuildCost()			{return 10;}
+public func MaxDamage()			{return 100;}	//Maximalschaden
+public func AutoRepairDuration()	{return 0;}	//Keine Selbstreparatur
+public func BuildCost()			{return 10;}	//Baukosten
 
-public func Initialize()		{iHits = 5; return _inherited(...);}
+
+/* Initialisierung */
+
+public func Initialize()
+{
+  //Maximalzahl an Treffern
+  iHits = 5;
+
+  return _inherited(...);
+}
+
+/* Timer */
 
 protected func Check()
 {
+  //Clonks suchen
   for(var obj in FindObjects(Find_InRect(-10, -10, 20, 15), Find_OCF(OCF_Alive), Find_Hostile(GetOwner()), Find_NoContainer()))
   {
-  	if(GetEffect("BarbedWireHit", obj))
-  		continue;
-  	
-  	DoDmg(20, 0, obj, 0, 0, GetID(this));
-  	iHits--;
-  	if(!iHits)
-  		Destroyed();
-  	
-  	AddEffect("BarbedWireHit", obj, 30, 100, 0, GetID(this));
+    if(GetEffect("BarbedWireHit", obj))
+      continue;
+
+    //Schaden verursachen und abnutzen
+    DoDmg(20, 0, obj, 0, 0, GetID(this));
+    iHits--;
+    if(!iHits)
+      Destroyed();
+
+    //Clonk beeinflussen
+    AddEffect("BarbedWireHit", obj, 30, 75, 0, GetID(this));
+
+    //Effekte
+    if(GetEffectData(EFSM_ExplosionEffects) > 1) CastSmoke("Smoke3",4,5,0,0,40,80);
+    if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("MetalSplinter",4,40,0,0,20,40);
+    Sound("BarbwireCut*.ogg",0,obj);
   }
 }
+
+/* Lähmungseffekt */
 
 func FxBarbedWireHitStart(object pTarget, int iEffectNumber, int iTemp)
 {
   //Lähmung
-  SetPhysical("Walk", GetPhysical("Walk", 2, pTarget)/3, 3, pTarget);
-  SetPhysical("Jump", GetPhysical("Jump", 2, pTarget)/3, 3, pTarget);
-  SetPhysical("Scale", GetPhysical("Scale", 2, pTarget)/3, 3, pTarget);
-  SetPhysical("Hangle", GetPhysical("Hangle", 2, pTarget)/3, 3, pTarget);
-  SetPhysical("Swim", GetPhysical("Swim", 2, pTarget)/3, 3, pTarget);
+  SetPhysical("Walk", GetPhysical("Walk", 2, pTarget)/4, 3, pTarget);
+  SetPhysical("Jump", GetPhysical("Jump", 2, pTarget)/4, 3, pTarget);
+  SetPhysical("Scale", GetPhysical("Scale", 2, pTarget)/4, 3, pTarget);
+  SetPhysical("Hangle", GetPhysical("Hangle", 2, pTarget)/4, 3, pTarget);
+  SetPhysical("Swim", GetPhysical("Swim", 2, pTarget)/4, 3, pTarget);
 }
 
 public func FxBarbedWireHitStop(object pTarget, no, reason, temp)
@@ -47,8 +68,17 @@ public func FxBarbedWireHitStop(object pTarget, no, reason, temp)
   ResetPhysical(pTarget, "Swim");
 }
 
+/* Zerstörung */
+
 public func Destroyed()
 {
   fDestroyed = true;
-	FadeOut(this);
+
+  //Effekte
+  if(GetEffectData(EFSM_ExplosionEffects) > 1) CastSmoke("Smoke3",4,20,0,0,100,300);
+  if(GetEffectData(EFSM_ExplosionEffects) > 1) CastParticles("MetalSplinter",8,60,0,0,40,60);
+  Sound("FenceDestruct.ogg");
+
+  //Verschwinden
+  FadeOut(this);
 }
