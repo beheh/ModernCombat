63abdf3ba9a3e73b8ed609b750f95dec19ee9d09
Michael14 <michael@missile.beheh.de>
2014-05-23 20:21:45 +0000

    * Script gesäubert.

--- cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Grenades.c4d/PhosphorGrenade.c4d/Phosphor.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Items.c4d/Grenades.c4d/PhosphorGrenade.c4d/Phosphor.c4d/Script.c
@@ -9,11 +9,17 @@ local light,last_size,life;
 
 func Initialize()
 {
+  //Brennzeit setzen
   life = 300+Random(100);
+
+  //Effekte
   light = AddLight(40,RGB());
   AddFireEffect(this,0,RGB(0,255,255),0,-5 - Random(15));
+
+  //Besitzer setzen und Timer ausführen
   SetController(GetOwner());
   Timer();
+
   return 1;
 }
 
@@ -34,7 +40,7 @@ func Timer()
     speed = BoundBy (speed,1,40);
     speed = (255/40)*speed;	//Erhöhen von 0 auf 255
 
-  //Färbung
+    //Färbung
     var rgb = RGB(speed,BoundBy(speed,140,255),255);
     SetClrModulation(rgb);
     if(light)
@@ -49,7 +55,6 @@ func Timer()
     if(!Random(20))
       CastObjects(SPRK, 1, 20);
 
-
   //Brennzeit verwalten
   life--;
   if(GBackLiquid())
@@ -60,6 +65,8 @@ func Timer()
   if(life <= 0)
     RemoveObject();
 }
+
+/* Objekte verbrennen */
 
 func BurnObjects()
 {
@@ -114,7 +121,7 @@ public func FxPhosphoredStart(pTarget, iNo, iTemp, pPhosphor)
     return -1;
   if(GetLength(FindObjects(Find_ID(GetID()), Find_ActionTarget(pTarget))) > 3)
     return -1;
-    EffectVar(0, pTarget, iNo) = this; //Der Klumpen
+  EffectVar(0, pTarget, iNo) = this;
   SetAction("AGlobbing", pTarget);
 }
 
