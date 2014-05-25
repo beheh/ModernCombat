6b9f9b760defe48bf3a6fc79f6bc53c7092b62f7
ClonkGeist <clonkgeist@missile.beheh.de>
2014-05-25 17:21:28 +0000

    * CMC Liga: Grenzen für DM, LMS und CTF eingeführt

--- ModernCombatFINALTRY.c4d/System.c4g/Appendto-LastManStanding.c
+++ ModernCombatFINALTRY.c4d/System.c4g/Appendto-LastManStanding.c
@@ -101,3 +101,11 @@ public func OnPlayerRankUp(int iPlr)
 
   return true;
 }
+
+public func WinScoreChange(int iNewScore)
+{
+	if(GetLeague())
+		iNewScore = BoundBy(iNewScore, 5, 15);
+	
+	return iNewScore;
+}
