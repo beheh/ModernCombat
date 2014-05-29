df10a671ba2b78da1b8834a9f5ca6a35bcf98f4a
Michael14 <michael@missile.beheh.de>
2014-05-29 15:44:59 +0000

    * Sendemast: Nutzt neuen Zerstörungssound.

--- cmcrepos/ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/AntennaTower.c4d/Script.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Structural.c4d/Base.c4d/AntennaTower.c4d/Script.c
@@ -213,6 +213,7 @@ protected func PrepareCollapse()
   CastParticles("ConcreteSplinter", RandomX(8,16), 80, 0, 250, 20, 50);
 
   //Sound
+  Sound("StructureIntegrity*.ogg");
   Echo("TowerBreaking.ogg");
 }
 
