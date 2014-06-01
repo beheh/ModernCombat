bd1f8e349e5cd2e145b32813727ef296ae0fd2b2
Monkstar <monkstar@missile.beheh.de>
2014-06-01 15:29:10 +0000

    + Achievements: Börd-Achievement eingebaut (Grafik und Text fehlen)

---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/53Bird.c4d/Script.c
@@ -0,0 +1,14 @@
+/*-- Börd --*/
+
+#strict 2
+#include ACHV
+
+public func GetSavingSlot()
+{
+  return 53;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 0;	//Benötigte Punktzahl einfügen
+}
