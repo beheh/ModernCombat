3c028c086d728a7779ff47a6f6fd910637c8dff2
Monkstar <monkstar@missile.beheh.de>
2014-06-01 15:29:10 +0000

    + Achievements: Börd-Achievement eingebaut (Grafik und Text fehlen)

--- cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/AnimalSkins.c4d/System.c4g/Appendto-Bird.c
+++ cmcrepos/ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/AnimalSkins.c4d/System.c4g/Appendto-Bird.c
@@ -6,13 +6,38 @@
 
 #appendto BIRD
 
+local fHasNewSkin;
 
 /* Initialisierung */
 
 public func Initialize()
 {
   if(!Random(20))
+  {
     SetGraphics(0, this, BRSN);
+    fHasNewSkin = true;
+  }
 
   return inherited();
+}
+
+protected func Death()
+{
+	if(GetKiller() != NO_OWNER)
+	{
+		var killer = GetKiller();
+		var progress = GetAchievementExtra(AC53, killer);
+		if(!progress)
+			progress = CreateArray(2);
+		if(!fHasNewSkin)
+			progress[0]++;
+		else
+			progress[1]++;
+		SetAchievementExtra(progress, AC53, killer);
+		
+		if(progress[0] > 1 && progress[1] > 0)
+			AwardAchievement(AC53, killer);
+	}
+
+  _inherited(...);
 }
