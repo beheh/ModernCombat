850e11436dbfbb2fa4c4bfdbdae4426ad0ddb228
ClonkGeist <clonkgeist@missile.beheh.de>
2014-06-05 17:59:03 +0000

    * Raffinerie: Verarbeitung von Ressourcen

---
+++ CMC_LangleyFields.c4s/Structures.c4d/Refinery.c4d/Resource.c4d/Script.c
@@ -0,0 +1,43 @@
+/*-- Ressource --*/
+
+#strict 2
+
+local value, duration;
+
+public func IsDrawable()	{return true;}	//Wird sichtbar getragen
+public func HandSize()		{return 800;}
+public func HandX()		{return 6000;}
+public func HandY()		{return -1000;}
+
+/* Inhalt */
+
+protected func Set(int iValue, int iDuration)
+{
+  value = iValue;
+  duration = iDuration;
+}
+
+public func GetVal() { return value; }
+public func GetDuration() { return duration; }
+
+/* Aktivierung */
+
+protected func Activate(object pClonk)
+{
+  SetWealth(GetOwner(pClonk), GetWealth(GetOwner(pClonk))+value/2);
+  Sound("GetCash*.ogg");
+}
+
+/* Sonstiges */
+
+protected func Hit()
+{
+  Sound("AmmoBoxHit*.ogg");
+  return 1;
+}
+
+protected func Selection()
+{
+  Sound("FAPK_Charge.ogg");
+  return 1;
+}
