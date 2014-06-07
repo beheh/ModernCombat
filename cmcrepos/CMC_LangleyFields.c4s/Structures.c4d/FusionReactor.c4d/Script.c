7b508f3554e42aae680ed19b8c4bd8b4aed1368a
ClonkGeist <clonkgeist@missile.beheh.de>
2014-06-07 18:00:49 +0000

    * Fusionsreaktor: Windanlage und Solarpanele funktionieren nun

--- cmcrepos/CMC_LangleyFields.c4s/Structures.c4d/FusionReactor.c4d/Script.c
+++ cmcrepos/CMC_LangleyFields.c4s/Structures.c4d/FusionReactor.c4d/Script.c
@@ -38,6 +38,8 @@ private func Smoke()
   CastSmoke("Smoke",3,10,40,-30,100,500);
 }
 
+/* Upgrades */
+
 public func OnResearchingUpgradeStart(int iEffect, id idUpgrade, int iDuration, int iCost)
 {
 	//Alle anderen Erweiterungen rausnehmen
@@ -48,12 +50,112 @@ public func OnResearchingUpgradeStart(int iEffect, id idUpgrade, int iDuration, 
 
 public func OnUpgrade(id idUpgrade)
 {
-	//Vorab keine wirkliche Funktion in Upgrades;
-
-	//ToDo: Ortsabhängiges Arbeiten der Upgrades anhand von Effekten
+	//[Allgemeine Effekte/Sounds]
 
 	if(idUpgrade == U_FR)
 		iAdditionalEnergy = 40;
+	else if(idUpgrade == U_SP)
+		SetupSolarPanel();
+	else if(idUpgrade == U_WA)
+		SetupWaterTurbine();
+	else if(idUpgrade == U_WT)
+		SetupWindTurbine();
+}
+
+public func UpgradeOffset(id idUpgrade, int &iX, int &iY)
+{
+	if(idUpgrade == U_SP)       //Solarpanele
+	{
+		 iX = -6;
+		 iY = -10;
+	}
+	else if(idUpgrade == U_WA)  //Wasserturbine
+	{
+		iX = -10;
+		iY = 8;
+	}
+	else if(idUpgrade == U_WT)  //Windanlage
+	{
+		iX = 16;
+		iY = -10;
+	}
+}
+
+public func SetAdditionalEnergy(int iValue) {return iAdditionalEnergy = iValue;}
+
+/* Upgrade: Solarpanele */
+
+public func SetupSolarPanel()
+{
+	//Eventuelle Starteffekte/-sounds	
+	AddEffect("SolarPanel", this, 100, 35, 0, GetID());
+	return true;
+}
+
+public func FxSolarPanelTimer(object pTarget, int iNr)
+{
+	//Offsets für Upgrade rausfinden
+	var x,y,active;
+	pTarget->~UpgradeOffset(U_SP, x, y);
+	x += GetX(pTarget);
+	y += GetY(pTarget);
+
+	var time;
+	
+	//Nur tagsüber benutzbar
+	if((time = FindObject2(Find_ID(TIME))) && time->IsDay())
+		active = true;
+	//Hintergrund ist Himmel?
+	else if(GetMaterial(x, y) == -1)
+		active = true;
 	else
-		iAdditionalEnergy = 50;
+	{
+		//Alternativ: Nach oben hin frei?
+		var simx = x, simy, simxdir, simydir = 1;
+		SimFlight(simx, simy, simxdir, simydir);
+		
+		if(simy >= y)
+			active = true;
+	}
+	
+	if(active)
+		pTarget->~SetAdditionalEnergy(50);
+	else
+		pTarget->~SetAdditionalEnergy(0);
+}
+
+/* Upgrade: Wasserturbine */
+
+public func SetupWaterTurbine()
+{
+	//Eventuelle Starteffekte/-sounds	
+	AddEffect("WaterTurbine", this, 100, 35, 0, GetID());
+	return true;
+}
+
+public func FxWaterTurbineTimer(object pTarget, int iNr)
+{
+	//...todo
+}
+
+/* Upgrade: Windanlge */
+
+public func SetupWindTurbine()
+{
+	//Eventuelle Starteffekte/-sounds	
+	AddEffect("WindTurbine", this, 100, 35, 0, GetID());
+	return true;
+}
+
+public func FxWindTurbineTimer(object pTarget, int iNr)
+{
+	var x,y;
+	pTarget->~UpgradeOffset(U_WT, x, y);
+	x += GetX(pTarget);
+	y += GetY(pTarget);
+	
+	//Zusätzliche Energie abhängig von Windstärke (0 bis 50 Energie)
+	pTarget->~SetAdditionalEnergy(Abs(GetWind(x, y))/2);
+	
+	return true;
 }
