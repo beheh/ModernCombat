56bdb0c06dc2a5333b5107f0947d7b951414e768
ClonkGeist <clonkgeist@missile.beheh.de>
2014-06-07 12:17:17 +0000

    * Upgradeentwicklung ausgebaut;

--- CMC_LangleyFields.c4s/Structures.c4d/FusionReactor.c4d/Script.c
+++ CMC_LangleyFields.c4s/Structures.c4d/FusionReactor.c4d/Script.c
@@ -4,10 +4,12 @@
 #include CCBS
 
 local basement;	//Fundament
+local aPUpgrades, iAdditionalEnergy;
 
 public func TechLevel()		{return 1;}	//Techstufe
 public func BuildingRadius()	{return 200;}	//Bauradius
-public func EnergyProduction()	{return 100;}	//Energiehersteller
+public func EnergyProduction()	{return 100+iAdditionalEnergy;}	//Energiehersteller
+public func PossibleUpgrades()  {return aPUpgrades;}
 
 
 /* Konstruktion */
@@ -15,6 +17,7 @@ public func EnergyProduction()	{return 100;}	//Energiehersteller
 protected func Construction()
 {
   basement = CreateObject(BT02,0,8,GetOwner());
+  aPUpgrades = [U_FR, U_SP, U_WA, U_WT];
 }
 
 /* Initalisierung */
@@ -33,4 +36,24 @@ private func Smoke()
   //Effekte
   CastSmoke("Smoke",3,10,-20,-30,100,500);
   CastSmoke("Smoke",3,10,40,-30,100,500);
+}
+
+public func OnResearchingUpgradeStart(int iEffect, id idUpgrade, int iDuration, int iCost)
+{
+	//Alle anderen Erweiterungen rausnehmen
+	aPUpgrades = [idUpgrade];
+
+	return true;
+}
+
+public func OnUpgrade(id idUpgrade)
+{
+	//Vorab keine wirkliche Funktion in Upgrades;
+
+	//ToDo: Ortsabhängiges Arbeiten der Upgrades anhand von Effekten
+
+	if(idUpgrade == U_FR)
+		iAdditionalEnergy = 40;
+	else
+		iAdditionalEnergy = 50;
 }
