88e9b3c56f4864959e276bc83046a43f77d7ab5c
Monkstar <monkstar@missile.beheh.de>
2014-06-10 16:23:25 +0000

    + Achievement: "Didn't see it coming" eingebaut (Grafik/Text fehlen).

---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/54DidntSeeItComing.c4d/Script.c
@@ -0,0 +1,14 @@
+/*-- Didn't see it coming --*/
+
+#strict 2
+#include ACHV
+
+public func GetSavingSlot()
+{
+  return 54;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 0;	//Benötigte Punktzahl einfügen
+}
