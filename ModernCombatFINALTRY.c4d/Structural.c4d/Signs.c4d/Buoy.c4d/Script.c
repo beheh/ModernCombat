c3a5baf1048dc6ef72d38620b70f785b5062f843
Michael14 <michael@missile.beheh.de>
2014-07-10 16:56:53 +0000

    * Script gesäubert.

--- ModernCombatFINALTRY.c4d/Structural.c4d/Signs.c4d/Buoy.c4d/Script.c
+++ ModernCombatFINALTRY.c4d/Structural.c4d/Signs.c4d/Buoy.c4d/Script.c
@@ -22,7 +22,7 @@ func CalcMax()
 
 func Timer()
 {
-  if(GBackLiquid(0,+2) != GBackLiquid (0,-2))
+  if(GBackLiquid(0,+2) != GBackLiquid(0,-2))
   {
     if(sway == 0)
       CalcMax();
@@ -41,15 +41,15 @@ func Timer()
     }
 
     var r2 = Sin(sway*90/max,max);
-    SetDTRotation (r2,0,0,this);
+    SetDTRotation(r2,0,0,this);
   }
   else
-    SetDTRotation (0,0,0,this);
+    SetDTRotation(0,0,0,this);
 }
 
-protected func SetDTRotation (int r, int xoff, int yoff, object obj)
+protected func SetDTRotation(int r, int xoff, int yoff, object obj)
 {
-  var fsin=Sin(r, 1000), fcos=Cos(r, 1000);
+  var fsin = Sin(r,1000),fcos=Cos(r,1000);
 
   SetObjDrawTransform(+fcos, +fsin, (1000-fcos)*xoff - fsin*yoff, -fsin, +fcos, (1000-fcos)*yoff + fsin*xoff, obj);
 }
