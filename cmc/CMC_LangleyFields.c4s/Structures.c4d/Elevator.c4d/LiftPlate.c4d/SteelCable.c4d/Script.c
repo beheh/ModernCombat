d5af2a3a5babe2add0c000b0d2f2f0d43909a18d
ClonkGeist <clonkgeist@missile.beheh.de>
2014-07-12 23:55:36 +0000

    + Gebäudesystem: Aufzug mit fancy Stahlseilen!

---
+++ cmc/CMC_LangleyFields.c4s/Structures.c4d/Elevator.c4d/LiftPlate.c4d/SteelCable.c4d/Script.c
@@ -0,0 +1,16 @@
+/*-- Stahlseil --*/
+
+#strict 2
+
+public func Set(object pTarget, bool fBack, int iColor)
+{
+	SetAction("Rope", pTarget);
+	if(!iColor)
+		iColor = RGB(75, 75, 75);
+	
+	SetClrModulation(iColor, this);
+	
+	if(!fBack)
+	  SetCategory(GetCategory()|C4D_Foreground);
+	return true;
+}
