99a08711e1238b30ae1aa10d3363539379597da0
Michael14 <michael@missile.beheh.de>
2014-07-13 22:01:00 +0000

    * Script gesäubert.

--- cmc/CMC_LangleyFields.c4s/Structures.c4d/Base.c4d/Script.c
+++ cmc/CMC_LangleyFields.c4s/Structures.c4d/Base.c4d/Script.c
@@ -3,19 +3,22 @@
 #strict 2
 #include CCBS
 
-public func IsBase()		{return true;}	//Heimatbasis
-public func NeedBuildingRadius() {return false;}
-public func BuildingRadius()	{return 400;}	//Bauradius
-public func EnergyProduction()	{return 50;}	//Energiehersteller
-public func BasementID()	{return BT02;}	//Fundament
+public func IsBase()			{return true;}	//Heimatbasis
+public func NeedBuildingRadius()	{return false;}	//Benötigt keinen Bauradius
+public func BuildingRadius()		{return 400;}	//Bauradius
+public func EnergyProduction()		{return 50;}	//Energiehersteller
+public func BasementID()		{return BT02;}	//Fundament
+
 
 /* Bau */
 
-public func BuildingConditions(object pBy, int iX, int iY) {return !GetTeamTechLevel(GetPlayerTeam(GetOwner(pBy)));}
+public func BuildingConditions(object pBy, int iX, int iY)	{return !GetTeamTechLevel(GetPlayerTeam(GetOwner(pBy)));}
 
 public func Construction()
 {
+  //Techlevel anheben
   SetTeamTechLevel(GetPlayerTeam(GetOwner()), 1);
+
   return _inherited(...);
 }
 
