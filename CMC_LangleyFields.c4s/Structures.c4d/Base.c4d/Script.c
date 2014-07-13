47bf3a9800680983b2c339ad563439b0346e77ce
ClonkGeist <clonkgeist@missile.beheh.de>
2014-07-13 15:31:05 +0000

    * Gebäudesystem: Stützpunkt nur auf Techstufe 0 baubar * Baumenü: Kleinigkeiten geändert

--- CMC_LangleyFields.c4s/Structures.c4d/Base.c4d/Script.c
+++ CMC_LangleyFields.c4s/Structures.c4d/Base.c4d/Script.c
@@ -4,10 +4,20 @@
 #include CCBS
 
 public func IsBase()		{return true;}	//Heimatbasis
+public func NeedBuildingRadius() {return false;}
 public func BuildingRadius()	{return 400;}	//Bauradius
 public func EnergyProduction()	{return 50;}	//Energiehersteller
 public func BasementID()	{return BT02;}	//Fundament
 
+/* Bau */
+
+public func BuildingConditions(object pBy, int iX, int iY) {return !GetTeamTechLevel(GetPlayerTeam(GetOwner(pBy)));}
+
+public func Construction()
+{
+  SetTeamTechLevel(GetPlayerTeam(GetOwner()), 1);
+  return _inherited(...);
+}
 
 /* Initalisierung */
 
