ac3b5d5410850e0cb83686a76fa2816904f82c1f
ClonkGeist <clonkgeist@missile.beheh.de>
2014-07-17 22:00:11 +0000

    * Gebäudesystem: Upgrade-Descriptions verändert, ggf. halbwegs vernünftige Kosten/Zeitaufwand eingestellt.

--- cmc/CMC_LangleyFields.c4s/Structures.c4d/FusionReactor.c4d/UpgradeWindTurbine.c4d/Script.c
+++ cmc/CMC_LangleyFields.c4s/Structures.c4d/FusionReactor.c4d/UpgradeWindTurbine.c4d/Script.c
@@ -4,8 +4,8 @@
 
 #include CCUS
 
-public func ResearchCost()			{return 1;}	//Entwicklungskosten
-public func ResearchDuration()			{return 100;}	//Entwicklungsdauer in Frames
+public func ResearchCost()			{return 100;}	//Entwicklungskosten
+public func ResearchDuration()			{return 35*6;}	//Entwicklungsdauer in Frames
 
 public func ResearchBase()			{return [];}	//Upgrades, die bereits erforscht sein müssen
 public func UpgradeConditions(object pByObj)	{return true;}	//Zusätzliche Bedingungen
