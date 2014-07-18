c90cdfe02d33fd7fa78fd025305069628a412b16
End <end@missile.beheh.de>
2014-07-18 21:25:42 +0000

    ! Gebäudesystem: Stützpunkt hat nun eine Flagge.

--- cmc/CMC_LangleyFields.c4s/Structures.c4d/Base.c4d/Script.c
+++ cmc/CMC_LangleyFields.c4s/Structures.c4d/Base.c4d/Script.c
@@ -4,6 +4,8 @@
 #include CCBS
 #include BT02
 
+local flag;
+
 public func TechLevel()			{return TECHLEVEL_Start;}	//Benötigte Techstufe
 public func ProvideTechLevel()		{return TECHLEVEL_1;}		//Vorhandene Techstufe
 public func IsBase()			{return true;}			//Heimatbasis
@@ -25,6 +27,11 @@ protected func Construction()
 
 protected func Initialize()
 {
+  //Flagge
+  flag = CreateObject(OFLG,-27,-38);
+  //notwendig, da die Flagge den Besitzer in Initialize löscht
+  SetOwner(GetOwner(), flag);
+  
   //Effekt
   Sound("Building_BaseUp.ogg",true,0,0,GetOwner()+1);
 
