e94562700277ae61c233599410d9f19daf2696d2
End <end@missile.beheh.de>
2014-07-18 09:40:43 +0000

    * Gebäudesystem: Gerüste werden nun abhängig von GetCon() korrekt platziert.

--- CMC_LangleyFields.c4s/Libraries.c4d/BasicBuilding.c4d/Script.c
+++ CMC_LangleyFields.c4s/Libraries.c4d/BasicBuilding.c4d/Script.c
@@ -113,7 +113,7 @@ public func Construction()
       if(!aScaffolds[x])
       aScaffolds[x] = [];
       aScaffolds[x][y] = CreateObject(ScaffoldID(),(GetXOffset() - GetXOffset(ScaffoldID())) + (GetDefWidth()*x)/xcount,
-      													  -(GetDefHeight()*y)/ycount);
+      													  -((GetYOffset()*GetCon())/100)-(GetDefHeight()*y)/ycount);
       SetObjDrawTransform(xsize,0,xpos,0,ysize,ypos,aScaffolds[x][y],0);
       aScaffolds[x][y]->AddVertex(0,((GetDefHeight()*y)/ycount)-GetYOffset(ScaffoldID())+1);
       SetObjectOrder(aScaffolds[x][y],this,1);
