583b979f6f0886d022b0c7cf797d416c97413ec7
End <end@missile.beheh.de>
2014-07-18 20:24:11 +0000

    * Gebäudesystem: Gerüste werden nun korrekt auf- und abgebaut.

--- cmc/CMC_LangleyFields.c4s/Libraries.c4d/BasicBuilding.c4d/Scaffold.c4d/Script.c
+++ cmc/CMC_LangleyFields.c4s/Libraries.c4d/BasicBuilding.c4d/Scaffold.c4d/Script.c
@@ -1,16 +1,54 @@
-/*-- Gerüst --*/
+/*-- Geruest --*/
 
 #strict 2
 
-public func ConstructionTime() {return GetActMapVal("Length", "Construct") * GetActMapVal("Delay", "Construct");}
-public func DestructionTime() {return GetActMapVal("Length", "Deconstruct") * GetActMapVal("Delay", "Deconstruct");}
+local xpos, ypos, xlength, ylength;
 
-protected func StartConstruction(int iX, int iY)
+public func ConstructionTime() 
 {
+  return (GetActMapVal("Length", "Construct") * GetActMapVal("Delay", "Construct"));
+}
+
+public func DestructionTime() 
+{
+  return (GetActMapVal("Length", "Deconstruct") * GetActMapVal("Delay", "Deconstruct"));
+}
+
+protected func StartConstruction(int iX, int iY, int iXLength, int iYLength)
+{
+  //erstmal verstecken 
+  SetClrModulation(RGBa(255,255,255,255));
+  xpos = iX;
+  ypos = iY;
+  xlength = iXLength;
+  ylength = iYLength;
+  if(ypos)
+    Schedule("Construct()",ConstructionTime()* ypos);
+  else
+    Construct();
   return 1;
 }
 
-protected func StartDestruction(int iX, int iY)
+protected func Construct()
 {
+  //wieder sichtbar machen
+  SetClrModulation(RGBa(255,255,255,0));
+  //Aufbauaction setzen
+  SetAction("Construct");
+  return 1;
+}
+
+protected func StartDestruction()
+{
+  if(ylength-ypos)
+    Schedule("SetAction(\"Deconstruct\")",DestructionTime()*(ylength-ypos));
+  else
+    SetAction("Deconstruct");
+  return 1;
+}
+
+protected func Destruct()
+{
+  RemoveObject();
   return 1;
 }
