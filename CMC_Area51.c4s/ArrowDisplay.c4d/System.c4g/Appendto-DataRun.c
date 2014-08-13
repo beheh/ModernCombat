96641c603436a04ee52c2967170c29336e7ec833
ClonkGeist <clonkgeist@missile.beheh.de>
2014-08-13 15:27:34 +0000

    * Kleine Farbanpassungen für DataRun-Pfeile

--- CMC_Area51.c4s/ArrowDisplay.c4d/System.c4g/Appendto-DataRun.c
+++ CMC_Area51.c4s/ArrowDisplay.c4d/System.c4g/Appendto-DataRun.c
@@ -9,7 +9,7 @@ public func IsMarkerTarget(object pBy)
 	return GetOCF(GetComputer2()) & OCF_CrewMember;
 }
 
-public func TVAR_MarkerColor(object pBy) { return RGB(0, 0, 255); } //Farbe des Pfeils
+public func TVAR_MarkerColor(object pBy) { return GetColorDw(GetComputer2()); } //Farbe des Pfeils
 public func TVAR_MarkerIcon(object pBy) { return 0; }	//Angezeigtes Icon (String)
 public func TVAR_MarkerTarget(object pBy) { return GetComputer2(); } //Verweis auf anderes Objekt zum zeigen (0 = this)
 
