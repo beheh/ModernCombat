1e7613cb15698a52245c800ea7e40ced7f9726a0
ClonkGeist <clonkgeist@missile.beheh.de>
2014-08-13 15:27:34 +0000

    * Kleine Farbanpassungen für DataRun-Pfeile

--- cmc/CMC_Area51.c4s/ArrowDisplay.c4d/System.c4g/Appendto-Computer.c
+++ cmc/CMC_Area51.c4s/ArrowDisplay.c4d/System.c4g/Appendto-Computer.c
@@ -8,7 +8,7 @@ public func IsMarkerTarget(object pBy) { return FindObject(GDAR); }
 public func TVAR_MaxMarkerDistance() { return 0; }  //Maximale Entfernung, damit ein Marker erscheint (0 = unendlich)
 public func TVAR_MaxMarkerCount() { return 0; }  //Maximale Anzahl an Marker (0 = unendlich)
 
-public func TVAR_MarkerColor(object pBy) { return RGB(0, 0, 255); }
+public func TVAR_MarkerColor(object pBy) { return RGB(0, 0, 0); }
 public func TVAR_MarkerIcon(object pBy) { return 0; }
 
 public func TVAR_MarkerVisibility(object pBy)
