9d0bb630cf057a0887fb6ac78d93da29be2bc1d0
Monkstar <monkstar@missile.beheh.de>
2014-10-21 17:26:05 +0000

    + Achievement: Peek-a-Boo Script (Getarnten Feind töten) hinzugefügt

---
+++ ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/55Peek-a-Boo.c4d/Script.c
@@ -0,0 +1,14 @@
+/*-- Peek-a-Boo --*/
+
+#strict 2
+#include ACHV
+
+public func GetSavingSlot()
+{
+  return 55;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
