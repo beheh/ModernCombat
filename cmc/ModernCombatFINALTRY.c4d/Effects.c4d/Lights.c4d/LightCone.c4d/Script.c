9db632a2b97e4488282496c73bdabd3c11d2bfad
End <end@missile.beheh.de>
2014-11-29 18:35:57 +0000

    ! Blendgranaten Icon funktioniert nun korrekt.

--- cmc/ModernCombatFINALTRY.c4d/Effects.c4d/Lights.c4d/LightCone.c4d/Script.c
+++ cmc/ModernCombatFINALTRY.c4d/Effects.c4d/Lights.c4d/LightCone.c4d/Script.c
@@ -113,7 +113,7 @@ public func FxFlashlightBlindnessStart(object pTarget, int iNr, temp)
     return;
 
   EffectVar(0, pTarget, iNr) = ScreenRGB(pTarget, RGBa(255, 255, 255, 254), 0, 0, false, SR4K_LayerLight);
-  EffectVar(1, pTarget, iNr) = 6;
+  EffectVar(5, pTarget, iNr) = 0;
 }
 
 static const Flashlight_MinAlpha = 80;
@@ -122,69 +122,60 @@ static const Flashlight_MaxAlpha = 180;
 public func FxFlashlightBlindnessTimer(object pTarget, int iNr)
 {
   var rgb = EffectVar(0, pTarget, iNr);
-  var pUser = EffectVar(2, pTarget, iNr);
-  var iBlindDistance = EffectVar(3, pTarget, iNr);
-  var distAlpha = EffectVar(4, pTarget, iNr) = Max(Flashlight_MinAlpha, (Flashlight_MaxAlpha * Distance(GetX(pTarget), GetY(pTarget), GetX(pUser), GetY(pUser))) / iBlindDistance);
-
+  var distAlpha = EffectVar(4, pTarget, iNr);
+  
   if(!rgb)
     rgb = EffectVar(0, pTarget, iNr) = ScreenRGB(pTarget, RGBa(255, 255, 255, 254), 0, 0, false, SR4K_LayerLight);
 
-  if(rgb->GetAlpha() < Flashlight_MinAlpha)
-    return;
-
+  var a = rgb->GetAlpha();
+	
+  if(a < Flashlight_MinAlpha)
+    return;	
+	
   if(--EffectVar(1, pTarget, iNr) <= 0)
-  {
-    if(GetEffect("IntFlashbang", pTarget))
-      return;
-
     rgb->DoAlpha(-5, distAlpha, 255);
-  }
   else
     rgb->DoAlpha(+18, Min(rgb->GetAlpha()+18, distAlpha), 255);
 
   if(!rgb)
-    return -1;
+    return;
 
-  if(!GetEffect("IntFlashbang", pTarget))
+  a = rgb->GetAlpha();
+	
+  if(!Contained())
   {
-    if(!Contained() && rgb)
+    for(var i = 0; i < GetPlayerCount(); i++)
     {
-      var a = rgb->~GetAlpha(), c;
-      for(var i = 0; i < GetPlayerCount(); i++)
-      {
-        var pCursor = GetCursor(GetPlayerByIndex(i));
-        if(!pCursor)
-          continue;
-        
-        pCursor = pCursor->~GetRealCursor();
-        if(!pCursor && !(pCursor = GetCursor(GetPlayerByIndex(i))))
-          continue;
+	  if(i == GetOwner(pTarget))
+	    continue;
+		
+      var pCursor = GetCursor(GetPlayerByIndex(i))->~GetRealCursor();
+      if(!pCursor && !(pCursor = GetCursor(GetPlayerByIndex(i))))
+        continue;
 
-        if(Contained(pCursor))
-          continue;
+      if(Contained(pCursor))
+        continue;
 
-        var srgb = GetScreenRGB(GetPlayerByIndex(i), SR4K_LayerLight, pCursor);
-        var val;
+      var srgb = GetScreenRGB(GetPlayerByIndex(i), SR4K_LayerLight, pCursor);
 
-        if(srgb)
-          val = srgb->~GetAlpha();
-
-        if(val && 255-a >= val)
-          val = 255 - val;
-        else
-          val = 255 - a;
-
-        var flag = 0;
-        if(c != 0)
-          flag = MSG_Multiple;
-
-        CustomMessage(Format("<c %x>{{SM07}}</c>", RGBa(255,255,255,BoundBy(val, 1, 254))), pTarget, GetPlayerByIndex(i), 0, 0, 0, 0, 0, flag); 
-        c++;
-        }
-      }
-      else
-        CustomMessage("@", pTarget, GetPlayerByIndex(i), 0, 0, 0, 0, 0, 0);
-    }
+      if(srgb && srgb->GetAlpha() < 50) 
+	  {
+        CustomMessage("@", pTarget, GetPlayerByIndex(i));
+		continue;
+	  }
+	  
+	  srgb = GetScreenRGB(GetPlayerByIndex(i), SR4K_LayerSmoke, pCursor);
+      
+      if(srgb && srgb->GetAlpha() < 200) 
+	  {
+        CustomMessage("@", pTarget, GetPlayerByIndex(i));
+		continue;
+	  }
+	  
+      CustomMessage(Format("<c %x>{{SM07}}</c>", RGBa(255,255,255,BoundBy(255 - a, 1, 254))), pTarget, GetPlayerByIndex(i), 0, 0, 0, 0, 0, MSG_Multiple); 
+	}
+  }
+  
   return true;
 }
 
