356b081052e3ff2d765e2425f024ff01d7126a38
Monkstar <monkstar@missile.beheh.de>
2014-12-05 09:28:21 +0000

    * Engineer: Baulimits für Strukturen eingeführt.

--- cmc/CMC_Area51.c4s/Engineer.c4d/Toolkit.c4d/Script.c
+++ cmc/CMC_Area51.c4s/Engineer.c4d/Toolkit.c4d/Script.c
@@ -4,10 +4,22 @@
 
 #include PACK
 
-local plans;
+static EngiPlans, EngiBuildLimits, EngiStorage;
 
 public func RefillTime() {return 30;}
-public func Initialize() {plans = [BBWE, BNGT, HLSM]; return _inherited(...);}
+public func Initialize()
+{
+	if(!EngiPlans)
+	{
+		EngiPlans = [BBWE, BNGT, HLSM];
+		EngiBuildLimits = [10, 4, 5];
+		EngiStorage = CreateArray(GetLength(EngiPlans));
+	
+		for(var i = 0; i < GetLength(EngiPlans); i++)
+			EngiStorage[i] = CreateArray(GetTeamCount());
+	}
+	return _inherited(...);
+}
 
 public func Activate(pClonk)
 { 
@@ -17,11 +29,11 @@ public func Activate(pClonk)
   SetComDir(COMD_Stop, pClonk);
   // Kann der Clonk überhaupt bauen?
   // Menü erzeugen und mit Bauplänen des Spielers füllen
-  CreateMenu(CXCN, pClonk, this(), 1, "$TxtNoconstructionplansa$");
+  CreateMenu(CXCN, pClonk, this(), 1, "$EngiPlansTxtNoconstructiona$");
   var idType; var i = 0;
-  for(i = 0; i < GetLength(plans); i++)
+  for(i = 0; i < GetLength(EngiPlans); i++)
   {
-    idType = plans[i];
+    idType = EngiPlans[i];
     AddMenuItem(Format("%d: $TxtConstructions$", idType->BuildCost()), "CreateConstructionSite", idType, pClonk);
   }
   return(1);
@@ -30,13 +42,45 @@ public func Activate(pClonk)
 protected func CreateConstructionSite(idType)
 {
   // Nur wenn der Clonk steht und sich im Freien befindet
-  if (!WildcardMatch(GetAction(Contained()), "Walk*")) return(0);
-  if (Contained(Contained())) return(0);
+  if (!WildcardMatch(GetAction(Contained()), "Walk*")) return;
+  if (Contained(Contained())) return;
 	if(GetPackPoints() < idType->BuildCost()) return PlayerMessage(GetOwner(), "Not enough points!", this);
 	
   // Besitzer setzen für CreateConstruction
   SetOwner(GetOwner(Contained()));
   var obj = CreateObject(idType, 0, 0, GetOwner());
+  
+  var index;
+  for(var i = 0; i < GetLength(EngiPlans); i++)
+  	if(EngiPlans[i] == idType)
+  		index = i;
+
+  var team = GetPlayerTeam(GetOwner());
+  if(!EngiStorage[index][team])
+ 		EngiStorage[index][team] = CreateArray(EngiBuildLimits[index]);
+  
+  var best;
+  var bestNumber = -1;
+  for(var i = 0; i < EngiBuildLimits[index]; i++)
+  {
+  	if(!EngiStorage[index][team][i] || EngiStorage[index][team][i]->IsDestroyed())
+  	{
+  		best = i;
+  		break;
+  	}
+  	
+  	if(bestNumber == -1 || ObjectNumber(EngiStorage[index][team][i]) < bestNumber)
+  	{
+  		best = i;
+  		bestNumber = ObjectNumber(EngiStorage[index][team][i]);
+  	}
+  }
+  
+  if(EngiStorage[index][team][best] && !EngiStorage[index][team][best]->IsDestroyed())
+  	EngiStorage[index][team][best]->Replaced();
+  	
+  EngiStorage[index][team][best] = obj;
+  
   if(idType == BNGT)
   {
   	if(GetDir(Contained()) == DIR_Left)
@@ -45,10 +89,8 @@ protected func CreateConstructionSite(idType)
   		obj->SetRotation(90);
   }
   
-  DoPackPoints(-idType->BuildCost());
-
   // Bausatz verbrauchen
-  
+  DoPackPoints(-idType->BuildCost());
 
   return(1);
 }
