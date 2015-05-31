cbabcf5d13f946523081efe1be14bf1f8a2e10e5
End <end@missile.beheh.de>
2015-05-31 04:25:36 +0000

    * TouchableAiming Update fuer JnR Spieler

--- cmc/ModernCombatFINALTRY.c4d/System.c4g/TouchableAiming.c
+++ cmc/ModernCombatFINALTRY.c4d/System.c4g/TouchableAiming.c
@@ -20,3 +20,30 @@ public func ControlDown(object pObj)
 
   return(_inherited(...));	
 }
+
+public func ControlUpdate(object pObj, int comdir, bool dig, bool throw)
+{
+  if((comdir != 5) || !(GetOCF() & OCF_Grab))
+    return(_inherited(pObj, comdir, dig, throw));
+
+  //bei Dreifachstop loslassen und Zielen
+  if(pObj && GetEffect("IntWasGrabbed") && Contents(0, pObj))
+  {
+	pObj->SetAction("Walk");
+    if(pObj->~ReadyToSquatAim())
+      pObj->~StartSquatAiming(); 
+    return;
+  }
+  
+  return(_inherited(pObj, comdir, dig, throw));
+}
+
+public func Grabbed(object pByObject, bool fGrab)
+{
+  if(fGrab)
+    AddEffect("IntWasGrabbed", 0, 100, 10);
+
+  return _inherited(...);
+}
+
+public func IntWasGrabbedStart() { return 1; }
