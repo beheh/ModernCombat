04a88e262cfac0a79dc2f9425fb5695a98e2bc03
Michael14 <michael@missile.beheh.de>
2015-08-10 16:40:06 +0000

    * Glas- und Labortische in CMC eingefügt und überladen: Nun zerstörbar. * Glastische ohne Glasplatte bzw. SolidMask sind visuell unterscheidbar zu denen mit. * Grafik aktualisiert.

---
+++ cmc/ModernCombatFINALTRY.c4d/Structural.c4d/Barriers.c4d/Cover.c4d/Labtable.c4d/Script.c
@@ -0,0 +1,32 @@
+/*-- Labortisch --*/
+
+#strict 2
+#include GTBL
+
+public func MaxDamage()			{return 60;}
+public func IsBulletTarget(id def)	{return false;}
+
+
+/* Zerstörung */
+
+public func Damage()
+{
+  if(GetDamage() > MaxDamage())
+  {
+    Destruct();
+    return;
+  }
+  return 1;
+}
+
+public func Destruct()
+{
+  //Effekte
+  CastSmoke("Smoke3",8,15,0,-5,250,200,RGBa(255,255,255,100),RGBa(255,255,255,130));
+  CastParticles("MetalSplinter",3,50,-20,-5,140,70);
+  CastParticles("MetalSplinter",3,70,0,-5,140,70);
+  CastParticles("MetalSplinter",3,50,20,-5,140,70);
+  Sound("CrateDestruct*.ogg",0,0,50);
+
+  RemoveObject();
+}
