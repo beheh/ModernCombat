acd4c886cc1088bbb977e03e8651a781df7cd9e7
Michael14 <michael@missile.beheh.de>
2015-08-28 21:26:06 +0000

    * Artilleriefadenkreuz nach Displays.c4d verschoben. * Script gesäubert.

--- cmc/ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/HazardClonkAp.c4d/Displays.c4d/HeadUpDisplay.c4d/Char.c4d/Script.c
+++ cmc/ModernCombatFINALTRY.c4d/Libraries.c4d/Modifications.c4d/HazardClonkAp.c4d/Displays.c4d/HeadUpDisplay.c4d/Char.c4d/Script.c
@@ -3,21 +3,27 @@
 #strict 2
 
 
+/* Initialisierung */
+
 protected func Initialize()
 {
   return Set();
 }
 
+/* Konfiguration */
+
 public func Set(int iChar, int dwClr)
 {
+  //Bei keiner Zeichenangabe: Null
   if(!iChar)
     iChar = 48;
+  //Bei keiner Farbangabe: Weiﬂ
   if(!dwClr)
     dwClr = RGB(255, 255, 255);
 
+  //Konfiguration setzen
   SetVisibility(VIS_Owner);
   SetAction(Format("%d",iChar));
-
   SetClrModulation(dwClr);
 
   return true;
