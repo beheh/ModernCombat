f071a3a5aaa8f066c4c3360ad5023091d2625eb1
End <end@missile.beheh.de>
2015-11-15 22:31:12 +0000

    + Achievement Juggler hinzugefuegt

---
+++ cmc/ModernCombatFINALTRY.c4d/Rules.c4d/Rewards.c4d/Achievements.c4d/58Juggler.c4d/Script.c
@@ -0,0 +1,14 @@
+/*-- Juggler --*/
+
+#strict 2
+#include ACHV
+
+public func GetSavingSlot()
+{
+  return 58;	//Laufende Nummer einfügen
+}
+
+public func GetAchievementScore()
+{
+  return 1;	//Benötigte Punktzahl einfügen
+}
