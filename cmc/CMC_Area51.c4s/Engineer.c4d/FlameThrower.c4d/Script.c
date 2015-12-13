6e0972d4bafaead6b1528fed74c59816c1520b5a
Michael14 <michael@missile.beheh.de>
2015-12-13 23:38:58 +0000

    + Karabiner eingeführt. * Flammenwerfer für Pionier in Granatenwerfer umgewandelt.

--- cmc/CMC_Area51.c4s/Engineer.c4d/FlameThrower.c4d/Script.c
+++ cmc/CMC_Area51.c4s/Engineer.c4d/FlameThrower.c4d/Script.c
@@ -1,64 +1,73 @@
-/*-- Flammenwerfer --*/
+/*-- Granatenwerfer --*/
 
 #strict 2
 #include WPN2
+
+local casing;
 
 public func HandSize()		{return(1000);}
 public func HandX()		{return(5000);}
 public func HandY()		{return(-2500);}
 public func IsSecondaryWeapon()	{return true;}
 
-public func SelectionTime()	{return 36;}
+public func SelectionTime()	{return 40;}		//Anwahlzeit
 
+
+/* Kompatible Waffenaufsätze */
+
+func PermittedAtts()
+{
+  return AT_Flashlight;
+}
 
 /* Nahkampfangriff */
 
 public func GetMCData(int data)
 {
-  if(data == MC_CanStrike)	return 1;
-  if(data == MC_Damage)		return 20;
-  if(data == MC_Recharge)	return 45;
-  if(data == MC_Power)		return 20;
-  if(data == MC_Angle)		return 45;
+  if(data == MC_CanStrike)	return 1;		//Waffe kann Kolbenschlag ausführen
+  if(data == MC_Damage)		return 20;		//Schaden eines Kolbenschlages
+  if(data == MC_Recharge)	return 45;		//Zeit nach Kolbenschlag bis erneut geschlagen oder gefeuert werden kann
+  if(data == MC_Power)		return 20;		//Wie weit das Ziel durch Kolbenschläge geschleudert wird
+  if(data == MC_Angle)		return 45;		//Mit welchem Winkel das Ziel durch Kolbenschläge geschleudert wird
 }
 
-/* Flammen */
+/* Granaten */
 
 public func FMData1(int data)
 {
-  if(data == FM_Name)		return "$Flamethrower$";
+  if(data == FM_Name)		return "$Grenades$";
 
-  if(data == FM_AmmoID)		return GRAM;
-  if(data == FM_AmmoLoad)	return 1;
+  if(data == FM_AmmoID)		return GRAM;		//ID der Munition
+  if(data == FM_AmmoLoad)	return 1;		//Magazingröße
 
-  if(data == FM_Reload)		return 125;
-  if(data == FM_Recharge)	return 5;
+  if(data == FM_Reload)		return 140;		//Zeit für Nachladen
+  if(data == FM_Recharge)	return 5;		//Zeit bis erneut geschossen werden kann
 
-  if(data == FM_Auto)		return false;
+  if(data == FM_Auto)		return false;		//Kein Automatikfeuer
   
-  if(data == FM_Damage)		return 22;
+  if(data == FM_Damage)		return 22;		//Schadenswert
   
-  if(data == FM_Slot)		return 1;
+  if(data == FM_Slot)		return 1;		//Slot des Feuermodus
   
-  if(data == FM_SpreadAdd)	return 20;
-  if(data == FM_StartSpread)	return 100;
-  if(data == FM_MaxSpread)	return 500;
+  if(data == FM_SpreadAdd)	return 20;		//Bei jedem Schuss hinzuzuaddierende Streuung
+  if(data == FM_StartSpread)	return 100;		//Bei Auswahl der Waffe gesetzte Streuung
+  if(data == FM_MaxSpread)	return 500;		//Maximaler Streuungswert
 
   return Default(data);
 }
 
-/* Flammen - Dauerfeuer */
+/* Granaten - Phosphorgranaten */
 
 public func FMData1T1(int data)
 {
-  if(data == FT_Name)		return "$Auto$";
+  if(data == FT_Name)		return "$Phosphor$";
 
   return FMData1(data);
 }
 
 public func Fire1T1()
 {
-  Fire1();
+  LaunchGrenade(PSHL, 120,Contained()->~AimAngle(0,0,true));
 }
 
 public func BotData1(int data)
@@ -70,59 +79,81 @@ public func BotData1(int data)
   return Default(data);
 }
 
-/* Flammen - Schuss */
+/* Granaten - Schuss */
 
-public func Fire1()
+public func LaunchGrenade(id idg, int speed, int angle, int mode)
 {
-  var user = GetUser();
-  var x,y; user->WeaponEnd(x,y);
-  var angle = user->~AimAngle(20);
-  var xdir = +Sin(angle,120)+GetXDir(user);
-  var ydir = -Cos(angle,120)+GetYDir(user);
-  /*
-  var j = aSlot_Amount[0];
-  for(var i = 0; i < j; i++)
-  { var rand = Random(30) - 15;
-  	var flame = CreateObject(PSPR, x,y, GetController(user));
-  	var xdir = +Sin(angle - i*4 + i * 2,60 + rand)+GetXDir(user);
-  	var ydir = -Cos(angle - i*4 + i * 2,60 - rand)+GetYDir(user) - 18;
+  //Austritt bestimmen
+  var user = Contained();
+  var dir = GetDir(user)*2-1;
+  var x,y;
+  user->WeaponEnd(x,y);
 
+  //Anpassung des Winkels
+  angle = BoundBy(angle/*+GetYDir(user)*/+dir,-360,360);
+  //Geschwindigkeit einstellen
+  var xdir = Sin(angle,speed);
+  var ydir = -Cos(angle,speed);
 
-		SetXDir(xdir, flame);
-		SetYDir(ydir, flame);
-	}
-	*/
-	//Granate abfeuern
-  var grenade=CreateObject(PSHL, x, y, GetController(user));
+  //Granate abfeuern
+  var grenade=CreateObject(idg, x, y, GetController(user));
   SetController(GetController(user), grenade);
   grenade->Launch(xdir+GetXDir(user)/5, ydir/*+GetYDir(user)/4*/, GetFMData(FM_Damage,2), 0, 0, 0, iAttachment, user);
-	
-	Sound("SGST_Fire*.ogg", 0, grenade);
+
+  //Sicht auf Granate wenn der Schütze zielt
+  if(!(user->~IsMachine()) && user->~IsAiming())
+  {
+    SetPlrView(GetController(user),grenade);
+    SetPlrViewRange(100, grenade);
+  }
+
+  //Effekte
+  if(GetEffectData(EFSM_ExplosionEffects) > 0)
+  {
+    CreateParticle("Thrust",x,y,GetXDir(user),GetYDir(user),80,RGBa(255,200,200,0),0,0);
+
+    for(var i=0; i<20; ++i)
+    {
+      CreateParticle("Smoke2",x+RandomX(-5,+5),y+RandomX(-5,+5),
+      		GetXDir(user)+RandomX(0,xdir/4),GetYDir(user)+RandomX(0,ydir/4),
+      		RandomX(100,160),RGBa(200,200,200,0),0,0);
+
+      CreateParticle("BlastSpark1",x+RandomX(-5,+5),y+RandomX(-5,+5),
+      		GetXDir(user)+RandomX(0,xdir/4),GetYDir(user)+RandomX(0,ydir/4),
+      		RandomX(60,90),RGBa(200,200,200,0),0,0);
+    }
+  }
+  Sound("SGST_Fire*.ogg", 0, grenade);
   Echo("SGST_Echo.ogg");
+
+  //Patronenhülse vorhanden
+  casing = 1;
 }
 
-/* Sounds */
+/* Handeffekt */
 
-public func OnAutoStart(int i)
+public func HandR()
 {
-  Sound("FMTR_StartFire.ogg");
-  Sound("FMTR_Fire.ogg",0,0,0,0,1);
-}
-
-public func OnAutoStop(int i)
-{
-  Sound("FMTR_EndFire.ogg");
-  Sound("FMTR_Fire.ogg",0,0,0,0,-1);
-}
-
-public func OnReload()
-{
-  Sound("FMTR_Reload.ogg");
+  var effect = IsReloading();
+  if(effect)
+    return Max(Sin(GetEffect(0,this,effect,6)*90/80,20),0);
 }
 
 /* Allgemein */
 
+func OnReload(i)
+{
+  Sound("GDLR_Reload.ogg");
+  if(casing)
+  {
+    var user = GetUser();
+    var dir = GetDir(user)*2-1;
+    SABulletCasing(dir*1,0,-dir*14*(Random(1)+1),-(13+Random(2)),6,RGB(255,0,0));
+    casing = 0;
+  }
+}
+
 public func OnSelect()
 {
-  Sound("MNGN_Charge.ogg");
+  Sound("GDLR_Charge.ogg");
 }
