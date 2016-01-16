308b18ced83ca9c713e123145e6a300cd60a377a
ClonkGeist <clonkgeist@missile.beheh.de>
2016-01-16 14:12:49 +0000

    ! #013089 ("Dunkelheit beeinflusst Sichtfeld")

--- ModernCombatFINALTRY.c4d/System.c4g/Appendto-Darkness.c
+++ ModernCombatFINALTRY.c4d/System.c4g/Appendto-Darkness.c
@@ -26,9 +26,6 @@ public func ChooserFinished()
 {
   for(var pClonk in FindObjects(Find_OCF(OCF_CrewMember), Find_Not(Find_Owner(NO_OWNER))))
   {
-    if(!GameCallEx("RejectViewRangeChange",500-GetDarkness(100)))
-      SetPlrViewRange(500-GetDarkness(100),pClonk);
-
     var tmp = AddLightAmbience(80,pClonk);
     tmp->ChangeColor(RGBa(255,255,255,110));
     SetOwner(GetOwner(pClonk), tmp);
@@ -46,9 +43,6 @@ public func OnClonkRecruitment(object pClonk)
 
   if(ObjectCount(GetID()) > 1)
     return ScheduleCall(this,"OnClonkRecruitment",1,0,pClonk);
-
-  if(!GameCallEx("RejectViewRangeChange",500-3*GetDarkness(100)))
-    SetPlrViewRange(500-3*GetDarkness(100),pClonk);
 
   var tmp = AddLightAmbience(80,pClonk);
   tmp->ChangeColor(RGBa(255,255,255,110));
