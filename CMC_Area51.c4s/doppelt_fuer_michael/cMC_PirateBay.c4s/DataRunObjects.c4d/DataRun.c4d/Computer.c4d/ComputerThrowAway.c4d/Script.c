e02743a659c9cdd45c5e8abf81da0c0299a9773b
ClonkGeist <clonkgeist@missile.beheh.de>
2016-01-25 03:45:11 +0000

    ! DataRun: ThrowAway-Objekt fuer den Computer wird nun nicht mehr im Inventar erzeugt.

--- CMC_Area51.c4s/doppelt_fuer_michael/cMC_PirateBay.c4s/DataRunObjects.c4d/DataRun.c4d/Computer.c4d/ComputerThrowAway.c4d/Script.c
+++ CMC_Area51.c4s/doppelt_fuer_michael/cMC_PirateBay.c4s/DataRunObjects.c4d/DataRun.c4d/Computer.c4d/ComputerThrowAway.c4d/Script.c
@@ -4,19 +4,23 @@
 
 public func ThrowAway() {
 	 //Auswerfen sofern verschachtelt
-  if(Contained())
+  var dir = +1, rot = 0;
+  if(GetDir(Contained()) == DIR_Right)
   {
-    var dir = +1, rot = 0;
-    if(GetDir(Contained()) == DIR_Right)
-    {
-      dir = -1;
-      rot = 180;
-    }
-
-    Exit(0, 0, 0, rot, dir,-3, RandomX(-8,8));
-
-    Sound("AT4R_ThrowAway.ogg");
+    dir = -1;
+    rot = 180;
   }
+
+  if(Contained())
+    Exit(0, 0, 0, rot, dir,-3, RandomX(-8,8));
+  else {
+  	SetR(rot);
+  	SetXDir(dir*10);
+  	SetYDir(-30);
+  	SetRDir(RandomX(-80, 80));
+  }
+
+  Sound("AT4R_ThrowAway.ogg");
 
   //Verschwinden
   FadeOut();
