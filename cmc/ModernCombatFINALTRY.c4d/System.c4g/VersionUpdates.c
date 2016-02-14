242ddd932f40dae2240e545a12f5b338e0613ad9
Michael14 <michael@missile.beheh.de>
2016-02-14 17:14:08 +0000

    * Überschriften im SV-Konfigurationsmenü grau gefärbt. * Spectator-Objekt: Titelbild eingefügt. * StringTbl korrigiert. * Script gesäubert.

--- cmc/ModernCombatFINALTRY.c4d/System.c4g/VersionUpdates.c
+++ cmc/ModernCombatFINALTRY.c4d/System.c4g/VersionUpdates.c
@@ -1,21 +1,25 @@
-/*-- Neues Script --*/
+/*-- Versionsupdates --*/
+
+//Aktualisiert CMC betreffende, veraltete Datensätze in Spielerdateien.
 
 #strict 2
 
 static const CMC_CURRENT_VERSION = 1;
 
-global func UpdatePlayerData(int iPlr) {
-	var plrversion = GetPlrExtraData(iPlr, "CMC_PlayerData_Version");
-	if(plrversion == CMC_CURRENT_VERSION)
-		return true;
 
-	var aUpdatedData = [["CMC_PlayerData_Version", CMC_CURRENT_VERSION]], iDMModules = GetPlrExtraData(iPlr, "CMC_DeathMenuModules");
+global func UpdatePlayerData(int iPlr)
+{
+  var plrversion = GetPlrExtraData(iPlr, "CMC_PlayerData_Version");
+  if(plrversion == CMC_CURRENT_VERSION)
+    return true;
 
-	if(plrversion < 1)
-		iDMModules |= FKDT_DeathMenu_SpectateMenuItem;
+  var aUpdatedData = [["CMC_PlayerData_Version", CMC_CURRENT_VERSION]], iDMModules = GetPlrExtraData(iPlr, "CMC_DeathMenuModules");
 
-	aUpdatedData[GetLength(aUpdatedData)] = ["CMC_DeathMenuModules", iDMModules];
-	
-	for(var updatedata in aUpdatedData)
-		SetPlrExtraData(iPlr, updatedata[0], updatedata[1]); 
+  if(plrversion < 1)
+    iDMModules |= FKDT_DeathMenu_SpectateMenuItem;
+
+  aUpdatedData[GetLength(aUpdatedData)] = ["CMC_DeathMenuModules", iDMModules];
+
+  for(var updatedata in aUpdatedData)
+    SetPlrExtraData(iPlr, updatedata[0], updatedata[1]); 
 }
